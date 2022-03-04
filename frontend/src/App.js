5b22e4f05e1cc8cf009ef261d3763af10eda64fc
Thomas Brosset <thomas.brosset@gmail.com>
2022-03-04 16:15:30 +0100

    Frontend - add firsts assets + hello world

---
+++ frontend/src/App.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const App = () => {
+  return <div>Hello MonitorEnv</div>
+}
+
+export default App
---
+++ frontend/src/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'rsuite/dist/styles/rsuite-default.css'
+import 'nouislider/distribute/nouislider.css'
+import './index.css'
+import 'ol/ol.css'
+import './App.css'
+
+import * as Sentry from '@sentry/react'
+import { Integrations } from '@sentry/tracing'
+
+import App from './App'
+import GlobalFonts from './fonts/fonts'
+
+if (!(process.env.NODE_ENV === 'development')) {
+  Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_DSN,
+    integrations: [new Integrations.BrowserTracing({
+      tracingOrigins: process.env.REACT_APP_SENTRY_TRACING_ORIGINS
+    })],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0
+  })
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <GlobalFonts />
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
