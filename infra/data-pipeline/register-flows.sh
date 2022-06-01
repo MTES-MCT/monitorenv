0a802859dec337ff9933decf8326599217625198
Thomas Brosset <thomas.brosset@gmail.com>
2022-06-01 15:24:08 +0200

    Infra - make run-container-bash interactive

--- infra/data-pipeline/register-flows.sh
+++ infra/data-pipeline/register-flows.sh
@@ -5,6 +5,5 @@ docker run --rm -t --network=host --name monitorenv-pipeline-agent \
         -v "$(pwd)"/datascience/.env:/home/monitorenv-pipeline/datascience/.env \
         --env-file datascience/.env \
         -e MONITORENV_VERSION \
-        -e LOGBOOK_FILES_GID="$(getent group di_etlmf | cut -d: -f3)" \
         ghcr.io/mtes-mct/monitorenv/monitorenv-pipeline:$MONITORENV_VERSION \
         python main.py
--- infra/data-pipeline/run-container-bash.sh
+++ infra/data-pipeline/run-container-bash.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-docker run --rm -t --network=host --name monitorenv-pipeline-agent \
+docker run -it --network=host --name monitorenv-pipeline-agent \
 	      -v "$(pwd)"/infra/configurations/prefect-agent/backend.toml:/home/monitorenv-pipeline/.prefect/backend.toml \
         -v "$(pwd)"/datascience/.env:/home/monitorenv-pipeline/datascience/.env \
         --env-file datascience/.env \
         -e MONITORENV_VERSION \
-        -e LOGBOOK_FILES_GID="$(getent group di_etlmf | cut -d: -f3)" \
         ghcr.io/mtes-mct/monitorenv/monitorenv-pipeline:$MONITORENV_VERSION \
         /bin/bash
