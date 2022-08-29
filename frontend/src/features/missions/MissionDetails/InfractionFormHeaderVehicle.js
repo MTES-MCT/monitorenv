9c63763ca29a055e9d3149fbb7c15110e11a37c3
Thomas Brosset <thomas.brosset@gmail.com>
2022-08-29 14:30:24 +0200

    feat(rapportage): hide vesselType&vesselSize when vehicle is not vessel

--- frontend/src/features/missions/MissionDetails/InfractionFormHeaderVehicle.js
+++ frontend/src/features/missions/MissionDetails/InfractionFormHeaderVehicle.js
@@ -1,27 +1,34 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Form } from 'rsuite'
-
+import {  useField } from 'formik'
 import { VesselSizeSelector } from './VesselSizeSelector'
 import { VesselTypeSelector } from './VesselTypeSelector'
 import { FormikInput } from '../../../uiMonitor/CustomFormikFields/FormikInput'
+import { vehicleTypeEnum } from '../../../domain/entities/missions'
 
 
 export const InfractionFormHeaderVehicle = ({currentActionIndex, infractionPath}) => {
+  const [vehicleTypeField] = useField(`envActions.${currentActionIndex}.vehicleType`)
+  console.log(vehicleTypeField?.value)
   return (
     <FormGroup>
       <FormColumn>
         <Form.ControlLabel htmlFor={`${infractionPath}.registrationNumber`}>Immatriculation</Form.ControlLabel>
         <FormikInput size='sm' name={`${infractionPath}.registrationNumber`} />
       </FormColumn>
-    
-      <FormColumn>
-        <VesselTypeSelector infractionPath={infractionPath} currentActionIndex={currentActionIndex}/>
-      </FormColumn>
-    
-      <FormColumn>
-        <VesselSizeSelector infractionPath={infractionPath} currentActionIndex={currentActionIndex}/>
-      </FormColumn>
+      {
+        vehicleTypeField?.value === vehicleTypeEnum.VESSEL.code &&
+        <>
+          <FormColumn>
+            <VesselTypeSelector infractionPath={infractionPath} currentActionIndex={currentActionIndex}/>
+          </FormColumn>
+        
+          <FormColumn>
+            <VesselSizeSelector infractionPath={infractionPath} currentActionIndex={currentActionIndex}/>
+          </FormColumn>
+        </>
+      }
     </FormGroup>
   )
 }
