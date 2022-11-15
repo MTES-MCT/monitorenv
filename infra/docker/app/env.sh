b05e56fb05432670f90706cbd77d44240286ca5a
Thomas Brosset <thomas.brosset@gmail.com>
2022-11-15 11:44:35 +0100

    feat(infra): add sentry tracking from env vars

--- infra/docker/app/env.sh
+++ infra/docker/app/env.sh
@@ -3,6 +3,9 @@
 sed -i 's#__REACT_APP_GEOSERVER_REMOTE_URL__#'"$REACT_APP_GEOSERVER_REMOTE_URL"'#g' /home/monitorenv/public/env.js
 sed -i 's#__REACT_APP_GEOSERVER_NAMESPACE__#'"$REACT_APP_GEOSERVER_NAMESPACE"'#g' /home/monitorenv/public/env.js
 sed -i 's#__REACT_APP_MAPBOX_KEY__#'"$REACT_APP_MAPBOX_KEY"'#g' /home/monitorenv/public/env.js
+sed -i 's#__REACT_APP_SENTRY_DSN__#'"$REACT_APP_SENTRY_DSN"'#g' /home/monitorenv/public/env.js
+sed -i 's#__REACT_APP_SENTRY_ENV__#'"$REACT_APP_SENTRY_ENV"'#g' /home/monitorenv/public/env.js
+sed -i 's#__REACT_APP_SENTRY_TRACING_ORIGINS__#'"$REACT_APP_SENTRY_TRACING_ORIGINS"'#g' /home/monitorenv/public/env.js
 sed -i 's#__REACT_APP_SHOM_KEY__#'"$REACT_APP_SHOM_KEY"'#g' /home/monitorenv/public/env.js
 sed -i 's#__REACT_APP_CYPRESS_TEST__#'"$REACT_APP_CYPRESS_TEST"'#g' /home/monitorenv/public/env.js
 
