5ba09d43290fd6a9002be7c2d9fa2a393347556d
Thomas Brosset <thomas.brosset@gmail.com>
2023-02-16 18:21:53 +0100

    remove early return

--- frontend/src/features/SideWindow/SideWindowLauncher.tsx
+++ frontend/src/features/SideWindow/SideWindowLauncher.tsx
@@ -1,5 +1,5 @@
 import { useForceUpdate } from '@mtes-mct/monitor-ui'
-import { useEffect, useRef } from 'react'
+import { MutableRefObject, useEffect, useRef } from 'react'
 import { StyleSheetManager } from 'styled-components'
 
 import { SideWindow } from '.'
@@ -9,16 +9,11 @@ import { sideWindowActions } from './slice'
 
 export function SideWindowLauncher() {
   const dispatch = useAppDispatch()
-  const newWindowRef = useRef<HTMLDivElement | null>(null)
+  const newWindowRef = useRef() as MutableRefObject<HTMLDivElement>
   const { forceUpdate } = useForceUpdate()
   useEffect(() => {
     forceUpdate()
   }, [forceUpdate])
-
-  // First, let's generate the right ref before passing it to <StyleSheetManager />
-  if (!newWindowRef.current) {
-    return <SideWindow ref={newWindowRef} />
-  }
 
   return (
     <StyleSheetManager target={newWindowRef.current}>
