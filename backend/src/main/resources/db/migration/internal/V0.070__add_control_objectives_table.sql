708f3b947a61b9b32fc90ceb047296e239a0d30d
Vincent <vincent.chery@m4x.org>
2023-02-21 16:34:44 +0100

    Add control_objectives table

---
+++ backend/src/main/resources/db/migration/internal/V0.070__add_control_objectives_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE public.control_objectives (
+    year INTEGER NOT NULL,
+    facade VARCHAR NOT NULL,
+    theme_level_1 VARCHAR NOT NULL,
+    theme_level_2 VARCHAR,
+    target_number_of_controls INTEGER NOT NULL
+);
