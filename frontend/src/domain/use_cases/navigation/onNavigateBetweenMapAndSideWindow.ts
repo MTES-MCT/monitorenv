d61640f63d4f8b9ee250a5606a4fbfff8aed4781
Claire Dagan <clairedagan@gmail.com>
2023-04-14 09:29:40 +0200

    [Tech] improve code

--- frontend/src/domain/use_cases/navigation/onNavigateBetweenMapAndSideWindow.ts
+++ frontend/src/domain/use_cases/navigation/onNavigateBetweenMapAndSideWindow.ts
@@ -1,9 +1,11 @@
 import { SideWindowStatus, sideWindowActions } from '../../../features/SideWindow/slice'
 
 export const onNavigateBetweenMapAndSideWindow = (path: string) => (dispatch, getState) => {
-  const { sideWindow } = getState()
-  const { isFormDirty, missionState } = getState().missionState
-  const { listener } = getState().draw
+  const {
+    draw: { listener },
+    missionState: { isFormDirty, missionState },
+    sideWindow
+  } = getState()
 
   if (sideWindow.status === SideWindowStatus.HIDDEN && missionState && !listener && isFormDirty) {
     return dispatch(sideWindowActions.onFocusAndDisplayCancelModal(path))
