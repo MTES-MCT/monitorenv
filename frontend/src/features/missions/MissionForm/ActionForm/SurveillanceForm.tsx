783129aa43b93f8e2ee2a1fb24d97536f8273baf
Claire Dagan <clairedagan@gmail.com>
2023-04-19 10:46:56 +0200

    [Test] console log

--- frontend/src/features/missions/MissionForm/ActionForm/SurveillanceForm.tsx
+++ frontend/src/features/missions/MissionForm/ActionForm/SurveillanceForm.tsx
@@ -20,7 +20,7 @@ export function SurveillanceForm({ currentActionIndex, remove, setCurrentActionI
     setCurrentActionIndex(null)
     remove(currentActionIndex)
   }
-  console.log('test', test2)
+  console.log('test2')
 
   return (
     <>
