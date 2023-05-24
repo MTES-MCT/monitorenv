31a9ee1247b7e537ac99ad43f3827c8099f8595e
Thomas Brosset <thomas.brosset@gmail.com>
2023-05-24 11:47:02 +0200

    amp: add not null constraints

--- backend/src/main/resources/db/migration/internal/V0.081__add_amp_table.sql
+++ backend/src/main/resources/db/migration/internal/V0.081__add_amp_table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS public."amp_cacem"
 (
     id serial NOT NULL,
-    geom geometry(MultiPolygon,4326),
-    mpa_oriname text,
-    des_desigfr text,
+    geom geometry(MultiPolygon,4326) NOT NULL,
+    mpa_oriname text NOT NULL,
+    des_desigfr text NOT NULL,
     row_hash text,
     CONSTRAINT "amp_cacem_pkey" PRIMARY KEY (id)
 );
