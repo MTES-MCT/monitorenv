fc57ac8922f683459a7746bb9730ab4ac863b1ee
Claire Dagan <clairedagan@gmail.com>
2023-06-07 13:29:09 +0200

    [Tech] new router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { THEME, ThemeProvider, OnlyFontGlobalStyle } from '@mtes-mct/monitor-ui'
 import { Provider } from 'react-redux'
-import { /* BrowserRouter as Router, Route, Routes, */ createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { persistStore } from 'redux-persist'
 import { PersistGate } from 'redux-persist/integration/react'
 import { CustomProvider } from 'rsuite'
@@ -12,21 +12,6 @@ import { HomePage } from './pages/HomePage'
 import { homeStore } from './store'
 import frFR from './uiMonitor/locale_frFR'
 import { isBrowserSupported } from './utils/isBrowserSupported'
-
-const router = createBrowserRouter([
-  {
-    Component() {
-      return <SideWindow />
-    },
-    path: '/side_window'
-  },
-  {
-    Component() {
-      return <HomePage />
-    },
-    path: '/'
-  }
-])
 
 export function App() {
   if (!isBrowserSupported()) {
@@ -41,18 +26,13 @@ export function App() {
       <CustomProvider locale={frFR}>
         <Provider store={homeStore}>
           <PersistGate loading={undefined} persistor={persistor}>
-            <RouterProvider fallbackElement={<p>Chargement...</p>} router={router} />
-            {/* 
-            
-            2ème possibilité ici : 
             <Router>
               <Routes>
                 <Route element={<SideWindow />} path="/side_window" />
                 <Route element={<HomePage />} path="/" />
               </Routes>
-            </Router> 
-            
-            */}
+            </Router>
+
             <ErrorToastNotification />
           </PersistGate>
         </Provider>
