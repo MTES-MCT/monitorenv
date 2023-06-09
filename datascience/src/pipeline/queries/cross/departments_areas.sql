6bcb4d2589039c0553988ed0cb51c802174ce60e
Vincent <vincent.chery@m4x.org>
2023-06-09 17:26:18 +0200

    Import overseas facades as departments

--- datascience/src/pipeline/queries/cross/departments_areas.sql
+++ datascience/src/pipeline/queries/cross/departments_areas.sql
@@ -15,4 +15,23 @@ SELECT
 FROM prod.departements dep
 LEFT JOIN prod.departements_en_mer_metropole dep_mer
 ON dep.id = dep_mer.id
+
+UNION ALL
+
+SELECT
+	CASE
+		WHEN facade_cacem = 'Guadeloupe' THEN '971'
+		WHEN facade_cacem = 'Mayotte' THEN '976D'
+		WHEN facade_cacem = 'Martinique' THEN '972R'
+		WHEN facade_cacem = 'Guyane' THEN '973R'
+		WHEN facade_cacem = 'Sud Océan Indien' THEN '974'
+	END AS insee_dep,
+	CASE
+		WHEN facade_cacem = 'Sud Océan Indien' THEN 'Réunion'
+		ELSE facade_cacem
+	END AS name,
+	geometry
+FROM prod.facade_areas
+WHERE facade_cacem IN ('Guadeloupe', 'Mayotte', 'Martinique', 'Guyane', 'Sud Océan Indien')
+
 ORDER BY insee_dep
