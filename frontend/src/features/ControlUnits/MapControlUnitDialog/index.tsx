f8b6c35ac18cd393e5ebfc0a58a013afdd14c5e8
Ivan Gabriele <ivan.gabriele@gmail.com>
2023-09-07 10:21:59 +0200

    Fix map control unit dialog closure

--- frontend/src/features/ControlUnits/MapControlUnitDialog/index.tsx
+++ frontend/src/features/ControlUnits/MapControlUnitDialog/index.tsx
@@ -25,7 +25,7 @@ export function MapControlUnitDialog() {
   const close = useCallback(() => {
     dispatch(
       globalActions.setDisplayedItems({
-        isControlUnitListDialogVisible: false
+        isControlUnitDialogVisible: false
       })
     )
   }, [dispatch])
