48e410275f4107c1e6c9d685fe2807a87018c347
Vincent <vincent.chery@m4x.org>
2023-10-10 18:01:36 +0200

    Create ENUM for control_units_resources type

--- backend/src/main/resources/db/migration/internal/V0.097__alter_control_unit_resources_table.sql
+++ backend/src/main/resources/db/migration/internal/V0.097__alter_control_unit_resources_table.sql
@@ -3,6 +3,38 @@ DROP CONSTRAINT fk_missions_control_resources_control_resource_id_control_unit_r
 
 TRUNCATE public.control_unit_resources;
 
+CREATE TYPE control_unit_resource_type AS ENUM (
+    'AIRPLANE',
+    'BARGE',
+    'CAR',
+    'DRONE',
+    'EQUESTRIAN',
+    'FAST_BOAT',
+    'FRIGATE',
+    'HELICOPTER',
+    'HYDROGRAPHIC_SHIP',
+    'KAYAK',
+    'LIGHT_FAST_BOAT',
+    'MINE_DIVER',
+    'MOTORCYCLE',
+    'NET_LIFTER',
+    'NO_RESOURCE',
+    'OTHER',
+    'PATROL_BOAT',
+    'PEDESTRIAN',
+    'PIROGUE',
+    'RIGID_HULL',
+    'SEA_SCOOTER',
+    'SEMI_RIGID',
+    'SUPPORT_SHIP',
+    'TRAINING_SHIP',
+    'TUGBOAT'
+);
+
+ALTER TABLE public.control_unit_resources
+ALTER COLUMN type TYPE control_unit_resource_type USING type::control_unit_resource_type;
+
+
 ALTER TABLE public.control_unit_resources
     ALTER COLUMN base_id SET NOT NULL,
     ALTER COLUMN type SET NOT NULL;
