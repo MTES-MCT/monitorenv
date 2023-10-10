0f8593d51e27f0b6049f39b2505292a04568dd78
Vincent <vincent.chery@m4x.org>
2023-10-10 16:30:26 +0200

    Remove default latitude and longitude from bases table

--- backend/src/main/resources/db/migration/internal/V0.100__alter_bases_table.sql
+++ backend/src/main/resources/db/migration/internal/V0.100__alter_bases_table.sql
@@ -1,6 +1,6 @@
 ALTER TABLE public.bases
-    ADD COLUMN latitude DOUBLE PRECISION NOT NULL DEFAULT 0,
-    ADD COLUMN longitude DOUBLE PRECISION NOT NULL DEFAULT 0;
+    ADD COLUMN latitude DOUBLE PRECISION NOT NULL,
+    ADD COLUMN longitude DOUBLE PRECISION NOT NULL;
 
 -- Drop and re-add the existing constraint to remove the `ON DELETE CASCADE` option declared in `V0.096`
 ALTER TABLE public.control_unit_resources
