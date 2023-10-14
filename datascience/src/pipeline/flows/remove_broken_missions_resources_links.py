592b4586ca84610558fc634791789a3feabe3969
Vincent <vincent.chery@m4x.org>
2023-10-14 12:09:52 +0200

    Add missing flow file name

--- datascience/src/pipeline/flows/remove_broken_missions_resources_links.py
+++ datascience/src/pipeline/flows/remove_broken_missions_resources_links.py
@@ -1,6 +1,10 @@
+from pathlib import Path
+
 from prefect import Flow
 
 from src.pipeline.shared_tasks.etl import run_sql_script
 
 with Flow("Remove broken missions resources links") as flow:
     run_sql_script("monitorenv/remove_broken_missions_resources_links.sql")
+
+flow.file_name = Path(__file__).name
