b5dca1d7fb090b9e99ed879779002e60ddd6000f
Thomas Brosset <thomas.brosset@gmail.com>
2023-11-03 12:41:52 +0100

    lint back

--- backend/src/main/kotlin/fr/gouv/cacem/monitorenv/infrastructure/database/repositories/JpaReportingRepository.kt
+++ backend/src/main/kotlin/fr/gouv/cacem/monitorenv/infrastructure/database/repositories/JpaReportingRepository.kt
@@ -13,23 +13,23 @@ import fr.gouv.cacem.monitorenv.infrastructure.database.repositories.interfaces.
 import fr.gouv.cacem.monitorenv.infrastructure.database.repositories.interfaces.IDBMissionRepository
 import fr.gouv.cacem.monitorenv.infrastructure.database.repositories.interfaces.IDBReportingRepository
 import fr.gouv.cacem.monitorenv.infrastructure.database.repositories.interfaces.IDBSemaphoreRepository
-import java.time.Instant
-import java.util.UUID
 import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.orm.jpa.JpaObjectRetrievalFailureException
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
+import java.time.Instant
+import java.util.UUID
 
 @Repository
 class JpaReportingRepository(
-        private val dbReportingRepository: IDBReportingRepository,
-        private val dbMissionRepository: IDBMissionRepository,
-        private val dbSemaphoreRepository: IDBSemaphoreRepository,
-        private val dbControlUnitRepository: IDBControlUnitRepository,
-        private val dbEnvActionRepository: IDBEnvActionRepository,
-        private val mapper: ObjectMapper,
+    private val dbReportingRepository: IDBReportingRepository,
+    private val dbMissionRepository: IDBMissionRepository,
+    private val dbSemaphoreRepository: IDBSemaphoreRepository,
+    private val dbControlUnitRepository: IDBControlUnitRepository,
+    private val dbEnvActionRepository: IDBEnvActionRepository,
+    private val mapper: ObjectMapper,
 ) : IReportingRepository {
 
     @Transactional
@@ -49,26 +49,26 @@ class JpaReportingRepository(
     }
 
     override fun findAll(
-            pageable: Pageable,
-            reportingType: List<ReportingTypeEnum>?,
-            seaFronts: List<String>?,
-            sourcesType: List<SourceTypeEnum>?,
-            startedAfter: Instant,
-            startedBefore: Instant?,
-            status: List<String>?,
+        pageable: Pageable,
+        reportingType: List<ReportingTypeEnum>?,
+        seaFronts: List<String>?,
+        sourcesType: List<SourceTypeEnum>?,
+        startedAfter: Instant,
+        startedBefore: Instant?,
+        status: List<String>?,
     ): List<ReportingDTO> {
         val sourcesTypeAsStringArray = sourcesType?.map { it.name }
         val reportingTypeAsStringArray = reportingType?.map { it.name }
         return dbReportingRepository.findAll(
-                        pageable,
-                        reportingType = convertToString(reportingTypeAsStringArray),
-                        seaFronts = convertToString(seaFronts),
-                        sourcesType = convertToString(sourcesTypeAsStringArray),
-                        startedAfter = startedAfter,
-                        startedBefore = startedBefore,
-                        status = convertToString(status),
-                )
-                .map { it.toReportingDTO(mapper) }
+            pageable,
+            reportingType = convertToString(reportingTypeAsStringArray),
+            seaFronts = convertToString(seaFronts),
+            sourcesType = convertToString(sourcesTypeAsStringArray),
+            startedAfter = startedAfter,
+            startedBefore = startedBefore,
+            status = convertToString(status),
+        )
+            .map { it.toReportingDTO(mapper) }
     }
 
     override fun findByControlUnitId(controlUnitId: Int): List<ReportingEntity> {
@@ -84,51 +84,51 @@ class JpaReportingRepository(
     override fun save(reporting: ReportingEntity): ReportingDTO {
         return try {
             val semaphoreReference =
-                    if (reporting.semaphoreId != null) {
-                        dbSemaphoreRepository.getReferenceById(
-                                reporting.semaphoreId,
-                        )
-                    } else {
-                        null
-                    }
-            val controlUnitReference =
-                    if (reporting.controlUnitId != null) {
-                        dbControlUnitRepository.getReferenceById(
-                                reporting.controlUnitId,
-                        )
-                    } else {
-                        null
-                    }
-            val missionReference =
-                    if (reporting.missionId != null) {
-                        dbMissionRepository.getReferenceById(
-                                reporting.missionId,
-                        )
-                    } else {
-                        null
-                    }
-            val envActionReference =
-                    if (reporting.attachedEnvActionId != null) {
-                        dbEnvActionRepository.getReferenceById(
-                                reporting.attachedEnvActionId,
-                        )
-                    } else {
-                        null
-                    }
-            val reportingModel =
-                    ReportingModel.fromReportingEntity(
-                            reporting = reporting,
-                            semaphoreReference = semaphoreReference,
-                            controlUnitReference = controlUnitReference,
-                            missionReference = missionReference,
-                            envActionReference = envActionReference,
+                if (reporting.semaphoreId != null) {
+                    dbSemaphoreRepository.getReferenceById(
+                        reporting.semaphoreId,
                     )
+                } else {
+                    null
+                }
+            val controlUnitReference =
+                if (reporting.controlUnitId != null) {
+                    dbControlUnitRepository.getReferenceById(
+                        reporting.controlUnitId,
+                    )
+                } else {
+                    null
+                }
+            val missionReference =
+                if (reporting.missionId != null) {
+                    dbMissionRepository.getReferenceById(
+                        reporting.missionId,
+                    )
+                } else {
+                    null
+                }
+            val envActionReference =
+                if (reporting.attachedEnvActionId != null) {
+                    dbEnvActionRepository.getReferenceById(
+                        reporting.attachedEnvActionId,
+                    )
+                } else {
+                    null
+                }
+            val reportingModel =
+                ReportingModel.fromReportingEntity(
+                    reporting = reporting,
+                    semaphoreReference = semaphoreReference,
+                    controlUnitReference = controlUnitReference,
+                    missionReference = missionReference,
+                    envActionReference = envActionReference,
+                )
 
             dbReportingRepository.saveAndFlush(reportingModel).toReportingDTO(mapper)
         } catch (e: JpaObjectRetrievalFailureException) {
             throw NotFoundException(
-                    "Invalid reference to semaphore, control unit or mission: not found in referential",
-                    e,
+                "Invalid reference to semaphore, control unit or mission: not found in referential",
+                e,
             )
         } catch (e: DataIntegrityViolationException) {
             throw NotFoundException("Invalid combination of mission and/or envAction", e)
