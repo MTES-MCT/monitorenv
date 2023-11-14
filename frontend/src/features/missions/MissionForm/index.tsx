8c5a0043a4685602e253a975b176cc28c026ed83
Loup Theron <loup.theron@gmail.com>
2023-11-14 16:55:44 +0100

    Add reconnection of SSE

--- frontend/src/features/missions/MissionForm/index.tsx
+++ frontend/src/features/missions/MissionForm/index.tsx
@@ -29,6 +29,7 @@ export function Mission() {
   const { data: missionToEdit, isLoading } = useGetMissionQuery(
     !missionIsNewMission && idTyped ? Number(idTyped) : skipToken
   )
+
   const selectedMission = useMemo(
     () => selectedMissions.find(mis => mis.mission.id === idTyped),
     [idTyped, selectedMissions]
@@ -41,6 +42,7 @@ export function Mission() {
 
     return missionFactory(missionToEdit)
   }, [idTyped, missionIsNewMission, missionToEdit])
+
   if (isLoading) {
     return <div>Chargement en cours</div>
   }
