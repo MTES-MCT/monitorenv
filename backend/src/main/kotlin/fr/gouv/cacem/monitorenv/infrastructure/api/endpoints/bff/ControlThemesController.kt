be980390272d52b694a35357ab12071249305c79
Thomas Brosset <thomas.brosset@gmail.com>
2023-12-14 17:37:50 +0100

    add test for ControlPlanThemesController

--- backend/src/main/kotlin/fr/gouv/cacem/monitorenv/infrastructure/api/endpoints/bff/ControlThemesController.kt
+++ backend/src/main/kotlin/fr/gouv/cacem/monitorenv/infrastructure/api/endpoints/bff/ControlThemesController.kt
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@Deprecated("Use ControlPlanThemesController instead")
 @RestController
 @RequestMapping("/bff/v1/controlthemes")
 @Tag(name = "BFF.Control Themes", description = "API control themes")
