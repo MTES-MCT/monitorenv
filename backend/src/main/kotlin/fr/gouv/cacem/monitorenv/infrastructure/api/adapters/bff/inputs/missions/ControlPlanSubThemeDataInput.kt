e2a2ff2abc1d322f529d1f13f9654b39ad9e73a7
Thomas Brosset <thomas.brosset@gmail.com>
2023-12-14 17:37:50 +0100

    fix test data

--- backend/src/main/kotlin/fr/gouv/cacem/monitorenv/infrastructure/api/adapters/bff/inputs/missions/ControlPlanSubThemeDataInput.kt
+++ backend/src/main/kotlin/fr/gouv/cacem/monitorenv/infrastructure/api/adapters/bff/inputs/missions/ControlPlanSubThemeDataInput.kt
@@ -3,12 +3,12 @@ package fr.gouv.cacem.monitorenv.infrastructure.api.adapters.bff.inputs.missions
 import fr.gouv.cacem.monitorenv.domain.entities.mission.envAction.EnvActionControlPlanSubThemeEntity
 
 data class ControlPlanSubThemeDataInput(
-    val id: Int,
+    val subThemeId: Int,
     val tags: List<String> ? = null,
 ) {
     fun toEnvActionControlPlanSubThemeEntity(): EnvActionControlPlanSubThemeEntity {
         return EnvActionControlPlanSubThemeEntity(
-            subThemeId = this.id,
+            subThemeId = this.subThemeId,
             tags = this.tags,
         )
     }
