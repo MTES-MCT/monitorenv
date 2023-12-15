ccc00233974866fd64c7efa017b86c3d06278f5d
Thomas Brosset <thomas.brosset@gmail.com>
2023-12-15 11:27:53 +0100

    insert new themes

--- backend/src/main/resources/db/migration/internal/V0.105__add_control_plan_2024.sql
+++ backend/src/main/resources/db/migration/internal/V0.105__add_control_plan_2024.sql
@@ -1,24 +1,25 @@
 INSERT INTO control_plan_themes (id, theme) VALUES 
-(100,'Mouillage individuel')
-(101,'Zone de mouillage et d''équipement léger (ZMEL)')
-(102,'Rejet')
-(103,'Espèce protégée et leur habitat (faune et flore)')
-(104,'Bien culturel maritime')
-(105,'Épave')
-(106,'Activité et manifestation soumises à évaluation d''incidence Natura 2003')
-(107,'Domanialité publique (circulation et dégradation)')
-(108,'Culture marine')
-(109,'Travaux en milieu marin')
-(110,'Arrêté à visa environnemental')
-(111,'Parc national')
-(112,'Réserve naturelle')
-(113,'Arrêté de protection')
-(114,'Autres')
-(115,'Pêche à pied')
-(116,'Pêche de loisir (autre que PAP)')
+(100,'Mouillage individuel'),
+(101,'Zone de mouillage et d''équipement léger (ZMEL)'),
+(102,'Rejet'),
+(103,'Espèce protégée et leur habitat (faune et flore)'),
+(104,'Bien culturel maritime'),
+(105,'Épave'),
+(107,'Domanialité publique (circulation et dégradation)'),
+(108,'Culture marine'),
+(111,'Parc national'),
+(112,'Réserve naturelle'),
+(113,'Arrêté de protection'),
+(114,'Autres'),
+(116,'Pêche de loisir (autre que PAP)'),
 (117,'Surveillance générale');
 
-SELECT setval('control_plans_themes_id_seq', (SELECT max(id) FROM control_plans_themes), true);
+-- (106,'Activité et manifestation soumises à évaluation d’incidence Natura 2000'),
+-- (109,'Travaux en milieu marin'),
+-- (110,'Arrêté à visa environnemental'),
+-- (115,'Pêche à pied'),
+
+SELECT setval('control_plans_themes_id_seq', (SELECT max(id) FROM control_plan_themes), true);
 
 INSERT INTO control_plan_tags (id,theme_id,tag) VALUES 
 (10,103,'Mammifères marins'),
@@ -27,11 +28,17 @@ INSERT INTO control_plan_tags (id,theme_id,tag) VALUES
 (13,103,'Poissons'),
 (14,103,'Flore'),
 (15,103,'Habitat'),
-(16,103,'Autres espèces protégées'),
-(17,115,'Bichique'),
-(18,115,'Civelle'),
-(19,115,'Filet fixe'),
-(20,115,'Autre');
+(16,103,'Autres espèces protégées');
+
+INSERT INTO control_plan_tags (id,tag,theme_id)
+  SELECT new_tags.*, existing_theme.id FROM (VALUES
+    (17,'Bichique'),
+    (18,'Civelle'),
+    (19,'Filet fixe'),
+    (20,'Autre')) as new_tags,
+    (
+    SELECT id FROM control_plan_themes
+    WHERE theme = 'Pêche à pied') existing_theme;
 
 SELECT setval('control_plan_tags_id_seq', (SELECT max(id) FROM control_plan_tags), true);
 
@@ -69,11 +76,6 @@ INSERT INTO control_plan_sub_themes (id,theme_id,subtheme,year) VALUES
 (129,105,'Recel ou détournement d''une épave maritime',2024),
 (130,105,'Épave / Navire abandonné',2024),
 (131,105,'Autre (Épave)',2024),
-(132,106,'Existence d''une évaluation d''incidence Natura 2000',2024),
-(133,106,'Prescriptions environnementales des manifestations / activités dans une zone Natura 2000',2024),
-(134,106,'Travaux dans une zone Natura 2000',2024),
-(135,106,'Charte Natura 2000',2024),
-(136,106,'Autre (EIN2000)',2024),
 (137,107,'Circulation des VTM sur le DPM',2024),
 (138,107,'Respect des espaces balisés',2024),
 (139,107,'Dégradation du DPM',2024),
@@ -82,17 +84,6 @@ INSERT INTO control_plan_sub_themes (id,theme_id,subtheme,year) VALUES
 (142,108,'Remise en état après occupation du DPM',2024),
 (143,108,'Implantation',2024),
 (144,108,'Autre (Culture marine)',2024),
-(145,109,'Dragage',2024),
-(146,109,'Clapage',2024),
-(147,109,'Extraction de granulats ',2024),
-(148,109,'Chantier marin',2024),
-(149,109,'Chantier portuaire',2024),
-(150,109,'Travaux réglementés par AMP',2024),
-(151,109,'Autres travaux en mer',2024),
-(152,110,'Arrêtés municipaux réglementant certaines activités avec un impact sur l''environnement marin ',2024),
-(153,110,'Arrêtés du préfet de département réglementant certaines activités avec un impact sur l''environnement marin',2024),
-(154,110,'Arrêtés du préfet maritime réglementant certaines activités avec un impact sur l''environnement marin ',2024),
-(155,110,'Autres arrêtés réglementant certaines activités avec un impact sur l''environnement marin ',2024),
 (156,111,'Réglementation du parc national',2024),
 (157,111,'Autre (Parc national)',2024),
 (158,112,'Réglementation de la réserve naturelle',2024),
@@ -106,14 +97,65 @@ INSERT INTO control_plan_sub_themes (id,theme_id,subtheme,year) VALUES
 (166,114,'Manifestation sur le DPM avec prescriptions environnementales',2024),
 (167,114,'Chasse sur le DPM',2024),
 (168,114,'Autre',2024),
-(169,115,'Pêche à pied de loisir',2024),
-(170,115,'Pêche à pied professionnelle',2024),
-(171,115,'Engin non-marqué',2024),
-(172,115,'Autre (Pêche à pied)',2024),
 (173,116,'Pêche embarquée',2024),
 (174,116,'Pêche sous-marine',2024),
 (175,116,'Engin non-marqué',2024),
 (176,116,'Autre (Pêche de loisir hors PAP)',2024),
-(177,117,'Surveillance générale',2024);
+(177,117,'Surveillance générale',2024)
+;
+
+
+INSERT INTO control_plan_sub_themes (id,subtheme,year,theme_id)
+  SELECT new_sub_themes.*, existing_theme.id FROM (VALUES
+  (132,'Existence d''une évaluation d''incidence Natura 2000',2024),
+  (133,'Prescriptions environnementales des manifestations / activités dans une zone Natura 2000',2024),
+  (134,'Travaux dans une zone Natura 2000',2024),
+  (135,'Charte Natura 2000',2024),
+  (136,'Autre (EIN2000)',2024)) as new_sub_themes,
+    (
+    SELECT id FROM control_plan_themes
+        WHERE theme = 'Activités et manifestations soumises à évaluation d’incidence Natura 2000') existing_theme
+;
+
+
+INSERT INTO control_plan_sub_themes (id,subtheme,year,theme_id)
+  SELECT new_sub_themes.*, existing_theme.id FROM (VALUES
+  (145,'Dragage',2024),
+  (146,'Clapage',2024),
+  (147,'Extraction de granulats ',2024),
+  (148,'Chantier marin',2024),
+  (149,'Chantier portuaire',2024),
+  (150,'Travaux réglementés par AMP',2024),
+  (151,'Autres travaux en mer',2024)) as new_sub_themes,
+    (
+    SELECT id FROM control_plan_themes
+        WHERE theme = 'Travaux en milieu marin') existing_theme
+;
+
+
+INSERT INTO control_plan_sub_themes (id,subtheme,year,theme_id)
+  SELECT new_sub_themes.*, existing_theme.id FROM (VALUES
+  (152,'Arrêtés municipaux réglementant certaines activités avec un impact sur l''environnement marin ',2024),
+  (153,'Arrêtés du préfet de département réglementant certaines activités avec un impact sur l''environnement marin',2024),
+  (154,'Arrêtés du préfet maritime réglementant certaines activités avec un impact sur l''environnement marin ',2024),
+  (155,'Autres arrêtés réglementant certaines activités avec un impact sur l''environnement marin ',2024)
+) as new_sub_themes,
+    (
+    SELECT id FROM control_plan_themes
+        WHERE theme = 'Arrêté à visa environnemental') existing_theme
+;
+
+INSERT INTO control_plan_sub_themes (id,subtheme,year,theme_id)
+  SELECT new_sub_themes.*, existing_theme.id FROM (VALUES
+  (169,'Pêche à pied de loisir',2024),
+  (170,'Pêche à pied professionnelle',2024),
+  (171,'Engin non-marqué',2024),
+  (172,'Autre (Pêche à pied)',2024)
+) as new_sub_themes,
+    (
+    SELECT id FROM control_plan_themes
+        WHERE theme = 'Pêche à pied') existing_theme
+;
+
 
 SELECT setval('control_plan_sub_themes_id_seq', (SELECT max(id) FROM control_plan_sub_themes), true);
