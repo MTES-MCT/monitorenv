142d4036a38e901a32f3ce0e7751aa0dbd9dbe67
Claire Dagan <clairedagan@gmail.com>
2023-12-15 09:59:43 +0100

    [Test] re-add deleted test

---
+++ backend/src/test/kotlin/fr/gouv/cacem/monitorenv/infrastructure/database/repositories/JpaControlPlanThemeRepositoryITests.kt
@@ -0,0 +1,31 @@
+package fr.gouv.cacem.monitorenv.infrastructure.database.repositories
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+
+class JpaControlPlanThemeRepositoryITests : AbstractDBTests() {
+    @Autowired private lateinit var jpaControlPlanThemeRepository: JpaControlPlanThemeRepository
+
+    @Test
+    fun `findAll Should return all control plan themes`() {
+        // When
+        val requestedControlPlanThemes = jpaControlPlanThemeRepository.findAll()
+        // Then
+        assertThat(requestedControlPlanThemes.size).isEqualTo(23)
+        assertThat(requestedControlPlanThemes[5].id).isEqualTo(6)
+        assertThat(requestedControlPlanThemes[5].theme).isEqualTo("Divers")
+    }
+
+    @Test
+    fun `findByYear Should return all control plan theme for a specific year`() {
+        // When
+        val requestedControlPlanThemesFor2023 = jpaControlPlanThemeRepository.findByYear(2023)
+        val requestedControlPlanThemesFor2024 = jpaControlPlanThemeRepository.findByYear(2024)
+        // Then
+        assertThat(requestedControlPlanThemesFor2023.size).isEqualTo(83)
+        assertThat(requestedControlPlanThemesFor2024.size).isEqualTo(12)
+        assertThat(requestedControlPlanThemesFor2024[5].id).isEqualTo(100001)
+        assertThat(requestedControlPlanThemesFor2024[5].theme).isEqualTo("Mouillage Individuel")
+    }
+}
