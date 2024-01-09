943dce91733390ac635f821c59c8ebe189b52407
Thomas Brosset <thomas.brosset@gmail.com>
2024-01-09 11:49:02 +0100

    add query counter with datasource-proxy

---
+++ backend/src/main/kotlin/fr/gouv/cacem/monitorenv/config/DataSourceProxyBeanPostProcessor.kt
@@ -0,0 +1,29 @@
+package fr.gouv.cacem.monitorenv.config
+
+import net.ttddyy.dsproxy.support.ProxyDataSourceBuilder
+import org.springframework.beans.BeansException
+import org.springframework.beans.factory.config.BeanPostProcessor
+import org.springframework.stereotype.Component
+import javax.sql.DataSource
+
+@Component
+class DataSourceProxyBeanPostProcessor(private val customQueryCountListener: CustomQueryCountListener) : BeanPostProcessor {
+    @Throws(BeansException::class)
+    override fun postProcessBeforeInitialization(bean: Any, beanName: String): Any? {
+        return bean
+    }
+
+    @Throws(BeansException::class)
+    override fun postProcessAfterInitialization(bean: Any, beanName: String): Any? {
+        if (bean is DataSource) {
+            val listener = customQueryCountListener
+
+            return ProxyDataSourceBuilder
+                .create(bean)
+                .name("MyDS-Proxy")
+                .listener(listener)
+                .build()
+        }
+        return bean
+    }
+}
