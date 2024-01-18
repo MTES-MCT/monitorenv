27324f588dd615c807b1d4438795d87bdeadfb06
Claire Dagan <clairedagan@gmail.com>
2024-01-18 14:41:41 +0100

    [Mission] update api to get fish actions

--- frontend/src/api/monitorFishMissionActions.ts
+++ frontend/src/api/monitorFishMissionActions.ts
@@ -3,7 +3,7 @@ import { monitorFishPublicApi } from './api'
 export const monitorFishMissionActions = monitorFishPublicApi.injectEndpoints({
   endpoints: builder => ({
     getFishMissionActions: builder.query<any, number>({
-      query: missionId => `/v1/mission_actions/${missionId}`
+      query: missionId => `/v1/mission_actions?missionId=${missionId}`
     })
   })
 })
