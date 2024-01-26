d059abed4ad31ab2ccb1b962081d7f6fa1ef03e2
Thomas Brosset <thomas.brosset@gmail.com>
2024-01-26 12:14:38 +0100

    add query counter with datasource-proxy

---
+++ backend/src/main/kotlin/fr/gouv/cacem/monitorenv/config/CustomQueryCountListener.kt
@@ -0,0 +1,24 @@
+package fr.gouv.cacem.monitorenv.config
+
+import net.ttddyy.dsproxy.ExecutionInfo
+import net.ttddyy.dsproxy.QueryInfo
+import net.ttddyy.dsproxy.listener.QueryExecutionListener
+import java.util.concurrent.atomic.AtomicInteger
+
+class CustomQueryCountListener : QueryExecutionListener {
+    private val queryCount = AtomicInteger()
+    override fun beforeQuery(execInfo: ExecutionInfo, queryInfoList: List<QueryInfo>) {
+    }
+
+    override fun afterQuery(execInfo: ExecutionInfo, queryInfoList: List<QueryInfo>) {
+        queryCount.incrementAndGet()
+    }
+
+    fun getQueryCount(): Int {
+        return queryCount.get()
+    }
+
+    fun resetQueryCount() {
+        queryCount.set(0)
+    }
+}
---
+++ backend/src/main/kotlin/fr/gouv/cacem/monitorenv/config/DataSourceProxyTestConfig.kt
@@ -0,0 +1,13 @@
+package fr.gouv.cacem.monitorenv.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class DataSourceProxyTestConfig {
+
+    @Bean
+    fun customQueryCountListener(): CustomQueryCountListener? {
+        return CustomQueryCountListener()
+    }
+}
