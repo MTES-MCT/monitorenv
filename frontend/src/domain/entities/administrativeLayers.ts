83994c9f20a593097ee716dbeeb25b4076a1de33
Thomas Brosset <thomas.brosset@gmail.com>
2024-01-31 17:31:49 +0100

    refacto layername definition

--- frontend/src/domain/entities/administrativeLayers.ts
+++ frontend/src/domain/entities/administrativeLayers.ts
@@ -1,78 +1,17 @@
-import { LayerType } from './layers/constants'
+import { Layers } from './layers/constants'
 
 export const administrativeLayers = [
-  [
-    {
-      code: '3_miles_areas',
-      containsMultipleZones: false,
-      group: null,
-      isIntersectable: false,
-      name: '3 Milles',
-      showMultipleZonesInAdministrativeZones: false,
-      subZoneFieldKey: null,
-      type: LayerType.ADMINISTRATIVE
-    }
-  ],
-  [
-    {
-      code: '6_miles_areas',
-      containsMultipleZones: false,
-      group: null,
-      isIntersectable: false,
-      name: '6 Milles',
-      showMultipleZonesInAdministrativeZones: false,
-      subZoneFieldKey: null,
-      type: LayerType.ADMINISTRATIVE
-    }
-  ],
-  [
-    {
-      code: '12_miles_areas',
-      containsMultipleZones: false,
-      group: null,
-      isIntersectable: false,
-      name: '12 Milles',
-      showMultipleZonesInAdministrativeZones: false,
-      subZoneFieldKey: null,
-      type: LayerType.ADMINISTRATIVE
-    }
-  ],
-  [
-    {
-      code: 'eez_areas',
-      containsMultipleZones: true,
-      group: null,
-      isIntersectable: true,
-      name: 'Zones ZEE',
-      showMultipleZonesInAdministrativeZones: false,
-      subZoneFieldKey: 'union',
-      type: LayerType.ADMINISTRATIVE
-    }
-  ],
-  [
-    {
-      code: 'fao_areas',
-      containsMultipleZones: true,
-      group: null,
-      isIntersectable: true,
-      name: 'Zones FAO / CIEM',
-      showMultipleZonesInAdministrativeZones: false,
-      subSubZoneFieldKey: 'f_subdivis',
-      subZoneFieldKey: 'f_division',
-      type: LayerType.ADMINISTRATIVE,
-      zoneFieldKey: 'f_subarea'
-    }
-  ],
-  [
-    {
-      code: 'aem_areas',
-      containsMultipleZones: false,
-      group: null,
-      isIntersectable: false,
-      name: 'Zones AEM (MED)',
-      showMultipleZonesInAdministrativeZones: false,
-      subZoneFieldKey: 'name',
-      type: LayerType.ADMINISTRATIVE
-    }
-  ]
+  [Layers.THREE_MILES],
+  [Layers.SIX_MILES],
+  [Layers.TWELVE_MILES],
+  [Layers.EEZ],
+  [Layers.FAO],
+  [Layers.AEM],
+  [Layers.DEPARTMENTS],
+  [Layers.FACADES],
+  [Layers.LOW_WATER_LINE],
+  [Layers.SALTWATER_LIMIT_AREAS],
+  [Layers.STRAIGHT_BASELINE],
+  [Layers.TERRITORIAL_SEAS],
+  [Layers.TRANSVERSAL_SEA_LIMIT_AREAS]
 ]
--- frontend/src/domain/entities/layers/constants.ts
+++ frontend/src/domain/entities/layers/constants.ts
@@ -6,60 +6,76 @@ export enum LayerType {
   REGULATORY = 'REGULATORY'
 }
 
+type Layer = {
+  code: string
+  getZoneName?: (feature: any) => string
+  name?: string
+  subSubZoneFieldKey?: string
+  subZoneFieldKey?: string
+  type?: LayerType
+  zIndex?: number
+  zoneFieldKey?: string
+}
+
+enum MonitorEnvLayers {
+  ACTIONS = 'ACTIONS',
+  AEM = 'AEM',
+  AMP = 'AMP',
+  BASE_LAYER = 'BASE_LAYER',
+  DEPARTMENTS = 'DEPARTMENTS',
+  DRAW = 'DRAW',
+  EEZ = 'EEZ',
+  FACADES = 'FACADES',
+  FAO = 'FAO',
+  INTEREST_POINT = 'INTEREST_POINT',
+  LOW_WATER_LINE = 'LOW_WATER_LINE',
+  MEASUREMENT = 'MEASUREMENT',
+  MISSIONS = 'MISSIONS',
+  MISSION_SELECTED = 'MISSION_SELECTED',
+  MISSION_TO_ATTACH_ON_REPORTING = 'MISSION_TO_ATTACH_ON_REPORTING',
+  REGULATORY_ENV = 'REGULATORY_ENV',
+  REGULATORY_ENV_PREVIEW = 'REGULATORY_ENV_PREVIEW',
+  REPORTINGS = 'REPORTINGS',
+  REPORTING_SELECTED = 'REPORTING_SELECTED',
+  REPORTING_TO_ATTACH_ON_MISSION = 'REPORTING_TO_ATTACH_ON_MISSION',
+  SALTWATER_LIMIT_AREAS = 'SALTWATER_LIMIT_AREAS',
+  SELECTED_MISSION_TO_ATTACH_ON_REPORTING = 'SELECTED_MISSION_TO_ATTACH_ON_REPORTING',
+  SELECTED_REPORTING_TO_ATTACH_ON_MISSION = 'SELECTED_REPORTING_TO_ATTACH_ON_MISSION',
+  SEMAPHORES = 'SEMAPHORES',
+  SIX_MILES = 'SIX_MILES',
+  STATIONS = 'STATIONS',
+  STRAIGHT_BASELINE = 'STRAIGHT_BASELINE',
+  TERRITORIAL_SEAS = 'TERRITORIAL_SEAS',
+  THREE_MILES = 'THREE_MILES',
+  TRANSVERSAL_SEA_LIMIT_AREAS = 'TRANSVERSAL_SEA_LIMIT_AREAS',
+  TWELVE_MILES = 'TWELVE_MILES'
+}
+
+/**
+ * /!\ Do not modify the code property : in some cases, it is the Geoserver layer name, hence the name of the PostGIS table
+ */
 // TODO Generate `zIndex` from order of declaration rather than manually setting it.
-export const Layers = {
-  ACTIONS: {
-    code: 'actions',
-    zIndex: 1110
+/* eslint-disable sort-keys-fix/sort-keys-fix */
+export const Layers: Record<MonitorEnvLayers, Layer> = {
+  [MonitorEnvLayers.BASE_LAYER]: {
+    code: 'baselayer',
+    name: '',
+    type: LayerType.BASE_LAYER
   },
-  AEM: {
+  [MonitorEnvLayers.AEM]: {
     code: 'aem_areas',
-    containsMultipleZones: false,
-    group: null,
-    isIntersectable: false,
     name: 'Zones AEM (MED)',
-    showMultipleZonesInAdministrativeZones: false,
     subZoneFieldKey: 'name',
     type: LayerType.ADMINISTRATIVE
   },
-  AMP: {
-    code: 'AMP',
-    containsMultipleZones: false,
-    group: null,
-    isIntersectable: false,
-    name: 'environment_amp_areas',
-    showMultipleZonesInAdministrativeZones: false,
-    subZoneFieldKey: null,
-    type: LayerType.REGULATORY,
-    zIndex: 925
-  },
-  BASE_LAYER: {
-    code: 'baselayer',
-    containsMultipleZones: false,
-    group: null,
-    isIntersectable: false,
-    name: '',
-    showMultipleZonesInAdministrativeZones: false,
-    subZoneFieldKey: null,
-    type: LayerType.BASE_LAYER
-  },
-  DRAW: {
-    code: 'draw_layer',
-    zIndex: 1500
-  },
-  EEZ: {
+  [MonitorEnvLayers.EEZ]: {
     code: 'eez_areas',
-    containsMultipleZones: true,
-    group: null,
-    isIntersectable: true,
     name: 'Zones ZEE',
-    showMultipleZonesInAdministrativeZones: false,
     subZoneFieldKey: 'union',
     type: LayerType.ADMINISTRATIVE
   },
-  FAO: {
+  [MonitorEnvLayers.FAO]: {
     code: 'fao_areas',
-    containsMultipleZones: true,
     getZoneName: feature => {
       if (feature.get(Layers.FAO.subSubZoneFieldKey)) {
         return feature.get(Layers.FAO.subSubZoneFieldKey)
@@ -73,121 +89,137 @@ export const Layers = {
 
       return ''
     },
-    group: null,
-    isIntersectable: true,
     name: 'Zones FAO / CIEM',
-    showMultipleZonesInAdministrativeZones: false,
     subSubZoneFieldKey: 'f_subdivis',
     subZoneFieldKey: 'f_division',
     type: LayerType.ADMINISTRATIVE,
     zoneFieldKey: 'f_subarea'
   },
-  INTEREST_POINT: {
-    zIndex: 1220
+  [MonitorEnvLayers.SALTWATER_LIMIT_AREAS]: {
+    code: 'saltwater_limit_areas',
+    name: 'Limites de salure des eaux',
+    type: LayerType.ADMINISTRATIVE
   },
-  MEASUREMENT: {
-    code: 'measurement',
-    containsMultipleZones: false,
-    group: null,
-    isIntersectable: false,
-    name: '',
-    showMultipleZonesInAdministrativeZones: false,
-    subZoneFieldKey: null,
-    type: LayerType.MEASUREMENT,
-    zIndex: 1210
+  [MonitorEnvLayers.TRANSVERSAL_SEA_LIMIT_AREAS]: {
+    code: 'transversal_sea_limit_areas',
+    name: 'Limites transversales de mer',
+    type: LayerType.ADMINISTRATIVE
   },
-  MISSION_SELECTED: {
-    code: 'mission_selected',
-    zIndex: 1050
+  [MonitorEnvLayers.THREE_MILES]: {
+    code: '3_miles_areas',
+    name: '3 Milles',
+    type: LayerType.ADMINISTRATIVE
   },
-  MISSION_TO_ATTACH_ON_REPORTING: {
-    code: 'mission_to_attach_on_reporting',
-    zIndex: 1050
+  [MonitorEnvLayers.SIX_MILES]: {
+    code: '6_miles_areas',
+    name: '6 Milles',
+    type: LayerType.ADMINISTRATIVE
   },
-  MISSIONS: {
-    code: 'missions',
-    zIndex: 1050
+  [MonitorEnvLayers.TWELVE_MILES]: {
+    code: '12_miles_areas',
+    name: '12 Milles',
+    type: LayerType.ADMINISTRATIVE
   },
-  REGULATORY_ENV: {
-    code: 'REGULATORY_ENV',
-    containsMultipleZones: false,
-    group: null,
-    isIntersectable: false,
+  [MonitorEnvLayers.TERRITORIAL_SEAS]: {
+    code: 'territorial_seas',
+    name: 'Eaux territoriales',
+    type: LayerType.ADMINISTRATIVE
+  },
+  [MonitorEnvLayers.STRAIGHT_BASELINE]: {
+    code: 'straight_baseline',
+    name: 'Lignes de base droite',
+    type: LayerType.ADMINISTRATIVE
+  },
+  [MonitorEnvLayers.LOW_WATER_LINE]: {
+    code: 'low_water_line',
+    name: 'Laisses des basses eaux',
+    type: LayerType.ADMINISTRATIVE
+  },
+  [MonitorEnvLayers.FACADES]: {
+    code: 'facade_areas_subdivided',
+    name: 'Facades',
+    type: LayerType.ADMINISTRATIVE
+  },
+  [MonitorEnvLayers.DEPARTMENTS]: {
+    code: 'departments_areas',
+    name: 'Départements',
+    type: LayerType.ADMINISTRATIVE
+  },
+  [MonitorEnvLayers.REGULATORY_ENV]: {
+    code: MonitorEnvLayers.REGULATORY_ENV,
     name: 'environment_regulatory_areas',
-    showMultipleZonesInAdministrativeZones: false,
-    subZoneFieldKey: null,
     type: LayerType.REGULATORY,
     zIndex: 900
   },
-  REGULATORY_ENV_PREVIEW: {
-    code: 'REGULATORY_ENV_PREVIEW',
-    containsMultipleZones: false,
-    group: null,
-    isIntersectable: false,
+  [MonitorEnvLayers.AMP]: {
+    code: 'AMP',
+    name: 'environment_amp_areas',
+    type: LayerType.REGULATORY,
+    zIndex: 925
+  },
+  [MonitorEnvLayers.REGULATORY_ENV_PREVIEW]: {
+    code: MonitorEnvLayers.REGULATORY_ENV_PREVIEW,
     name: '',
-    showMultipleZonesInAdministrativeZones: false,
-    subZoneFieldKey: null,
-    type: null,
     zIndex: 950
   },
-  REPORTING_SELECTED: {
-    code: 'reporting_selected',
+  [MonitorEnvLayers.SELECTED_MISSION_TO_ATTACH_ON_REPORTING]: {
+    code: MonitorEnvLayers.SELECTED_MISSION_TO_ATTACH_ON_REPORTING,
+    zIndex: 1000
+  },
+  [MonitorEnvLayers.SELECTED_REPORTING_TO_ATTACH_ON_MISSION]: {
+    code: MonitorEnvLayers.SELECTED_REPORTING_TO_ATTACH_ON_MISSION,
+    zIndex: 1000
+  },
+  [MonitorEnvLayers.REPORTING_SELECTED]: {
+    code: MonitorEnvLayers.REPORTING_SELECTED,
     zIndex: 1025
   },
-  REPORTING_TO_ATTACH_ON_MISSION: {
-    code: 'reporting_to_attach_on_mission',
+  [MonitorEnvLayers.REPORTING_TO_ATTACH_ON_MISSION]: {
+    code: MonitorEnvLayers.REPORTING_TO_ATTACH_ON_MISSION,
     zIndex: 1025
   },
-  REPORTINGS: {
-    code: 'reportings',
+  [MonitorEnvLayers.MISSION_SELECTED]: {
+    code: MonitorEnvLayers.MISSION_SELECTED,
     zIndex: 1050
   },
-  SELECTED_MISSION_TO_ATTACH_ON_REPORTING: {
-    code: 'SELECTED_MISSION_TO_ATTACH_ON_REPORTING',
-    zIndex: 1000
+  [MonitorEnvLayers.MISSION_TO_ATTACH_ON_REPORTING]: {
+    code: MonitorEnvLayers.MISSION_TO_ATTACH_ON_REPORTING,
+    zIndex: 1050
   },
-  SELECTED_REPORTING_TO_ATTACH_ON_MISSION: {
-    code: 'SELECTED_REPORTING_TO_ATTACH_ON_MISSION',
-    zIndex: 1000
+  [MonitorEnvLayers.MISSIONS]: {
+    code: MonitorEnvLayers.MISSIONS,
+    zIndex: 1050
   },
-  SEMAPHORES: {
-    code: 'semaphores',
+  [MonitorEnvLayers.REPORTINGS]: {
+    code: MonitorEnvLayers.REPORTINGS,
+    zIndex: 1050
+  },
+  [MonitorEnvLayers.ACTIONS]: {
+    code: MonitorEnvLayers.ACTIONS,
+    zIndex: 1110
+  },
+  [MonitorEnvLayers.MEASUREMENT]: {
+    code: MonitorEnvLayers.MEASUREMENT,
+    name: '',
+    type: LayerType.MEASUREMENT,
+    zIndex: 1210
+  },
+  [MonitorEnvLayers.INTEREST_POINT]: {
+    code: MonitorEnvLayers.INTEREST_POINT,
+    zIndex: 1220
+  },
+  [MonitorEnvLayers.SEMAPHORES]: {
+    code: MonitorEnvLayers.SEMAPHORES,
     zIndex: 1300
   },
-  SIX_MILES: {
-    code: '6_miles_areas',
-    containsMultipleZones: false,
-    group: null,
-    isIntersectable: false,
-    name: '6 Milles',
-    showMultipleZonesInAdministrativeZones: false,
-    subZoneFieldKey: null,
-    type: LayerType.ADMINISTRATIVE
+  [MonitorEnvLayers.DRAW]: {
+    code: 'draw_layer',
+    zIndex: 1500
   },
-  STATIONS: {
-    code: 'stations',
+  [MonitorEnvLayers.STATIONS]: {
+    code: MonitorEnvLayers.STATIONS,
     // Station layer should always appear above all other layers
     zIndex: 2000
-  },
-  THREE_MILES: {
-    code: '3_miles_areas',
-    containsMultipleZones: false,
-    group: null,
-    isIntersectable: false,
-    name: '3 Milles',
-    showMultipleZonesInAdministrativeZones: false,
-    subZoneFieldKey: null,
-    type: LayerType.ADMINISTRATIVE
-  },
-  TWELVE_MILES: {
-    code: '12_miles_areas',
-    containsMultipleZones: false,
-    group: null,
-    isIntersectable: false,
-    name: '12 Milles',
-    showMultipleZonesInAdministrativeZones: false,
-    subZoneFieldKey: null,
-    type: LayerType.ADMINISTRATIVE
   }
 }
 
