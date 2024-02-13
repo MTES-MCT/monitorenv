a892188daade9662565f1aeb88321e6af9ebfe66
Claire Dagan <clairedagan@gmail.com>
2024-02-13 13:27:26 +0100

    [Tech] fix tests and api key variable

--- frontend/cypress/e2e/utils.ts
+++ frontend/cypress/e2e/utils.ts
@@ -19,22 +19,8 @@ export function visitSideWindow(isAutoSaveEnabled = 'true') {
       Object.defineProperty(window, 'EventSource', { value: EventSource })
       Object.defineProperty(window, 'mockEventSources', { value: sources })
 
-      if (!window.env) {
-        Object.defineProperty(window, 'env', {
-          value: {
-            REACT_APP_CYPRESS_TEST: true,
-            REACT_APP_MISSION_FORM_AUTO_SAVE_ENABLED: isAutoSaveEnabled,
-            REACT_APP_MISSION_FORM_AUTO_UPDATE: 'true'
-          }
-        })
-
-        return
-      }
-
-      // eslint-disable-next-line no-param-reassign
-      window.env.REACT_APP_MISSION_FORM_AUTO_SAVE_ENABLED = isAutoSaveEnabled
-      // eslint-disable-next-line no-param-reassign
-      window.env.REACT_APP_MISSION_FORM_AUTO_UPDATE = 'true'
+      Cypress.env('CYPRESS_MISSION_FORM_AUTO_SAVE_ENABLED', isAutoSaveEnabled)
+      Cypress.env('CYPRESS_MISSION_FORM_AUTO_UPDATE', 'true')
     }
   })
 }
