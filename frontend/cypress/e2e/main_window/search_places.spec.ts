d84addeab100c95122aee838205c0fd43e6e2124
Claire Dagan <clairedagan@gmail.com>
2024-02-21 12:00:55 +0100

    [Search] fix search places

---
+++ frontend/cypress/e2e/main_window/search_places.spec.ts
@@ -0,0 +1,14 @@
+import { FAKE_MAPBOX_RESPONSE } from '../constants'
+
+context('Search Places', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'https://api.mapbox.com/**', FAKE_MAPBOX_RESPONSE)
+    cy.visit('/#@-824534.42,6082993.21,8.70')
+  })
+
+  it('A user can search a place and zoom on it when it is selected', () => {
+    cy.fill('Rechercher un lieu', 'Nantes')
+    cy.get('.rs-auto-complete-item').first().click()
+    cy.url().should('include', '/#@-173695.64,5981051.92,12.96')
+  })
+})
