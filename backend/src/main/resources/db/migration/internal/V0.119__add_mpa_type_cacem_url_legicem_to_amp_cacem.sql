8176699986d0d534944523f87b5e8faf6e810389
Thomas Brosset <thomas.brosset@gmail.com>
2024-04-05 16:18:02 +0200

    add columns + cacem table + update queries

---
+++ backend/src/main/resources/db/migration/internal/V0.119__add_mpa_type_cacem_url_legicem_to_amp_cacem.sql
@@ -0,0 +1,3 @@
+ALTER TABLE amp_cacem 
+  ADD COLUMN mpa_type_cacem text,
+  ADD COLUMN url_legicem text;
--- datascience/src/pipeline/queries/cross/cacem/amp.sql
+++ datascience/src/pipeline/queries/cross/cacem/amp.sql
@@ -1,19 +1,24 @@
-SELECT id,
+SELECT amp.id,
 st_multi(ST_SimplifyPreserveTopology(ST_CurveToLine(geom), 0.00001)) geom,
 mpa_oriname,
 des_desigfr,
 mpa_type,
+mpa_type_cacem,
+url_legicem,
 md5(
-  coalesce(id::text,'')||
+  coalesce(amp.id::text,'')||
   coalesce(geom::text,'')||
   coalesce(mpa_oriname,'')||
   coalesce(des_desigfr,'')||
-  coalesce(mpa_type,'')
+  coalesce(mpa_type,'') ||
+  coalesce(mpa_type_cacem,'')||
+  coalesce(url_legicem,'')
   ) as row_hash
-	FROM prod."Aires marines protégées"
+	FROM prod."Aires marines protégées" amp
+    LEFT OUTER JOIN prod.amp_metadata_cacem ON (amp.id = amp_metadata_cacem.id)
   WHERE
     geom IS NOT NULL 
     AND st_isvalid(geom)
     AND mpa_oriname IS NOT NULL
     AND des_desigfr IS NOT NULL
-    AND id IN :ids
+    AND amp.id IN :ids
