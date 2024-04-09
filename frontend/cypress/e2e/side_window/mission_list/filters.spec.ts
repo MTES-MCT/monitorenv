54099b159afb6727d14446e17e3b9ee8d875811f
Claire Dagan <clairedagan@gmail.com>
2024-04-09 15:25:19 +0200

    [Test] update filter with envActions test

--- frontend/cypress/e2e/side_window/mission_list/filters.spec.ts
+++ frontend/cypress/e2e/side_window/mission_list/filters.spec.ts
@@ -220,6 +220,11 @@ context('Side Window > Mission List > Filter Bar', () => {
     cy.wait('@getMissions')
 
     cy.get('.Table-SimpleTable tr').should('have.length', 6)
+    cy.getDataCy('edit-mission-53').should('exist')
+    cy.getDataCy('edit-mission-38').should('exist')
+    cy.getDataCy('edit-mission-22').should('not.exist')
+    cy.getDataCy('edit-mission-43').should('not.exist')
+
     cy.fill('Missions avec actions env.', false)
   })
 })
