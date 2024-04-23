2b60815b053d106a2e94f3fe118865ef3e18a471
Claire Dagan <clairedagan@gmail.com>
2024-04-23 10:02:34 +0200

    [Tech] changes after code review

--- frontend/src/context/useMissionEventContext.ts
+++ frontend/src/context/useMissionEventContext.ts
@@ -3,10 +3,10 @@ import { useContext } from 'react'
 import { MissionEventContext } from './MissionEventContext'
 
 export const useMissionEventContext = () => {
-  const onboardingContext = useContext(MissionEventContext)
-  if (onboardingContext === undefined) {
-    throw new Error('useOnboardingContext must be inside a MissionEventContext')
+  const missionEventContext = useContext(MissionEventContext)
+  if (missionEventContext === undefined) {
+    throw new Error('useMissionEventContext must be inside a MissionEventContext')
   }
 
-  return onboardingContext
+  return missionEventContext
 }
