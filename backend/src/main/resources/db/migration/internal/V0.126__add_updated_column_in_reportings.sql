e0542a437174500b4fd97ff6d541bc3fe64c031b
Claire Dagan <clairedagan@gmail.com>
2024-05-08 10:52:45 +0200

    [Reporting] fix migration

--- backend/src/main/resources/db/migration/internal/V0.126__add_updated_column_in_reportings.sql
+++ backend/src/main/resources/db/migration/internal/V0.126__add_updated_column_in_reportings.sql
@@ -2,4 +2,4 @@ ALTER TABLE reportings
   ADD COLUMN updated_at_utc TIMESTAMP;
 
 -- Another statement is required so we don't update the existing rows with a default timestamp
-ALTER TABLE missions ALTER updated_at_utc SET DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE reportings ALTER updated_at_utc SET DEFAULT CURRENT_TIMESTAMP;
