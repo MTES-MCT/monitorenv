aa6c8a4c27cbe61514cb25a54996ad3b6021299d
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-05-13 14:56:19 +0200

    Add PATCH method in Backend CORS config

--- backend/src/main/kotlin/fr/gouv/cacem/monitorenv/config/WebSecurityConfig.kt
+++ backend/src/main/kotlin/fr/gouv/cacem/monitorenv/config/WebSecurityConfig.kt
@@ -27,7 +27,7 @@ class WebSecurityConfig {
     fun corsConfigurationSource(): CorsConfigurationSource {
         val configuration = CorsConfiguration().apply {
             allowedOrigins = listOf("*")
-            allowedMethods = listOf("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS")
+            allowedMethods = listOf("HEAD", "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             allowedHeaders = listOf("Authorization", "Cache-Control", "Content-Type")
         }
 
