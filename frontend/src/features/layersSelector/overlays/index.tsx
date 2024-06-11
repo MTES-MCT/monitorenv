4effcf381b2c37c22dff059f2165d14930e86f61
Claire Dagan <clairedagan@gmail.com>
2024-06-11 08:49:24 +0200

    [Map] don't show layers  (AMP and regulatory areas) overlay when draw modal is open

--- frontend/src/features/layersSelector/overlays/index.tsx
+++ frontend/src/features/layersSelector/overlays/index.tsx
@@ -1,20 +1,33 @@
 import { getHoveredItems } from '@features/map/utils'
+import { useAppDispatch } from '@hooks/useAppDispatch'
 import { useShallowEqualSelector } from '@hooks/useAppSelector'
+import { useHasMapInteraction } from '@hooks/useHasMapInteraction'
+import { useEffect } from 'react'
 import { createPortal } from 'react-dom'
 
 import { HoveredOverlay } from './HoveredOverlay'
 import { OverlayPositionOnCoordinates } from './OverlayPositionOnCoordinate'
 import { PinnedOverlay } from './PinnedOverlay'
+import { closeLayerOverlay } from '../metadataPanel/slice'
 
 import type { BaseMapChildrenProps } from '@features/map/BaseMap'
 
 export function LayersOverlay({ currentFeatureListOver, map, pixel }: BaseMapChildrenProps) {
+  const dispatch = useAppDispatch()
+
   const { layerOverlayCoordinates, layerOverlayIsOpen, layerOverlayItems } = useShallowEqualSelector(state => ({
     layerOverlayCoordinates: state.layersMetadata.layerOverlayCoordinates,
     layerOverlayIsOpen: state.layersMetadata.layerOverlayIsOpen,
     layerOverlayItems: state.layersMetadata.layerOverlayItems
   }))
   const hoveredItems = getHoveredItems(currentFeatureListOver)
+  const hasMapListerner = useHasMapInteraction()
+
+  useEffect(() => {
+    if (hasMapListerner) {
+      dispatch(closeLayerOverlay())
+    }
+  }, [hasMapListerner, dispatch])
 
   return (
     <>
@@ -26,7 +39,7 @@ export function LayersOverlay({ currentFeatureListOver, map, pixel }: BaseMapChi
         {layerOverlayIsOpen && <PinnedOverlay items={layerOverlayItems} />}
       </OverlayPositionOnCoordinates>
       {createPortal(
-        !layerOverlayIsOpen && hoveredItems && hoveredItems.length > 0 && pixel && (
+        !layerOverlayIsOpen && !hasMapListerner && hoveredItems && hoveredItems.length > 0 && pixel && (
           <HoveredOverlay items={hoveredItems} pixel={pixel} />
         ),
         document.body as HTMLElement
