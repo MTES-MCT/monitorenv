5d094d8c9def4fd2f5a424c65fd4a75533c190ee
Claire Dagan <clairedagan@gmail.com>
2024-07-01 14:11:06 +0200

    [Test] create tests for back

--- backend/src/test/kotlin/fr/gouv/cacem/monitorenv/domain/use_cases/vigilanceArea/GetVigilanceAreaByIdUTests.kt
+++ backend/src/test/kotlin/fr/gouv/cacem/monitorenv/domain/use_cases/vigilanceArea/GetVigilanceAreaByIdUTests.kt
@@ -1,14 +1,15 @@
 package fr.gouv.cacem.monitorenv.domain.use_cases.vigilanceArea
 
 import com.nhaarman.mockitokotlin2.given
-import com.nhaarman.mockitokotlin2.times
-import com.nhaarman.mockitokotlin2.verify
 import fr.gouv.cacem.monitorenv.domain.entities.vigilanceArea.VigilanceAreaEntity
 import fr.gouv.cacem.monitorenv.domain.repositories.IVigilanceAreaRepository
-import org.junit.jupiter.api.Test
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
+@ExtendWith(SpringExtension::class)
 class GetVigilanceAreaByIdUTests {
 
     @MockBean
@@ -16,20 +17,18 @@ class GetVigilanceAreaByIdUTests {
 
     @Test
     fun `execute should return vigilance area entity`() {
-
         val vigilanceAreaId = 10
         val expectedEntity = VigilanceAreaEntity(
             id = vigilanceAreaId,
-            comments= "Test Area",
-            isDeleted= false,
-            isDraft = true
+            comments = "Test Area",
+            isDeleted = false,
+            isDraft = true,
         )
 
         given(vigilanceAreaRepository.findById(vigilanceAreaId)).willReturn(expectedEntity)
+
         val result = GetVigilanceAreaById(vigilanceAreaRepository).execute(vigilanceAreaId)
 
-
-        verify(vigilanceAreaRepository, times(1)).findById(vigilanceAreaId)
         assertThat(expectedEntity).isEqualTo(result)
     }
 }
