b8fe43d662bd716e3c57e96fce2550924e9c5d76
Vincent <vincent.chery@m4x.org>
2024-07-08 11:15:19 +0200

    Remove grant on non-existing user

--- backend/src/main/resources/db/migration/internal/V0.138__metabase_upload_schema.sql
+++ backend/src/main/resources/db/migration/internal/V0.138__metabase_upload_schema.sql
@@ -4,4 +4,6 @@ CREATE ROLE metabase_uploads WITH LOGIN;
 
 GRANT USAGE ON SCHEMA metabase_uploads TO metabase_uploads;
 GRANT INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA "metabase_uploads" TO metabase_uploads;
-GRANT metabase_uploads TO metabase;
+
+-- A faire en prod à la mano :
+-- GRANT metabase_uploads TO metabase;
