af7815919930c5d56b60242e44896e410b3b115b
Claire Dagan <clairedagan@gmail.com>
2024-07-12 16:46:22 +0200

    [Tech] don't send token in headers when oidc is not enabled

--- frontend/src/api/utils/setAuthorizationHeaders.ts
+++ frontend/src/api/utils/setAuthorizationHeaders.ts
@@ -1,15 +1,17 @@
 import { sha256 } from '@utils/sha256'
+import { getOIDCConfig } from 'auth/getOIDCConfig'
 import { getOIDCUser } from 'auth/getOIDCUser'
 
 const AUTHORIZATION_HEADER = 'authorization'
 const CORRELATION_HEADER = 'X-Correlation-Id'
 
 export const setAuthorizationHeader = async headers => {
+  const { IS_OIDC_ENABLED } = getOIDCConfig()
   const user = getOIDCUser()
   const token = user?.access_token
 
   // If we have a token set in state, we pass it.
-  if (token) {
+  if (IS_OIDC_ENABLED && token) {
     headers.set(AUTHORIZATION_HEADER, `Bearer ${token}`)
 
     if (crypto?.subtle) {
