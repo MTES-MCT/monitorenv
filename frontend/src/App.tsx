da80ebc8d9ba40454860390475d4aca4fa4ccbb6
Claire Dagan <clairedagan@gmail.com>
2024-07-12 16:07:10 +0200

    [Tech] redirect to login page if not logged and add cors configuration in securityFilterChain

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -79,7 +79,7 @@ export function App() {
 
                     <Route
                       element={
-                        <RequireAuth>
+                        <RequireAuth redirect>
                           <HomePage />
                         </RequireAuth>
                       }
