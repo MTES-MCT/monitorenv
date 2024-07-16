3472cdc7b5c8dce21069e2d69da63d7a18893670
Maxime Perrault <maxperrault94@gmail.com>
2024-07-16 12:10:16 +0200

    ui: adeline review

--- frontend/cypress/e2e/side_window/mission_list/filters.spec.ts
+++ frontend/cypress/e2e/side_window/mission_list/filters.spec.ts
@@ -232,7 +232,7 @@ context('Side Window > Mission List > Filter Bar', () => {
   })
 
   it('Should filter missions by search query', () => {
-    cy.fill('Rechercher dans un contrôle (navire, personne morale ou physique)', 'BALTIK')
+    cy.fill('Rechercher dans un contrôle (navire, personne...)', 'BALTIK')
 
     cy.get('.Table-SimpleTable tr').should('have.length', 2)
     cy.getDataCy('edit-mission-34').click({ force: true })
