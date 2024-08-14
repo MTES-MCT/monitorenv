19c6c3075ebe71fc7decbc62fdab8ae71ab33fa8
Maxime Perrault <maxperrault94@gmail.com>
2024-08-14 18:11:03 +0200

    feat: add view all mission and add open mission buttons from control unit

--- frontend/src/features/ControlUnit/components/ControlUnitDialog/index.tsx
+++ frontend/src/features/ControlUnit/components/ControlUnitDialog/index.tsx
@@ -1,4 +1,10 @@
-import { Accent, Button, Icon, MapMenuDialog } from '@mtes-mct/monitor-ui'
+import { useGetMissionsQuery } from '@api/missionsAPI'
+import { sideWindowActions } from '@features/SideWindow/slice'
+import { Accent, Button, customDayjs, Icon, MapMenuDialog } from '@mtes-mct/monitor-ui'
+import { DateRangeEnum } from 'domain/entities/dateRange'
+import { sideWindowPaths } from 'domain/entities/sideWindow'
+import { MissionFiltersEnum, updateFilters } from 'domain/shared_slices/MissionFilters'
+import { editMissionInLocalStore } from 'domain/use_cases/missions/editMissionInLocalStore'
 import { Formik } from 'formik'
 import { noop } from 'lodash/fp'
 import { useCallback } from 'react'
@@ -40,6 +46,31 @@ export function ControlUnitDialog() {
     dispatch(mainWindowActions.setHasFullHeightRightDialogOpen(false))
   }, [dispatch])
 
+  const now = customDayjs().utc()
+  const { selectedMission } = useGetMissionsQuery(undefined, {
+    selectFromResult: ({ data }) => ({
+      selectedMission: data?.find(
+        mission =>
+          mission.controlUnits.some(unit => unit.id === controlUnit?.id) &&
+          now.isBetween(customDayjs(mission.startDateTimeUtc), customDayjs(mission.endDateTimeUtc))
+      )
+    }),
+    skip: !controlUnit
+  })
+
+  const openMission = (missionId: number) => {
+    dispatch(editMissionInLocalStore(missionId, 'map'))
+  }
+
+  const openFilteredMissions = (controlUnitId: number) => {
+    const sixMonthsAgo = customDayjs().subtract(6, 'month').toISOString()
+    dispatch(updateFilters({ key: MissionFiltersEnum.UNIT_FILTER, value: [controlUnitId] }))
+    dispatch(updateFilters({ key: MissionFiltersEnum.PERIOD_FILTER, value: DateRangeEnum.CUSTOM }))
+    dispatch(updateFilters({ key: MissionFiltersEnum.STARTED_AFTER_FILTER, value: sixMonthsAgo }))
+    dispatch(updateFilters({ key: MissionFiltersEnum.STARTED_BEFORE_FILTER, value: customDayjs().toISOString() }))
+    dispatch(sideWindowActions.focusAndGoTo(sideWindowPaths.MISSIONS))
+  }
+
   if (!controlUnit) {
     return (
       <MapMenuDialog.Container>
@@ -61,9 +92,31 @@ export function ControlUnitDialog() {
       </MapMenuDialog.Header>
       <Formik initialValues={controlUnit} onSubmit={noop}>
         <StyledMapMenuDialogBody>
-          <Button accent={Accent.SECONDARY} Icon={Icon.Plus} isFullWidth onClick={openNewMission}>
+          <Button accent={Accent.PRIMARY} Icon={Icon.Plus} isFullWidth onClick={openNewMission}>
             Créer une mission avec cette unité
           </Button>
+          <MissionControls>
+            {selectedMission && (
+              <Button
+                accent={Accent.SECONDARY}
+                Icon={Icon.MissionAction}
+                isFullWidth
+                onClick={() => openMission(selectedMission.id)}
+              >
+                Ouvrir la mission en cours
+              </Button>
+            )}
+            {/* TODO: Mettre la bonne icone dans monitor-ui et ici */}
+            <Button
+              accent={Accent.SECONDARY}
+              Icon={Icon.List}
+              isFullWidth
+              onClick={() => openFilteredMissions(controlUnit.id)}
+            >
+              Voir les missions de l&apos;unité
+            </Button>
+          </MissionControls>
+
           <ControlUnitContactList controlUnit={controlUnit} onSubmit={updateControlUnit} />
           <ControlUnitResourceList controlUnit={controlUnit} />
           <AreaNote controlUnit={controlUnit} onSubmit={updateControlUnit} />
@@ -96,4 +149,10 @@ const StyledMapMenuDialogBody = styled(MapMenuDialog.Body)`
   > div:not(:first-child) {
     margin-top: 12px;
   }
+`
+
+const MissionControls = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
 `
