5b8d38f6720ba36118b260324e904d9d6cb02a18
Maxime Perrault <maxperrault94@gmail.com>
2024-08-20 10:53:11 +0200

    fix: add env variable to init_env.sh

--- infra/init/init_env.sh
+++ infra/init/init_env.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
+export $(grep -v '^#' $HOME/monitorenv/.env | xargs);
 cat << EOT
-            ###########################           
-           ##                        ###          
+            ###########################
+           ##                        ###
          ###                           ##          MONITORENV VERSION        : $MONITORENV_VERSION
-        ###     ###################     ###       
+        ###     ###################     ###
       ###     ###                 ##      ##       MONITORENV_LOGS_FOLDER    : $MONITORENV_LOGS_FOLDER
      ###     ###                   ###     ###     MONITORENV_BACKUPS_FOLDER : $MONITORENV_BACKUPS_FOLDER
     ##      ##          ###          ##      ##    MONITORENV_LOGS_AND_BACKUPS_GID : $MONITORENV_LOGS_AND_BACKUPS_GID
-  ###     ###        ###   ###        ###     ### 
+  ###     ###        ###   ###        ###     ###
  ##      ##         ##      ##         ###     ### POSTGRES_DB       : $POSTGRES_DB
                      ###   ###        ##      ##   POSTGRES_USER     : $POSTGRES_USER
             %%          ##          ###     ###    POSTGRES_PASSWORD : ***SECRET***
-        %%%%  %%%       ##         ###     ###    
-        %%      %%      ##       ###      ##      
-        %%%   %%%       ###########     ###       
+        %%%%  %%%       ##         ###     ###
+        %%      %%      ##       ###      ##
+        %%%   %%%       ###########     ###
            %%%          ##             ##         commandes principales:
       %%    %%          ##           ###            Run : make restart-app
       %%%%%%%%          ##############              Logs: make logs-backend / make logs-geoserver / make logs-db
