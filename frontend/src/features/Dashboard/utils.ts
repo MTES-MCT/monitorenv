3d7fc1dc3692882f4f767fd96182ea10b73bb92f
Maxime Perrault <maxperrault94@gmail.com>
2024-09-17 16:46:14 +0200

    tech: fix cypress env variable

--- frontend/src/features/Dashboard/utils.ts
+++ frontend/src/features/Dashboard/utils.ts
@@ -2,5 +2,5 @@ import { isCypress } from '@utils/isCypress'
 
 export const isDashboardEnabled = () =>
   isCypress()
-    ? window.Cypress.env('CYPRESS_FRONTEND_DASHBOARD') === 'true'
+    ? window.Cypress.env('CYPRESS_FRONTEND_DASHBOARD_ENABLED') === 'true'
     : import.meta.env.FRONTEND_DASHBOARD_ENABLED === 'true'
