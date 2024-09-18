0586b4aacb64932bbb5df0b6d5ba2d95457137e8
Maxime Perrault <166825146+maximeperraultdev@users.noreply.github.com>
2024-09-18 16:08:59 +0200

    Revert "tech: fix cypress env variable"
    This reverts commit 2a0093341165d07eb05514817d0fe9afd09b981b.

--- frontend/src/features/Dashboard/utils.ts
+++ frontend/src/features/Dashboard/utils.ts
@@ -2,5 +2,5 @@ import { isCypress } from '@utils/isCypress'
 
 export const isDashboardEnabled = () =>
   isCypress()
-    ? window.Cypress.env('CYPRESS_FRONTEND_DASHBOARD_ENABLED') === 'true'
+    ? window.Cypress.env('CYPRESS_FRONTEND_DASHBOARD') === 'true'
     : import.meta.env.FRONTEND_DASHBOARD_ENABLED === 'true'
