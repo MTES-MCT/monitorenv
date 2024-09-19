14b7947766292fe58ca7c9d73eb5ba8c6f3e5fe4
Claire Dagan <clairedagan@gmail.com>
2024-09-19 16:21:30 +0200

    [Dashboard] create Accordion component

---
+++ frontend/src/features/Dashboard/components/DashboardForm/Accordion.tsx
@@ -0,0 +1,67 @@
+import { Accent, Icon, IconButton, Size } from '@mtes-mct/monitor-ui'
+import styled from 'styled-components'
+
+type AccordionProps = {
+  children: React.ReactNode
+  headerButton?: React.ReactNode
+  isExpanded: boolean
+  setExpandedAccordion: () => void
+  title: string
+}
+
+export function Accordion({ children, headerButton, isExpanded, setExpandedAccordion, title }: AccordionProps) {
+  return (
+    <AccordionContainer $withCursor={!headerButton} onClick={!headerButton ? setExpandedAccordion : undefined}>
+      <AccordionHeader>
+        <TitleContainer>
+          <Title>{title}</Title>
+          {headerButton}
+        </TitleContainer>
+        <StyledIconButton
+          $isExpanded={isExpanded}
+          accent={Accent.TERTIARY}
+          Icon={Icon.Chevron}
+          onClick={setExpandedAccordion}
+          size={Size.SMALL}
+        />
+      </AccordionHeader>
+      <HeaderSeparator />
+      <AccordionContent $isExpanded={isExpanded}>{children}</AccordionContent>
+    </AccordionContainer>
+  )
+}
+
+const AccordionContainer = styled.div<{ $withCursor: boolean }>`
+  box-shadow: 0px 3px 6px #70778540;
+  cursor: ${({ $withCursor }) => ($withCursor ? 'pointer' : 'default')};
+`
+const StyledIconButton = styled(IconButton)<{ $isExpanded: boolean }>`
+  transform: ${({ $isExpanded }) => ($isExpanded ? 'rotate(180deg)' : 'rotate(0deg)')};
+  transition: transform 0.3s;
+`
+const AccordionHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 24px;
+`
+const TitleContainer = styled.div`
+  align-items: center;
+  display: flex;
+  gap: 16px;
+`
+const Title = styled.span`
+  font-size: 16px;
+  font-weight: 500;
+`
+
+const HeaderSeparator = styled.div`
+  border-bottom: 2px solid ${p => p.theme.color.gainsboro};
+  padding: -24px;
+`
+const AccordionContent = styled.div<{ $isExpanded: boolean }>`
+  display: flex;
+  flex-direction: column;
+  max-height: ${({ $isExpanded }) => ($isExpanded ? '535px' : '0px')};
+  overflow: hidden;
+  transition: 0.5s max-height;
+`
