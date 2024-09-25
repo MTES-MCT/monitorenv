e397d6464e5661ea23b960ac803dd338b0455b6a
Maxime Perrault <maxperrault94@gmail.com>
2024-09-25 18:24:25 +0200

    fix: add control plans to envaction saving usecase

--- backend/src/main/kotlin/fr/gouv/cacem/monitorenv/infrastructure/database/repositories/JpaEnvActionRepository.kt
+++ backend/src/main/kotlin/fr/gouv/cacem/monitorenv/infrastructure/database/repositories/JpaEnvActionRepository.kt
@@ -5,7 +5,13 @@ import fr.gouv.cacem.monitorenv.domain.entities.mission.envAction.EnvActionEntit
 import fr.gouv.cacem.monitorenv.domain.exceptions.BackendUsageErrorCode
 import fr.gouv.cacem.monitorenv.domain.exceptions.BackendUsageException
 import fr.gouv.cacem.monitorenv.domain.repositories.IEnvActionRepository
+import fr.gouv.cacem.monitorenv.infrastructure.database.model.ControlPlanSubThemeModel
+import fr.gouv.cacem.monitorenv.infrastructure.database.model.ControlPlanTagModel
+import fr.gouv.cacem.monitorenv.infrastructure.database.model.ControlPlanThemeModel
 import fr.gouv.cacem.monitorenv.infrastructure.database.model.EnvActionModel
+import fr.gouv.cacem.monitorenv.infrastructure.database.repositories.interfaces.IDBControlPlanSubThemeRepository
+import fr.gouv.cacem.monitorenv.infrastructure.database.repositories.interfaces.IDBControlPlanTagRepository
+import fr.gouv.cacem.monitorenv.infrastructure.database.repositories.interfaces.IDBControlPlanThemeRepository
 import fr.gouv.cacem.monitorenv.infrastructure.database.repositories.interfaces.IDBEnvActionRepository
 import fr.gouv.cacem.monitorenv.infrastructure.database.repositories.interfaces.IDBMissionRepository
 import org.springframework.data.repository.findByIdOrNull
@@ -16,6 +22,9 @@ import java.util.UUID
 class JpaEnvActionRepository(
     private val idbEnvActionRepository: IDBEnvActionRepository,
     private val idbMissionRepository: IDBMissionRepository,
+    private val idbControlPlanThemeRepository: IDBControlPlanThemeRepository,
+    private val idbControlPlanSubThemeRepository: IDBControlPlanSubThemeRepository,
+    private val idbControlPlanTagRepository: IDBControlPlanTagRepository,
     private val objectMapper: ObjectMapper,
 ) : IEnvActionRepository {
     override fun findById(id: UUID): EnvActionEntity? {
@@ -23,15 +32,33 @@ class JpaEnvActionRepository(
     }
 
     override fun save(envAction: EnvActionEntity): EnvActionEntity {
-        envAction.missionId?.let {
-            val mission = idbMissionRepository.getReferenceById(it)
+        envAction.missionId?.let { missionId ->
+            val mission = idbMissionRepository.getReferenceById(missionId)
+            val controlPlanThemesReferenceModelMap: MutableMap<Int, ControlPlanThemeModel> = mutableMapOf()
+            val controlPlanTagssReferenceModelMap: MutableMap<Int, ControlPlanTagModel> = mutableMapOf()
+            val controlPlanSubThemesReferenceModelMap: MutableMap<Int, ControlPlanSubThemeModel> = mutableMapOf()
+
+
+            envAction.controlPlans?.forEach { controlPlan ->
+                controlPlan.tagIds?.forEach { tagId ->
+                    controlPlanTagssReferenceModelMap[tagId] = idbControlPlanTagRepository.getReferenceById(tagId)
+                }
+                controlPlan.themeId?.let { themeId ->
+                    controlPlanThemesReferenceModelMap[themeId] =
+                        idbControlPlanThemeRepository.getReferenceById(themeId)
+                }
+                controlPlan.subThemeIds?.forEach { subthemeId ->
+                    controlPlanSubThemesReferenceModelMap[subthemeId] =
+                        idbControlPlanSubThemeRepository.getReferenceById(subthemeId)
+                }
+            }
             return idbEnvActionRepository.save(
                 EnvActionModel.fromEnvActionEntity(
                     envAction,
                     mission = mission,
-                    controlPlanThemesReferenceModelMap = mapOf(),
-                    controlPlanTagsReferenceModelMap = mapOf(),
-                    controlPlanSubThemesReferenceModelMap = mapOf(),
+                    controlPlanThemesReferenceModelMap = controlPlanThemesReferenceModelMap,
+                    controlPlanTagsReferenceModelMap = controlPlanTagssReferenceModelMap,
+                    controlPlanSubThemesReferenceModelMap = controlPlanSubThemesReferenceModelMap,
                     mapper = objectMapper,
                 ),
             ).toActionEntity(objectMapper)
