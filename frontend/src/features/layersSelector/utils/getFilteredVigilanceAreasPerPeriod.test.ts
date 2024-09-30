620bba7bcaa1db150c11b4571442361fb26607cd
Maxime Perrault <maxperrault94@gmail.com>
2024-09-30 10:37:12 +0200

    fix: vigilance area period test

--- frontend/src/features/layersSelector/utils/getFilteredVigilanceAreasPerPeriod.test.ts
+++ frontend/src/features/layersSelector/utils/getFilteredVigilanceAreasPerPeriod.test.ts
@@ -5,52 +5,60 @@ import { customDayjs } from '@mtes-mct/monitor-ui'
 import { getFilterVigilanceAreasPerPeriod } from './getFilteredVigilanceAreasPerPeriod'
 
 describe('filterVigilanceAreas', () => {
-  const areas = [
-    {
-      computedEndDate: `${customDayjs().add(1, 'year').format('YYYY-MM-DD')} 23:59:59.99999`,
-      endDatePeriod: `${customDayjs().format('YYYY-MM-DD')} 23:59:59.99999`,
-      endingCondition: VigilanceArea.EndingCondition.END_DATE,
-      endingOccurrenceDate: `${customDayjs().add(1, 'year').format('YYYY-MM-DD')} 23:59:59.00000`,
-      endingOccurrencesNumber: undefined,
-      frequency: VigilanceArea.Frequency.ALL_MONTHS,
-      id: 1,
-      startDatePeriod: `${customDayjs().format('YYYY-MM-DD')} 00:00:00.00000`
-    },
-    {
-      computedEndDate: `${customDayjs().add(18, 'days').format('YYYY-MM-DD')} 23:59:59.99999`,
-      endDatePeriod: `${customDayjs().add(4, 'days').format('YYYY-MM-DD')} 00:00:00.00000`,
-      endingCondition: VigilanceArea.EndingCondition.OCCURENCES_NUMBER,
-      endingOccurrenceDate: undefined,
-      endingOccurrencesNumber: 3,
-      frequency: VigilanceArea.Frequency.ALL_WEEKS,
-      id: 2,
-      startDatePeriod: `${customDayjs().subtract(3, 'days').format('YYYY-MM-DD')} 00:00:00.00000`
-    },
-    {
-      computedEndDate: undefined,
-      endDatePeriod: `${customDayjs().add(24, 'days').format('YYYY-MM-DD')} 23:59:59.99999`,
-      endingCondition: VigilanceArea.EndingCondition.NEVER,
-      endingOccurrenceDate: undefined,
-      endingOccurrencesNumber: undefined,
-      frequency: VigilanceArea.Frequency.ALL_YEARS,
-      id: 3,
-      startDatePeriod: `${customDayjs().add(3, 'days').format('YYYY-MM-DD')} 00:00:00.00000`
-    },
-    {
-      computedEndDate: `${customDayjs().add(2, 'year').add(4, 'days').format('YYYY-MM-DD')} 23:59:59.99999`,
-      endDatePeriod: `${customDayjs()
-        .add(1, 'year')
-        .add(4, 'months')
-        .add(7, 'days')
-        .format('YYYY-MM-DD')} 23:59:59.99999`,
-      endingCondition: VigilanceArea.EndingCondition.OCCURENCES_NUMBER,
-      endingOccurrenceDate: undefined,
-      endingOccurrencesNumber: 36,
-      frequency: VigilanceArea.Frequency.ALL_WEEKS,
-      id: 4,
-      startDatePeriod: `${customDayjs().add(1, 'year').add(4, 'months').format('YYYY-MM-DD')} 00:00:00.00000`
-    }
-  ]
+  const today = {
+    computedEndDate: `${customDayjs().add(1, 'year').format('YYYY-MM-DD')} 23:59:59.99999`,
+    endDatePeriod: `${customDayjs().format('YYYY-MM-DD')} 23:59:59.99999`,
+    endingCondition: VigilanceArea.EndingCondition.END_DATE,
+    endingOccurrenceDate: `${customDayjs().add(1, 'year').format('YYYY-MM-DD')} 23:59:59.00000`,
+    endingOccurrencesNumber: undefined,
+    frequency: VigilanceArea.Frequency.ALL_MONTHS,
+    id: 1,
+    startDatePeriod: `${customDayjs().format('YYYY-MM-DD')} 00:00:00.00000`
+  }
+  const quarter = {
+    computedEndDate: `${customDayjs().startOf('quarter').add(3, 'weeks').format('YYYY-MM-DD')} 23:59:59.99999`,
+    endDatePeriod: `${customDayjs().endOf('quarter').format('YYYY-MM-DD')} 00:00:00.00000`,
+    endingCondition: VigilanceArea.EndingCondition.OCCURENCES_NUMBER,
+    endingOccurrenceDate: undefined,
+    endingOccurrencesNumber: 3,
+    frequency: VigilanceArea.Frequency.ALL_WEEKS,
+    id: 2,
+    startDatePeriod: `${customDayjs().startOf('quarter').format('YYYY-MM-DD')} 00:00:00.00000`
+  }
+  const outsideFilteredDate = {
+    computedEndDate: `${customDayjs().add(2, 'year').add(4, 'days').format('YYYY-MM-DD')} 23:59:59.99999`,
+    endDatePeriod: `${customDayjs()
+      .add(1, 'year')
+      .add(4, 'months')
+      .add(7, 'days')
+      .format('YYYY-MM-DD')} 23:59:59.99999`,
+    endingCondition: VigilanceArea.EndingCondition.OCCURENCES_NUMBER,
+    endingOccurrenceDate: undefined,
+    endingOccurrencesNumber: 36,
+    frequency: VigilanceArea.Frequency.ALL_WEEKS,
+    id: 4,
+    startDatePeriod: `${customDayjs().add(1, 'year').add(4, 'months').format('YYYY-MM-DD')} 00:00:00.00000`
+  }
+  const year = {
+    computedEndDate: undefined,
+    endDatePeriod: `${customDayjs()
+      .endOf('quarter')
+      .add(3, 'months')
+      .add(1, 'days')
+      .format('YYYY-MM-DD')} 00:00:00.00000`,
+    endingCondition: VigilanceArea.EndingCondition.NEVER,
+    endingOccurrenceDate: undefined,
+    endingOccurrencesNumber: undefined,
+    frequency: VigilanceArea.Frequency.ALL_YEARS,
+    id: 3,
+    startDatePeriod: `${customDayjs()
+      .startOf('quarter')
+      .add(3, 'months')
+      .add(1, 'days')
+      .format('YYYY-MM-DD')} 00:00:00.00000`
+  }
+
+  const areas = [today, quarter, year, outsideFilteredDate]
 
   it('filters areas for today', () => {
     const result = getFilterVigilanceAreasPerPeriod(
@@ -58,33 +66,33 @@ describe('filterVigilanceAreas', () => {
       VigilanceArea.VigilanceAreaFilterPeriod.AT_THE_MOMENT,
       undefined
     )
-    expect(result).toEqual([areas[0], areas[1]])
+    expect(result).toEqual([today, quarter])
   })
 
-  it('filters areas for current quarter', () => {
+  it('filters areas within current quarter', () => {
     const result = getFilterVigilanceAreasPerPeriod(
       areas,
       VigilanceArea.VigilanceAreaFilterPeriod.CURRENT_QUARTER,
       undefined
     )
-    expect(result).toEqual([areas[0], areas[1], areas[2]])
+    expect(result).toEqual([today, quarter])
   })
 
-  it('filters areas for current year', () => {
+  it('filters areas within current year', () => {
     const result = getFilterVigilanceAreasPerPeriod(
       areas,
       VigilanceArea.VigilanceAreaFilterPeriod.CURRENT_YEAR,
       undefined
     )
-    expect(result).toEqual([areas[0], areas[1], areas[2]])
+    expect(result).toEqual([today, quarter, year])
   })
 
-  it('filters areas for next three months', () => {
+  it('filters areas within next three months', () => {
     const result = getFilterVigilanceAreasPerPeriod(
       areas,
       VigilanceArea.VigilanceAreaFilterPeriod.NEXT_THREE_MONTHS,
       undefined
     )
-    expect(result).toEqual([areas[0], areas[1], areas[2]])
+    expect(result).toEqual([today, quarter, year])
   })
 })
