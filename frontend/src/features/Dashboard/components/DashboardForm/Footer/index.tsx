7aeb5cc2d8b46e8a26cabb7792703e46ba570e49
Maxime Perrault <maxperrault94@gmail.com>
2024-10-15 09:10:23 +0200

    review: add confirmation modal

--- frontend/src/features/Dashboard/components/DashboardForm/Footer/index.tsx
+++ frontend/src/features/Dashboard/components/DashboardForm/Footer/index.tsx
@@ -1,30 +1,66 @@
 import { saveDashboard } from '@features/Dashboard/useCases/saveDashboard'
 import { useAppDispatch } from '@hooks/useAppDispatch'
 import { useAppSelector } from '@hooks/useAppSelector'
-import { Accent, Button, Icon } from '@mtes-mct/monitor-ui'
+import { Accent, Button, Dialog, Icon, TextInput, THEME } from '@mtes-mct/monitor-ui'
+import { useState } from 'react'
 import styled from 'styled-components'
 
 export function Footer() {
   const dispatch = useAppDispatch()
+
   const activeDashboardId = useAppSelector(state => state.dashboard.activeDashboardId)
   const dashboardForm = useAppSelector(state =>
     activeDashboardId ? state.dashboard.dashboards[activeDashboardId] : undefined
   )
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [updatedName, setUpdatedName] = useState<string | undefined>(dashboardForm?.dashboard.name)
   if (!dashboardForm) {
     return null
   }
 
+  const save = () => {
+    dispatch(saveDashboard({ ...dashboardForm.dashboard, name: updatedName ?? dashboardForm.dashboard.name }))
+    setIsModalOpen(false)
+  }
+
   return (
-    <Wrapper>
-      <SaveButton
-        accent={Accent.SECONDARY}
-        disabled={!activeDashboardId}
-        Icon={Icon.Save}
-        onClick={() => dispatch(saveDashboard(dashboardForm.dashboard))}
-      >
-        Enregistrer le tableau
-      </SaveButton>
-    </Wrapper>
+    <>
+      {isModalOpen && (
+        <Dialog isAbsolute>
+          <Dialog.Title as="h2">Enregistrer le tableau de bord</Dialog.Title>
+          <Dialog.Body $color={THEME.color.gunMetal}>
+            <StyledDialogMessage>
+              Voulez-vous modifier le nom par défaut du tableau de bord avant de l&apos;enregistrer ?
+            </StyledDialogMessage>
+            <StyledTextInput
+              isLabelHidden
+              label="Nom du tableau de bord"
+              name="name"
+              onChange={value => setUpdatedName(value)}
+              value={updatedName}
+            />
+          </Dialog.Body>
+          <StyledDialogActions>
+            <Button accent={Accent.SECONDARY} onClick={() => setIsModalOpen(false)}>
+              Annuler
+            </Button>
+            <Button Icon={Icon.Save} onClick={save}>
+              Enregistrer
+            </Button>
+          </StyledDialogActions>
+        </Dialog>
+      )}
+      <Wrapper>
+        <SaveButton
+          accent={Accent.SECONDARY}
+          disabled={!activeDashboardId}
+          Icon={Icon.Save}
+          onClick={() => setIsModalOpen(true)}
+        >
+          Enregistrer le tableau
+        </SaveButton>
+      </Wrapper>
+    </>
   )
 }
 
@@ -39,4 +75,18 @@ const Wrapper = styled.div`
 
 const SaveButton = styled(Button)`
   float: right;
+`
+
+const StyledTextInput = styled(TextInput)`
+  margin-top: 24px;
+  width: 360px;
+  margin: 24px auto 0;
+`
+
+const StyledDialogMessage = styled.p`
+  font-size: 16px;
+`
+
+const StyledDialogActions = styled(Dialog.Action)`
+  align-items: stretch;
 `
