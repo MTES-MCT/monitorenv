965aa4378a2656c0e8a6ba7bceb5169c6c5ab9ae
Maxime Perrault <maxperrault94@gmail.com>
2024-10-17 17:21:39 +0200

    feat: review: add typing and refacto layer to avoid undefined dashboard

--- frontend/src/features/Dashboard/components/Layers/index.tsx
+++ frontend/src/features/Dashboard/components/Layers/index.tsx
@@ -1,7 +1,7 @@
 import { useGetAMPsQuery } from '@api/ampsAPI'
 import { useGetRegulatoryLayersQuery } from '@api/regulatoryLayersAPI'
 import { useGetVigilanceAreasQuery } from '@api/vigilanceAreasAPI'
-import { getDashboardById, type DashboardType } from '@features/Dashboard/slice'
+import { getDashboardById } from '@features/Dashboard/slice'
 import { getAMPFeature } from '@features/map/layers/AMP/AMPGeometryHelpers'
 import { getAMPLayerStyle } from '@features/map/layers/AMP/AMPLayers.style'
 import { getRegulatoryFeature } from '@features/map/layers/Regulatory/regulatoryGeometryHelpers'
@@ -30,9 +30,6 @@ export function DashboardLayer({ map }: BaseMapChildrenProps) {
   const displayDashboardLayer = useAppSelector(state => state.global.displayDashboardLayer)
 
   const activeDashboardId = useAppSelector(state => state.dashboard.activeDashboardId)
-  const openPanel = useAppSelector(state =>
-    activeDashboardId ? state.dashboard.dashboards?.[activeDashboardId]?.openPanel : undefined
-  )
 
   const { data: regulatoryLayers } = useGetRegulatoryLayersQuery()
   const { data: ampLayers } = useGetAMPsQuery()
@@ -41,11 +38,11 @@ export function DashboardLayer({ map }: BaseMapChildrenProps) {
   const geom = useAppSelector(state =>
     activeDashboardId ? state.dashboard.dashboards?.[activeDashboardId]?.dashboard.geom : undefined
   )
-
-  const dashboard: DashboardType = useAppSelector(state => getDashboardById(state, activeDashboardId))
+  const dashboard = useAppSelector(state => getDashboardById(state.dashboard, activeDashboardId))
+  const openPanel = dashboard?.openPanel
   const activeDashboard = dashboard?.dashboard
 
-  const isLayerVisible = displayDashboardLayer && !!dashboard
+  const isLayerVisible = displayDashboardLayer
 
   const layersVectorSourceRef = useRef(new VectorSource()) as React.MutableRefObject<VectorSource<Feature<Geometry>>>
   const layersVectorLayerRef = useRef(
@@ -64,89 +61,91 @@ export function DashboardLayer({ map }: BaseMapChildrenProps) {
     if (map) {
       layersVectorSourceRef.current.clear(true)
 
-      // Regulatory Areas
-      if (regulatoryLayers?.entities && activeDashboard) {
-        let regulatoryLayersIds = [...activeDashboard.regulatoryAreas]
-        const openPanelIsRegulatory = openPanel?.type === Dashboard.Block.REGULATORY_AREAS
-        // we don't want to display the area twice
-        if (openPanelIsRegulatory) {
-          regulatoryLayersIds = [...activeDashboard.regulatoryAreas]?.filter(id => id !== openPanel?.id)
-        }
-        const features = regulatoryLayersIds.reduce((feats: Feature[], layerId) => {
-          const layer = regulatoryLayers.entities[layerId]
-
-          if (layer && layer?.geom && layer?.geom?.coordinates.length > 0) {
-            const feature = getRegulatoryFeature({ code: Dashboard.featuresCode.DASHBOARD_REGULATORY_AREAS, layer })
-            feature.setStyle(getRegulatoryLayerStyle(feature))
-            feats.push(feature)
+      if (activeDashboard) {
+        // Regulatory Areas
+        if (regulatoryLayers?.entities) {
+          let regulatoryLayersIds = activeDashboard.regulatoryAreas
+          const openPanelIsRegulatory = openPanel?.type === Dashboard.Block.REGULATORY_AREAS
+          // we don't want to display the area twice
+          if (openPanelIsRegulatory) {
+            regulatoryLayersIds = regulatoryLayersIds.filter(id => id !== openPanel?.id)
           }
+          const features = regulatoryLayersIds.reduce((feats: Feature[], layerId) => {
+            const layer = regulatoryLayers.entities[layerId]
 
-          return feats
-        }, [])
+            if (layer && layer?.geom && layer?.geom?.coordinates.length > 0) {
+              const feature = getRegulatoryFeature({ code: Dashboard.featuresCode.DASHBOARD_REGULATORY_AREAS, layer })
+              feature.setStyle(getRegulatoryLayerStyle(feature))
+              feats.push(feature)
+            }
 
-        layersVectorSourceRef.current.addFeatures(features)
-      }
+            return feats
+          }, [])
 
-      // AMP
-      if (ampLayers?.entities && activeDashboard) {
-        let ampLayerIds = activeDashboard.amps
-        const openPanelIsRegulatory = openPanel?.type === Dashboard.Block.AMP
-        // we don't want to display the area twice
-        if (openPanelIsRegulatory) {
-          ampLayerIds = activeDashboard.amps?.filter(id => id !== openPanel?.id)
+          layersVectorSourceRef.current.addFeatures(features)
         }
 
-        const features = ampLayerIds?.reduce((feats: Feature[], layerId) => {
-          const layer = ampLayers.entities[layerId]
-
-          if (layer && layer?.geom && layer?.geom?.coordinates.length > 0) {
-            const feature = getAMPFeature({ code: Dashboard.featuresCode.DASHBOARD_AMP, layer })
-            feature.setStyle(getAMPLayerStyle(feature))
-
-            feats.push(feature)
+        // AMP
+        if (ampLayers?.entities) {
+          let ampLayerIds = activeDashboard.amps
+          const openPanelIsRegulatory = openPanel?.type === Dashboard.Block.AMP
+          // we don't want to display the area twice
+          if (openPanelIsRegulatory) {
+            ampLayerIds = ampLayerIds.filter(id => id !== openPanel?.id)
           }
 
-          return feats
-        }, [])
+          const features = ampLayerIds?.reduce((feats: Feature[], layerId) => {
+            const layer = ampLayers.entities[layerId]
 
-        layersVectorSourceRef.current.addFeatures(features)
-      }
+            if (layer && layer?.geom && layer?.geom?.coordinates.length > 0) {
+              const feature = getAMPFeature({ code: Dashboard.featuresCode.DASHBOARD_AMP, layer })
+              feature.setStyle(getAMPLayerStyle(feature))
 
-      // Vigilance Areas
-      if (vigilanceAreas?.entities && activeDashboard) {
-        let vigilanceAreaLayersIds = [...activeDashboard.vigilanceAreas]
-        const openPanelIsVigilanceArea = openPanel?.type === Dashboard.Block.VIGILANCE_AREAS
-        // we don't want to display the area twice
-        if (openPanelIsVigilanceArea) {
-          vigilanceAreaLayersIds = [...activeDashboard.vigilanceAreas]?.filter(id => id !== openPanel?.id)
+              feats.push(feature)
+            }
+
+            return feats
+          }, [])
+
+          layersVectorSourceRef.current.addFeatures(features)
         }
-        const features = vigilanceAreaLayersIds?.reduce((feats: Feature[], layerId) => {
-          const layer = vigilanceAreas.entities[layerId]
-          if (layer && layer?.geom && layer?.geom?.coordinates.length > 0) {
-            const feature = getVigilanceAreaZoneFeature(layer, Dashboard.featuresCode.DASHBOARD_VIGILANCE_AREAS)
-            feature.setStyle(getVigilanceAreaLayerStyle(feature))
-            feats.push(feature)
+
+        // Vigilance Areas
+        if (vigilanceAreas?.entities) {
+          let vigilanceAreaLayersIds = activeDashboard.vigilanceAreas
+          const openPanelIsVigilanceArea = openPanel?.type === Dashboard.Block.VIGILANCE_AREAS
+          // we don't want to display the area twice
+          if (openPanelIsVigilanceArea) {
+            vigilanceAreaLayersIds = vigilanceAreaLayersIds.filter(id => id !== openPanel?.id)
           }
+          const features = vigilanceAreaLayersIds.reduce((feats: Feature[], layerId) => {
+            const layer = vigilanceAreas.entities[layerId]
+            if (layer && layer?.geom && layer?.geom?.coordinates.length > 0) {
+              const feature = getVigilanceAreaZoneFeature(layer, Dashboard.featuresCode.DASHBOARD_VIGILANCE_AREAS)
+              feature.setStyle(getVigilanceAreaLayerStyle(feature))
+              feats.push(feature)
+            }
 
-          return feats
-        }, [])
+            return feats
+          }, [])
 
-        layersVectorSourceRef.current.addFeatures(features)
-      }
+          layersVectorSourceRef.current.addFeatures(features)
+        }
 
-      // Reportings
-      if (activeDashboard?.reportings) {
-        const features = activeDashboard.reportings.reduce((feats: Feature[], reporting) => {
-          if (reporting.geom) {
-            const feature = getReportingZoneFeature(reporting, Dashboard.featuresCode.DASHBOARD_REPORTINGS)
-            feature.setStyle(editingReportingStyleFn)
-            feats.push(feature)
-          }
+        // Reportings
+        if (activeDashboard.reportings) {
+          const features = activeDashboard.reportings.reduce((feats: Feature[], reporting) => {
+            if (reporting.geom) {
+              const feature = getReportingZoneFeature(reporting, Dashboard.featuresCode.DASHBOARD_REPORTINGS)
+              feature.setStyle(editingReportingStyleFn)
+              feats.push(feature)
+            }
 
-          return feats
-        }, [])
+            return feats
+          }, [])
 
-        layersVectorSourceRef.current.addFeatures(features)
+          layersVectorSourceRef.current.addFeatures(features)
+        }
       }
 
       if (geom) {
