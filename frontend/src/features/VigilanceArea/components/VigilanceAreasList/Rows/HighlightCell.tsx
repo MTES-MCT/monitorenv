1d9d5a3a4dfc60baf8825d06620fb9053ef0f8d4
Claire Dagan <clairedagan@gmail.com>
2024-10-30 10:44:49 +0100

    [VigilanceAreas] finalize table view

---
+++ frontend/src/features/VigilanceArea/components/VigilanceAreasList/Rows/HighlightCell.tsx
@@ -0,0 +1,9 @@
+import Highlighter from 'react-highlight-words'
+
+export function HighlightCell({ text }) {
+  // TODO(30/10/24): get search query filters to add to `searchWords` when filters are added
+
+  return (
+    <Highlighter autoEscape highlightClassName="highlight" searchWords={[]} textToHighlight={text ?? ''} title={text} />
+  )
+}
--- frontend/src/features/VigilanceArea/components/VigilanceAreasList/Rows/LocalizeCell.tsx
+++ frontend/src/features/VigilanceArea/components/VigilanceAreasList/Rows/LocalizeCell.tsx
@@ -1,21 +1,21 @@
 import { vigilanceAreaActions } from '@features/VigilanceArea/slice'
 import { useAppDispatch } from '@hooks/useAppDispatch'
-import { Icon, IconButton, OPENLAYERS_PROJECTION, Size } from '@mtes-mct/monitor-ui'
+import { Accent, Icon, IconButton, OPENLAYERS_PROJECTION } from '@mtes-mct/monitor-ui'
 import { setFitToExtent } from 'domain/shared_slices/Map'
 import { GeoJSON as GeoJsonOpenLayer } from 'ol/format'
 import styled from 'styled-components'
 
 import type { GeoJSON } from 'domain/types/GeoJSON'
 
-export function EditCell({ geom, id }: { geom?: GeoJSON.MultiPolygon; id: number }) {
+export function LocalizeCell({ geom, id }: { geom?: GeoJSON.MultiPolygon; id: number }) {
   const dispatch = useAppDispatch()
-  const editVigilanceArea = () => {
-    dispatch(vigilanceAreaActions.setEditingVigilanceAreaId(id))
-    dispatch(vigilanceAreaActions.setSelectedVigilanceAreaId(id))
 
-    if (!geom) {
-      return
-    }
+  if (!geom) {
+    return <StyledEmptyContainer>-</StyledEmptyContainer>
+  }
+
+  const handleZoomToVigilanceArea = () => {
+    dispatch(vigilanceAreaActions.setSelectedVigilanceAreaId(id))
     const feature = new GeoJsonOpenLayer({
       featureProjection: OPENLAYERS_PROJECTION
     }).readFeature(geom)
@@ -26,17 +26,16 @@ export function EditCell({ geom, id }: { geom?: GeoJSON.MultiPolygon; id: number
     }
   }
 
-  return (
-    <StyledIconButton
-      aria-label="Editer"
-      data-cy={`edit-mission-${id}`}
-      Icon={Icon.Edit}
-      onClick={editVigilanceArea}
-      size={Size.SMALL}
-    />
-  )
+  return <StyledIconButton accent={Accent.TERTIARY} Icon={Icon.FocusZones} onClick={handleZoomToVigilanceArea} />
 }
+
 const StyledIconButton = styled(IconButton)`
   display: inherit;
   margin: auto;
+`
+const StyledEmptyContainer = styled.div`
+  height: 32px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
