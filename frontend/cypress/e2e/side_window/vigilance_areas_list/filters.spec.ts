c28a13a5583167a8a29f88d0a264bfbaf299b7b7
Claire Dagan <clairedagan@gmail.com>
2024-11-04 11:56:39 +0100

    [Tests] refactor tests e2e

--- frontend/cypress/e2e/side_window/vigilance_areas_list/filters.spec.ts
+++ frontend/cypress/e2e/side_window/vigilance_areas_list/filters.spec.ts
@@ -9,84 +9,50 @@ context('Side Window > Vigilance Areas List > Filter Bar', () => {
     cy.getDataCy('reinitialize-filters').scrollIntoView().click()
   })
 
+  const verifyVigilanceAreaRows = (expectedText: string) => {
+    cy.getDataCy('vigilance-area-row').should('have.length.to.be.greaterThan', 0)
+    cy.getDataCy('vigilance-area-row').each((row, index) => {
+      if (index === 0) {
+        return
+      }
+      cy.wrap(row).should('contain', expectedText)
+    })
+  }
+
   it('Should filter vigilance areas for the current year', () => {
     cy.fill('Période de vigilance', 'Cette année')
-
     cy.getDataCy('vigilance-area-row').should('have.length.to.be.greaterThan', 0)
   })
 
   it('Should filter vigilance areas by themes filter', () => {
     cy.wait(500)
     cy.fill('Thématique réglementaire', ['Dragage'])
-
     cy.getDataCy('vigilance-areas-filter-tags').find('.Component-SingleTag > span').contains('Dragage')
-    cy.getDataCy('vigilance-area-row').should('have.length.to.be.greaterThan', 0)
-    cy.getDataCy('vigilance-area-row').each((row, index) => {
-      if (index === 0) {
-        return
-      }
-
-      cy.wrap(row).should('contain', 'Dragage')
-    })
+    verifyVigilanceAreaRows('Dragage')
   })
 
   it('Should filter vigilance areas by createdBy filter', () => {
     cy.fill('Zone créée par...', ['ABC'])
-
-    cy.getDataCy('vigilance-area-row').should('have.length.to.be.greaterThan', 0)
-    cy.getDataCy('vigilance-area-row').each((row, index) => {
-      if (index === 0) {
-        return
-      }
-
-      cy.wrap(row).should('contain', 'ABC')
-    })
+    verifyVigilanceAreaRows('ABC')
   })
 
   it('Should filter missions by seaFront filter', () => {
     cy.fill('Façade', ['NAMO'])
-
-    cy.getDataCy('vigilance-area-row').should('have.length.to.be.greaterThan', 0)
-    cy.getDataCy('vigilance-area-row').each((row, index) => {
-      if (index === 0) {
-        return
-      }
-
-      cy.wrap(row).should('contain', 'NAMO')
-    })
+    verifyVigilanceAreaRows('NAMO')
   })
 
   it('Should filter missions by draft status', () => {
-    // uncheck published status
     cy.fill('Publiée', false)
-
-    cy.getDataCy('vigilance-area-row').should('have.length.to.be.greaterThan', 0)
-    cy.getDataCy('vigilance-area-row').each((row, index) => {
-      if (index === 0) {
-        return
-      }
-
-      cy.wrap(row).should('contain', 'Non Publiée')
-    })
+    verifyVigilanceAreaRows('Non Publiée')
   })
 
   it('Should filter missions by published status', () => {
-    // uncheck draft status
     cy.fill('Non publiée', false)
-
-    cy.getDataCy('vigilance-area-row').should('have.length.to.be.greaterThan', 0)
-    cy.getDataCy('vigilance-area-row').each((row, index) => {
-      if (index === 0) {
-        return
-      }
-
-      cy.wrap(row).should('contain', 'Publiée')
-    })
+    verifyVigilanceAreaRows('Publiée')
   })
 
   it('Should filter missions by search query', () => {
     cy.fill('Rechercher dans les zones de vigilance', 'Proin')
-
     cy.getDataCy('vigilance-area-row').should('have.length', 1)
   })
 })
