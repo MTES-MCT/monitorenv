40672ec6092819013eb85d7707e4627f72122314
Claire Dagan <clairedagan@gmail.com>
2024-11-12 17:25:57 +0100

    [Tech] delete useless files

--- frontend/src/workers/MonitorEnvWorker.ts
+++
@@ -1,7 +0,0 @@
-import * as Comlink from 'comlink'
-
-import { MonitorEnvWebWorker } from './MonitorEnvWebWorker'
-import Worker from './MonitorEnvWebWorker?worker'
-
-const worker = new Worker()
-export const MonitorEnvWorker = Comlink.wrap<typeof MonitorEnvWebWorker>(worker)
