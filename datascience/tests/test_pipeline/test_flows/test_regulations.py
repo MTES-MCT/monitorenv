ea976565ce52c3a2aa6ca37416f0cad588e21209
Maxime Perrault <maxperrault94@gmail.com>
2024-11-26 17:31:24 +0100

    tech: modifying TU to add old values

--- datascience/tests/test_pipeline/test_flows/test_regulations.py
+++ datascience/tests/test_pipeline/test_flows/test_regulations.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import prefect
 import pytest
 
 from src.pipeline.flows.regulations import (
@@ -7,6 +8,7 @@ from src.pipeline.flows.regulations import (
     select_ids_to_delete,
     select_ids_to_update,
 )
+from src.pipeline.generic_tasks import load
 from src.read_query import read_query
 
 
@@ -74,9 +76,121 @@ def new_regulations() -> pd.DataFrame:
                 "40F57994631533F2BFE2B98CD545544640",
             ],
             "entity_name": [
+                "Zone 1_new",
+                "Zone 2",
+                "Zone 3",
+                "Zone 4",
+            ],
+            "layer_name": [
+                "Layer 1",
+                "Layer 2_new",
+                "Layer 3",
+                "Layer 4",
+            ],
+            "facade": [
+                "NAMO",
+                "NAMO",
+                "MED_new",
+                "MED"
+            ],
+            "ref_reg": [
+                "arrêté 1",
+                "arrêté 2",
+                "arrêté 3",
+                "arrêté 4_new",
+            ],
+            "url": [
+                "http://dummy_url_1",
+                "http://dummy_url_2",
+                "http://dummy_url_3",
+                "http://dummy_url_4",
+            ],
+            "row_hash": [
+                "cacem_row_hash_1",
+                "cacem_row_hash_2",
+                "cacem_row_hash_3",
+                "cacem_row_hash_4",
+            ],
+            "edition": [
+                "2021-10-14",
+                "2021-10-14",
+                "2021-10-14",
+                "2021-10-14"
+            ],
+            "editeur": [
+                "Claire Dagan",
+                "Maxime Perrault",
+                "Vincent Chery",
+                "Mike Data"
+            ],
+            "source": [
+                "Source 1",
+                "Source 2",
+                "Source 3",
+                "Source 4"
+            ],
+            "observation": [
+                "Obs1",
+                "Obs2",
+                "Obs3",
+                "Obs4"
+            ],
+            "thematique": [
+                "Mouillage",
+                "Dragage",
+                "Mouillage",
+                "Extraction"
+            ],
+            "date": [
+                "2021-09-04",
+                "2021-09-04",
+                "2021-09-04",
+                "2021-09-04"
+            ],
+            "duree_validite": [
+                "permanent",
+                "permanent",
+                "permanent",
+                None
+            ],
+            "temporalite": [
+                "permanent",
+                "permanent",
+                "permanent",
+                "temporaire"
+            ],
+            "type": [
+                "Arrêté préfectoral",
+                "Décret",
+                "Arrêté inter-préfectoral",
+                None
+            ]
+        }
+    )
+
+@pytest.fixture
+def old_regulations() -> pd.DataFrame:
+    return pd.DataFrame(
+        {
+            "id": [1, 2, 3, 4],
+            "geom": [
+                "0106000020E610000001000000010300000001000000040000001EA36CE84A6F04C028FCC"
+                "F619D7F47407B5A4C4F4F6904C06878344D997F4740906370C20E6A04C050111641647F47"
+                "401EA36CE84A6F04C028FCCF619D7F4740",
+                "0106000020E61000000100000001030000000100000004000000508B8D44B1B304C014238"
+                "1B3F47F4740A374D56D789004C0C0F2BF049B7F474033F02B2558B104C0CCA0D40BEE7E47"
+                "40508B8D44B1B304C0142381B3F47F4740",
+                "0106000020E61000000100000001030000000100000004000000D2204A8709EBE33F541AC"
+                "4E69B024940B8BC1FBE94F2E33F387D124AAF02494021642107D81FE43F387D124AAF0249"
+                "40D2204A8709EBE33F541AC4E69B024940",
+                "0106000020E61000000100000001030000000100000004000000F57994631533F2BFE2B98"
+                "CD5455446407A715E737969F3BFEAD7CEDEB655464036ED5A29A137F4BF97F69352CC3446"
+                "40F57994631533F2BFE2B98CD545544640",
+            ],
+            "entity_name": [
                 "Zone 1",
                 "Zone 2",
-                "Zone 3'",
+                "Zone 3",
                 "Zone 4",
             ],
             "layer_name": [
@@ -109,20 +223,83 @@ def new_regulations() -> pd.DataFrame:
                 "cacem_row_hash_3",
                 "cacem_row_hash_4_new",
             ],
+            "edition": [
+                "2021-10-14",
+                "2021-10-14",
+                "2021-10-14",
+                "2021-10-14"
+            ],
+            "editeur": [
+                "Claire Dagan",
+                "Maxime Perrault",
+                "Vincent Chery",
+                "Mike Data"
+            ],
+            "source": [
+                "Source 1",
+                "Source 2",
+                "Source 3",
+                "Source 4"
+            ],
+            "observation": [
+                "Obs1",
+                "Obs2",
+                "Obs3",
+                "Obs4"
+            ],
+            "thematique": [
+                "Mouillage",
+                "Dragage",
+                "Mouillage",
+                "Extraction"
+            ],
+            "date": [
+                "2021-09-04",
+                "2021-09-04",
+                "2021-09-04",
+                "2021-09-04"
+            ],
+            "duree_validite": [
+                "permanent",
+                "permanent",
+                "permanent",
+                None
+            ],
+            "temporalite": [
+                "permanent",
+                "permanent",
+                "permanent",
+                "temporaire"
+            ],
+            "type": [
+                "Arrêté préfectoral",
+                "Décret",
+                "Arrêté inter-préfectoral",
+                None
+            ]
         }
     )
 
-
-def test_load_new_regulations(new_regulations):
+def test_load_new_regulations(new_regulations, old_regulations):
+    load(
+        old_regulations,
+        table_name="regulations_cacem",
+        schema="public",
+        db_name="monitorenv_remote",
+        logger=prefect.context.get("logger"),
+        how="upsert",
+        table_id_column="id",
+        df_id_column="id",
+    )
     load_new_regulations.run(new_regulations)
     loaded_new_regulations = read_query(
         "monitorenv_remote", 
-        "SELECT id, geom, "
-            "entity_name, layer_name, "
-            "facade, ref_reg, "
-            "url, row_hash "
-            "FROM regulations_cacem "
-            "ORDER BY id"
+        """SELECT 
+            id, geom, entity_name, layer_name, facade,
+            ref_reg, url, row_hash, edition, editeur,
+            source, observation, thematique, date, 
+            duree_validite, temporalite, type
+            FROM public.regulations_cacem
+            ORDER BY id"""
     )
-
     pd.testing.assert_frame_equal(loaded_new_regulations, new_regulations)
