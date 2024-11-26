390d1bb30877ca2cfdad88e9fff2583008aa00b4
Claire Dagan <clairedagan@gmail.com>
2024-11-26 18:03:06 +0100

    [Tech] delete useless code

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,23 +1,9 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import { isCypress } from '@utils/isCypress'
 
 import { setAuthorizationHeader } from './utils/setAuthorizationHeaders'
 import { normalizeRtkBaseQuery } from '../utils/normalizeRtkBaseQuery'
 
 import type { BackendApiErrorResponse } from './types'
-
-// =============================================================================
-// GeoServer API
-
-const geoserverURL = isCypress()
-  ? window.Cypress.env('CYPRESS_FRONTEND_GEOSERVER_REMOTE_URL')
-  : import.meta.env.FRONTEND_REPORTING_FORM_AUTO_SAVE_ENABLED
-
-export const geoserverApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: `${geoserverURL}/geoserver` }),
-  endpoints: () => ({}),
-  reducerPath: 'geoserverApi'
-})
 
 // =============================================================================
 // Monitorenv Private API
