f6235256f27e31167fe2b7771d2221dca709fe46
Maxime Perrault <maxperrault94@gmail.com>
2024-11-26 10:31:59 +0100

    tech: update instead of delete then insert regulations and add TU

--- datascience/src/pipeline/flows/regulations.py
+++ datascience/src/pipeline/flows/regulations.py
@@ -1,10 +1,12 @@
-from pathlib import Path
-
+from logging import Logger
 import pandas as pd
 import prefect
+from pathlib import Path
 from prefect import Flow, case, task
-
+from sqlalchemy import text
+from src.db_config import create_engine
 from src.pipeline.generic_tasks import delete_rows, extract, load
+from src.pipeline.utils import psql_insert_copy
 
 
 @task(checkpoint=False)
@@ -107,7 +109,26 @@ def extract_new_regulations(ids_to_update: set) -> pd.DataFrame:
     )
 
 
-@task(checkpoint=False)
+# @task(checkpoint=False)
+# def load_new_regulations(new_regulations: pd.DataFrame):
+#     """Load the output of ``extract_rows_to_update`` task into ``regulations``
+#     table.
+
+#     Args:
+#         new_regulations (pd.DataFrame): output of ``extract_rows_to_update`` task.
+#     """
+#     load(
+#         new_regulations,
+#         table_name="regulations_cacem",
+#         schema="public",
+#         db_name="monitorenv_remote",
+#         logger=prefect.context.get("logger"),
+#         how="upsert",
+#         table_id_column="id",
+#         df_id_column="id",
+#     )
+
+
 def load_new_regulations(new_regulations: pd.DataFrame):
     """Load the output of ``extract_rows_to_update`` task into ``regulations``
     table.
@@ -115,20 +136,97 @@ def load_new_regulations(new_regulations: pd.DataFrame):
     Args:
         new_regulations (pd.DataFrame): output of ``extract_rows_to_update`` task.
     """
-    load(
-        new_regulations,
-        table_name="regulations_cacem",
-        schema="public",
-        db_name="monitorenv_remote",
-        logger=prefect.context.get("logger"),
-        how="upsert",
-        table_id_column="id",
-        df_id_column="id",
-    )
+    e = create_engine("monitorenv_remote")
+    logger = prefect.context.get("logger")
+
+    with e.begin() as connection:
+        logger.info("Creating temporary table")
+        connection.execute(
+            text(
+                """CREATE TEMP TABLE tmp_regulations_cacem(
+                 id serial,
+                 geom public.geometry(MultiPolygon,4326),
+                 entity_name character varying,
+                 url character varying,
+                 layer_name character varying,
+                 facade character varying,
+                 ref_reg character varying,
+                 edition character varying,
+                 editeur character varying,
+                 source character varying,
+                 observation character varying,
+                 thematique character varying,
+                 date character varying,
+                 duree_validite character varying,
+                 date_fin character varying,
+                 temporalite character varying,
+                 type character varying,
+                )
+                ON COMMIT DROP;"""
+            )
+        )
+
+        # a voir pour les geometries
+        # new_regulations = prepare_df_for_loading(
+        #     new_regulations,
+        #     logger,
+        # )
+
+        columns_to_load = [
+            "id",
+            "geom",
+            "entity_name",
+            "url",
+            "layer_name",
+            "facade",
+            "ref_reg",
+            "edition",
+            "editeur",
+            "source",
+            "observation",
+            "thematique",
+            "date",
+            "duree_validite",
+            "temporalite",
+            "type",
+        ]
+
+        logger.info("Loading to temporary table")
+
+        new_regulations[columns_to_load].to_sql(
+            "tmp_regulations_cacem",
+            connection,
+            if_exists="append",
+            index=False,
+            method=psql_insert_copy,
+        )
+
+        logger.info("Updating regulations_cacem from temporary table")
+        connection.execute(
+            text(
+                """UPDATE public.regulations_cacem reg
+                SET reg.geom = tmp.geom,
+                reg.entity_name = tmp.entity_name,
+                reg.url = tmp.url,
+                reg.layer_name = tmp.layer_name,
+                reg.facade = tmp.facade,
+                reg.ref_reg = tmp.ref_reg,
+                reg.edition = tmp.edition,
+                reg.editeur = tmp.editeur,
+                reg.source = tmp.source,
+                reg.observation = tmp.observation,
+                reg.thematique = tmp.thematique,
+                reg.date = tmp.date,
+                reg.duree_validite = tmp.duree_validite,
+                reg.temporalite = tmp.temporalite,
+                reg.type = tmp.type,
+                FROM tmp_regulations_cacem tmp
+                where reg.id = tmp.id;"""
+            ),
+        )
 
 
 with Flow("Regulations") as flow:
-
     local_hashes = extract_local_hashes()
     remote_hashes = extract_remote_hashes()
     hashes = merge_hashes(local_hashes, remote_hashes)
