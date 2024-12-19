4464bfbff96944801f08677f9173c653b091f349
Claire Dagan <clairedagan@gmail.com>
2024-12-19 08:34:52 +0100

    [Mission] Add id in form and NavBar

--- frontend/src/utils/getMissionTitle.ts
+++ frontend/src/utils/getMissionTitle.ts
@@ -5,7 +5,7 @@ export function getMissionTitle(isNewMission: boolean, values?: Partial<Mission>
     ? `Nouvelle mission ${
         values?.controlUnits && values?.controlUnits.length > 0 && values?.controlUnits[0]?.name ? '-' : ''
       } ${values?.controlUnits?.map(controlUnit => controlUnit.name).join(', ')}`
-    : `Mission ${
+    : `${values?.id} - Mission ${
         values?.missionTypes &&
         values?.missionTypes.map(missionType => missionTypeEnum[missionType].libelle).join(' / ')
       } – ${values?.controlUnits?.map(controlUnit => controlUnit.name?.replace('(historique)', '')).join(', ')}`
