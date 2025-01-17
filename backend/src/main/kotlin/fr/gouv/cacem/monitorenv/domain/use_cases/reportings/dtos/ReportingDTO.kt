bad134f45374c034e196d24ca4364d2e4c507c79
Claire Dagan <clairedagan@gmail.com>
2025-01-17 15:50:59 +0100

    [Reporting] Create function to getControlStatus

--- backend/src/main/kotlin/fr/gouv/cacem/monitorenv/domain/use_cases/reportings/dtos/ReportingDTO.kt
+++ backend/src/main/kotlin/fr/gouv/cacem/monitorenv/domain/use_cases/reportings/dtos/ReportingDTO.kt
@@ -1,7 +1,6 @@
 package fr.gouv.cacem.monitorenv.domain.use_cases.reportings.dtos
 
 import fr.gouv.cacem.monitorenv.domain.entities.mission.MissionEntity
-import fr.gouv.cacem.monitorenv.domain.entities.mission.envAction.ActionTypeEnum
 import fr.gouv.cacem.monitorenv.domain.entities.reporting.ControlStatusEnum
 import fr.gouv.cacem.monitorenv.domain.entities.reporting.ReportingEntity
 
@@ -12,24 +11,5 @@ data class ReportingDTO(
     val detachedMission: MissionEntity? = null,
 ) {
     val controlStatus: ControlStatusEnum?
-        get() =
-            when {
-                this.reporting.attachedEnvActionId != null &&
-                    this.attachedMission?.envActions
-                        ?.find { it.id == this.reporting.attachedEnvActionId }
-                        ?.actionType == ActionTypeEnum.SURVEILLANCE ->
-                    ControlStatusEnum.SURVEILLANCE_DONE
-
-                this.reporting.attachedEnvActionId != null &&
-                    this.attachedMission?.envActions
-                        ?.find { it.id == this.reporting.attachedEnvActionId }
-                        ?.actionType == ActionTypeEnum.CONTROL ->
-                    ControlStatusEnum.CONTROL_DONE
-
-                this.reporting.attachedEnvActionId == null &&
-                    this.reporting.isControlRequired == true ->
-                    ControlStatusEnum.CONTROL_TO_BE_DONE
-
-                else -> null
-            }
+        get() = getControlStatus(reporting, attachedMission)
 }
---
+++ backend/src/main/kotlin/fr/gouv/cacem/monitorenv/domain/use_cases/reportings/dtos/utils.kt
@@ -0,0 +1,30 @@
+package fr.gouv.cacem.monitorenv.domain.use_cases.reportings.dtos
+
+import fr.gouv.cacem.monitorenv.domain.entities.mission.MissionEntity
+import fr.gouv.cacem.monitorenv.domain.entities.mission.envAction.ActionTypeEnum
+import fr.gouv.cacem.monitorenv.domain.entities.reporting.ControlStatusEnum
+import fr.gouv.cacem.monitorenv.domain.entities.reporting.ReportingEntity
+
+fun getControlStatus(
+    reporting: ReportingEntity,
+    attachedMission: MissionEntity?,
+): ControlStatusEnum? {
+    return when {
+        reporting.attachedEnvActionId != null &&
+            attachedMission?.envActions
+                ?.find { it.id == reporting.attachedEnvActionId }
+                ?.actionType == ActionTypeEnum.SURVEILLANCE ->
+            ControlStatusEnum.SURVEILLANCE_DONE
+
+        reporting.attachedEnvActionId != null &&
+            attachedMission?.envActions
+                ?.find { it.id == reporting.attachedEnvActionId }
+                ?.actionType == ActionTypeEnum.CONTROL ->
+            ControlStatusEnum.CONTROL_DONE
+
+        reporting.attachedEnvActionId == null && reporting.isControlRequired == true ->
+            ControlStatusEnum.CONTROL_TO_BE_DONE
+
+        else -> null
+    }
+}
