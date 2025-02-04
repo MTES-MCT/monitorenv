8e29b820d611e0e5c917793c2ee1d04f44afb805
Claire Dagan <clairedagan@gmail.com>
2025-02-04 17:32:29 +0100

    [Brief] displayers layers group by entity in pdf

--- frontend/src/features/Dashboard/components/Pdf/v2/Table/index.tsx
+++ frontend/src/features/Dashboard/components/Pdf/v2/Table/index.tsx
@@ -1,5 +1,10 @@
+import { getAMPColorWithAlpha } from '@features/map/layers/AMP/AMPLayers.style'
+import { getRegulatoryEnvColorWithAlpha } from '@features/map/layers/styles/administrativeAndRegulatoryLayers.style'
+import { getVigilanceAreaColorWithAlpha } from '@features/VigilanceArea/components/VigilanceAreaLayer/style'
 import { THEME } from '@mtes-mct/monitor-ui'
 import { StyleSheet, Text, View, Image } from '@react-pdf/renderer'
+import { getTitle } from 'domain/entities/layers/utils'
+import { groupBy } from 'lodash'
 
 import { layoutStyle } from '../style'
 
@@ -10,13 +15,22 @@ import type { RegulatoryLayerWithMetadata } from 'domain/entities/regulatory'
 
 const styles = StyleSheet.create({
   amp: {
-    backgroundColor: '#C58F7E',
-    color: THEME.color.white
+    backgroundColor: '#D6DF64',
+    color: THEME.color.charcoal
   },
   cell: {
+    alignItems: 'center',
+    border: `1 solid ${THEME.color.blueGray25}`,
+    borderTop: 'none',
+    flexDirection: 'row',
+    fontSize: 6.8,
+    padding: '4.3 12'
+  },
+  groupName: {
     border: `1 solid ${THEME.color.blueGray25}`,
     borderTop: 'none',
     fontSize: 6.8,
+    fontWeight: 'bold',
     padding: '4.3 12'
   },
   header: {
@@ -30,6 +44,12 @@ const styles = StyleSheet.create({
     fontSize: 6.8,
     fontWeight: 'bold'
   },
+  layerLegend: {
+    border: `0.5 solid ${THEME.color.slateGray}`,
+    height: 8,
+    marginRight: 5,
+    width: 8
+  },
   regulatoryArea: {
     backgroundColor: '#8CC3C0',
     color: THEME.color.white
@@ -37,11 +57,11 @@ const styles = StyleSheet.create({
   table: {
     display: 'flex',
     flexDirection: 'column',
-    paddingBottom: 36
+    paddingTop: 36
   },
   vigilanceArea: {
-    backgroundColor: '#D6DF64',
-    color: THEME.color.charcoal
+    backgroundColor: '#C58F7E',
+    color: THEME.color.white
   }
 })
 
@@ -56,6 +76,16 @@ export function AreaTable({
   regulatoryAreas: RegulatoryLayerWithMetadata[]
   vigilanceAreas: VigilanceArea.VigilanceArea[]
 }) {
+  const groupedRegulatoryAreas = groupBy(
+    regulatoryAreas.sort((a, b) => a.layerName.localeCompare(b.layerName)),
+    regulatory => regulatory.layerName
+  )
+
+  const groupedAmps = groupBy(
+    [...amps].sort((a, b) => a.name.localeCompare(b.name)),
+    r => r.name
+  )
+
   return (
     <>
       <View style={layoutStyle.header}>
@@ -83,24 +113,62 @@ export function AreaTable({
 
         <View style={layoutStyle.row}>
           <View style={{ flex: 1 }}>
-            {regulatoryAreas.map(regulatoryArea => (
-              <Text key={regulatoryArea.id} style={styles.cell} wrap={false}>
-                {regulatoryArea.entityName || 'AUCUN NOM'}
-              </Text>
+            {Object.entries(groupedRegulatoryAreas).map(([groupName, layers]) => (
+              <View key={groupName} wrap={false}>
+                <Text style={styles.groupName}>{getTitle(groupName)}</Text>
+                {layers.map(layer => (
+                  <View style={styles.cell}>
+                    <View
+                      style={[
+                        styles.layerLegend,
+                        {
+                          backgroundColor: getRegulatoryEnvColorWithAlpha(layer.thematique, layer.entityName)
+                        }
+                      ]}
+                    />
+                    <Text key={layer.id} wrap={false}>
+                      {layer.entityName || 'AUCUN NOM'}
+                    </Text>
+                  </View>
+                ))}
+              </View>
             ))}
           </View>
           <View style={{ flex: 1 }}>
-            {amps.map(amp => (
-              <Text key={amp.id} style={styles.cell} wrap={false}>
-                {amp.name}
-              </Text>
+            {Object.entries(groupedAmps).map(([groupName, layers]) => (
+              <View key={groupName} wrap={false}>
+                <Text style={styles.groupName}>{getTitle(groupName)}</Text>
+                {layers.map(layer => (
+                  <View style={styles.cell}>
+                    <View
+                      style={[
+                        styles.layerLegend,
+                        {
+                          backgroundColor: getAMPColorWithAlpha(layer.type, layer.name)
+                        }
+                      ]}
+                    />
+                    <Text key={layer.id} wrap={false}>
+                      {layer.name || 'AUCUN NOM'}
+                    </Text>
+                  </View>
+                ))}
+              </View>
             ))}
           </View>
           <View style={{ flex: 1 }}>
             {vigilanceAreas.map(vigilanceArea => (
-              <Text key={vigilanceArea.id} style={styles.cell} wrap={false}>
-                {vigilanceArea.name}
-              </Text>
+              <View key={vigilanceArea.id} style={styles.cell}>
+                <View
+                  style={[
+                    styles.layerLegend,
+                    {
+                      backgroundColor: getVigilanceAreaColorWithAlpha(vigilanceArea.name, vigilanceArea.comments)
+                    }
+                  ]}
+                />
+                <Text wrap={false}>{vigilanceArea.name}</Text>
+              </View>
             ))}
           </View>
         </View>
