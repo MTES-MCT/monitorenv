542e179b2edc3c9e959ec54ee22db937bea81e84
Claire Dagan <clairedagan@gmail.com>
2025-02-06 16:55:47 +0100

    [Brief] test create odt file

--- frontend/src/features/Dashboard/components/DashboardForm/Footer/index.tsx
+++ frontend/src/features/Dashboard/components/DashboardForm/Footer/index.tsx
@@ -6,6 +6,7 @@ import { useState } from 'react'
 import styled from 'styled-components'
 
 import { CreateMailButton } from './CreateMailButton'
+import { GenerateEditableDocButton } from './GenerateEditableDocButton'
 import { GeneratePdfButton } from '../../Pdf/GeneratePdfButton'
 
 import type { DashboardType } from '@features/Dashboard/slice'
@@ -93,6 +94,7 @@ export function Footer({ dashboardForm: [key, dashboard] }: FooterProps) {
 
         <ButtonsWrapper>
           <CreateMailButton dashboard={dashboard.dashboard} />
+          <GenerateEditableDocButton dashboard={dashboard.dashboard} />
           <GeneratePdfButton dashboard={dashboard.dashboard} />
           <Button accent={Accent.SECONDARY} Icon={Icon.Save} onClick={handleSave}>
             Enregistrer le tableau
