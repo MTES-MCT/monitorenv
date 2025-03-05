219fc632f727daa0ab37a0347da790756e3f8367
Claire Dagan <clairedagan@gmail.com>
2025-03-05 16:43:19 +0100

    [RecentActivity] add distinction filters to display controls according to the number of infractions

--- frontend/src/features/RecentActivity/components/Layers/style.ts
+++ frontend/src/features/RecentActivity/components/Layers/style.ts
@@ -2,8 +2,15 @@ import { THEME } from '@mtes-mct/monitor-ui'
 
 import type { FlatIcon } from 'ol/style/flat'
 
+const featureHas = (key: string) => ['==', ['get', key], 1]
+
 export const recentControlActivityStyle: FlatIcon = {
-  'icon-color': THEME.color.charcoal,
+  'icon-color': [
+    'case',
+    featureHas('withDistinction'),
+    ['case', featureHas('hasInfraction'), THEME.color.maximumRed, THEME.color.yellowGreen],
+    THEME.color.charcoal
+  ],
   'icon-scale': 0.6,
   'icon-src': 'Close.svg'
 }
