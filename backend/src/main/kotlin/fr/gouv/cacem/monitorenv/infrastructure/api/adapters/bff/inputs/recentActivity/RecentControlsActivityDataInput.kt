9027fda82711d96cfaa5807cb23599687415125b
Claire Dagan <clairedagan@gmail.com>
2025-03-11 13:32:05 +0100

    [Tech] create Enum for infraction status

--- backend/src/main/kotlin/fr/gouv/cacem/monitorenv/infrastructure/api/adapters/bff/inputs/recentActivity/RecentControlsActivityDataInput.kt
+++ backend/src/main/kotlin/fr/gouv/cacem/monitorenv/infrastructure/api/adapters/bff/inputs/recentActivity/RecentControlsActivityDataInput.kt
@@ -1,12 +1,13 @@
 package fr.gouv.cacem.monitorenv.infrastructure.api.adapters.bff.inputs.recentActivity
 
+import fr.gouv.cacem.monitorenv.domain.entities.recentActivity.InfractionEnum
 import java.time.ZonedDateTime
 
 data class RecentControlsActivityDataInput(
     val administrationIds: List<Int>?,
     val controlUnitIds: List<Int>?,
     val geometry: String?,
-    val infractionsStatus: List<String>?,
+    val infractionsStatus: List<InfractionEnum>?,
     val startedAfter: ZonedDateTime,
     val startedBefore: ZonedDateTime,
     val themeIds: List<Int>?,
