294e73cdbc005096ca012e643bd5c5a912b379e6
Claire Dagan <clairedagan@gmail.com>
2025-03-14 12:19:24 +0100

    [RecentActivity] filter with distinction ok

--- frontend/src/features/RecentActivity/components/Layers/recentControlActivityGeometryHelper.ts
+++ frontend/src/features/RecentActivity/components/Layers/recentControlActivityGeometryHelper.ts
@@ -3,40 +3,44 @@ import { OPENLAYERS_PROJECTION, WSG84_PROJECTION } from '@mtes-mct/monitor-ui'
 import { Layers } from 'domain/entities/layers/constants'
 import { Feature } from 'ol'
 import { GeoJSON } from 'ol/format'
-import { getArea } from 'ol/sphere'
 
-export const getRecentControlActivityGeometry = (
-  control: RecentActivity.RecentControlsActivity,
-  distinctionFilter: RecentActivity.DistinctionFilterEnum,
-  infractionsStatus: RecentActivity.StatusFilterEnum[] | undefined
-) => {
+type RecentControlActivityGeometryProps = {
+  control: RecentActivity.RecentControlsActivity
+  totalControls?: number
+  totalControlsWithInfractions?: number
+  totalControlsWithoutInfractions?: number
+  withInfractions?: boolean
+}
+
+export const getRecentControlActivityGeometry = ({
+  control,
+  totalControls = 0,
+  totalControlsWithInfractions = 0,
+  totalControlsWithoutInfractions = 0,
+  withInfractions = false
+}: RecentControlActivityGeometryProps): Feature => {
   const geoJSON = new GeoJSON()
   const geometry = geoJSON.readGeometry(control.geom, {
     dataProjection: WSG84_PROJECTION,
     featureProjection: OPENLAYERS_PROJECTION
   })
-  const area = geometry && getArea(geometry)
 
   const feature = new Feature({
     geometry
   })
 
-  const totalControlsInInfractions = control.infractions.reduce((acc, infraction) => acc + infraction.nbTarget, 0)
-
-  let totalControls = control.actionNumberOfControls
-  if (infractionsStatus && infractionsStatus.length === 1) {
-    if (infractionsStatus[0] === RecentActivity.StatusFilterEnum.WITHOUT_INFRACTION) {
-      totalControls -= totalControlsInInfractions
-    }
+  feature.setId(`${Layers.RECENT_CONTROLS_ACTIVITY.code}:${control.id}`)
+  if (withInfractions) {
+    feature.setId(`${Layers.RECENT_CONTROLS_ACTIVITY.code}:${control.id}-with-infractions`)
   }
 
-  feature.setId(`${Layers.RECENT_CONTROLS_ACTIVITY.code}:${control.id}`)
   feature.setProperties({
-    area,
     hasInfraction: control.infractions.length > 0,
     totalControls,
-    withDistinction: distinctionFilter === RecentActivity.DistinctionFilterEnum.WITH_DISTINCTION,
-    ...control
+    totalControlsWithInfractions,
+    totalControlsWithoutInfractions,
+    ...control,
+    ...(withInfractions && { withInfractions: true })
   })
 
   return feature
