069a941646d185ab509b759b61928e4682f754b2
Claire Dagan <clairedagan@gmail.com>
2025-03-24 08:47:20 +0100

    [Tech] rename files

--- backend/src/main/kotlin/fr/gouv/cacem/monitorenv/domain/use_cases/reportings/dtos/Utils.kt
+++ backend/src/main/kotlin/fr/gouv/cacem/monitorenv/domain/use_cases/reportings/dtos/Utils.kt
@@ -6,26 +6,20 @@ import fr.gouv.cacem.monitorenv.domain.entities.reporting.ControlStatusEnum
 import fr.gouv.cacem.monitorenv.domain.entities.reporting.ReportingEntity
 
 fun getControlStatus(
-    reporting: ReportingEntity,
-    attachedMission: MissionEntity?,
+        reporting: ReportingEntity,
+        attachedMission: MissionEntity?,
 ): ControlStatusEnum? =
-    when {
-        reporting.attachedEnvActionId != null &&
-            attachedMission
-                ?.envActions
-                ?.find { it.id == reporting.attachedEnvActionId }
-                ?.actionType == ActionTypeEnum.SURVEILLANCE ->
-            ControlStatusEnum.SURVEILLANCE_DONE
-
-        reporting.attachedEnvActionId != null &&
-            attachedMission
-                ?.envActions
-                ?.find { it.id == reporting.attachedEnvActionId }
-                ?.actionType == ActionTypeEnum.CONTROL ->
-            ControlStatusEnum.CONTROL_DONE
-
-        reporting.attachedEnvActionId == null && reporting.isControlRequired == true ->
-            ControlStatusEnum.CONTROL_TO_BE_DONE
-
-        else -> null
-    }
+        when {
+            reporting.attachedEnvActionId != null &&
+                    attachedMission?.envActions
+                            ?.find { it.id == reporting.attachedEnvActionId }
+                            ?.actionType == ActionTypeEnum.SURVEILLANCE ->
+                    ControlStatusEnum.SURVEILLANCE_DONE
+            reporting.attachedEnvActionId != null &&
+                    attachedMission?.envActions
+                            ?.find { it.id == reporting.attachedEnvActionId }
+                            ?.actionType == ActionTypeEnum.CONTROL -> ControlStatusEnum.CONTROL_DONE
+            reporting.attachedEnvActionId == null && reporting.isControlRequired == true ->
+                    ControlStatusEnum.CONTROL_TO_BE_DONE
+            else -> null
+        }
