bb4a539abd85d928ed2996cd8bafe9d5dd1f6ff9
Claire Dagan <clairedagan@gmail.com>
2025-03-24 08:47:20 +0100

    [Tech] rename files

--- backend/src/main/kotlin/fr/gouv/cacem/monitorenv/utils/RequireNotNullList.kt
+++ backend/src/main/kotlin/fr/gouv/cacem/monitorenv/utils/RequireNotNullList.kt
@@ -1,3 +1,4 @@
 package fr.gouv.cacem.monitorenv.utils
 
-fun <T : Any> requireNotNullList(values: List<T?>?): List<T> = requireNotNull(values).map { requireNotNull(it) }
+fun <T : Any> requireNotNullList(values: List<T?>?): List<T> =
+        requireNotNull(values).map { requireNotNull(it) }
