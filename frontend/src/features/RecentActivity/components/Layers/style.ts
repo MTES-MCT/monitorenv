3ba7bc4a603fb249a78bee831e7b2a1a4b9aeb61
Claire Dagan <clairedagan@gmail.com>
2025-03-25 11:07:02 +0100

    [Tech] changes after code review

--- frontend/src/features/RecentActivity/components/Layers/style.ts
+++ frontend/src/features/RecentActivity/components/Layers/style.ts
@@ -4,28 +4,8 @@ import { THEME } from '@mtes-mct/monitor-ui'
 import { Layers } from 'domain/entities/layers/constants'
 import { Icon, Stroke, Style } from 'ol/style'
 
-const getIconColor = (ratioInfractionsInControls: number) => {
-  if (ratioInfractionsInControls < 6) {
-    return RecentActivity.CONTROLS_COLORS[0]
-  }
-  if (ratioInfractionsInControls < 11) {
-    return RecentActivity.CONTROLS_COLORS[1]
-  }
-  if (ratioInfractionsInControls < 26) {
-    return RecentActivity.CONTROLS_COLORS[2]
-  }
-  if (ratioInfractionsInControls < 51) {
-    return RecentActivity.CONTROLS_COLORS[3]
-  }
-  if (ratioInfractionsInControls < 76) {
-    return RecentActivity.CONTROLS_COLORS[4]
-  }
-  if (ratioInfractionsInControls < 91) {
-    return RecentActivity.CONTROLS_COLORS[5]
-  }
-
-  return RecentActivity.CONTROLS_COLORS[6]
-}
+const getIconColor = (ratioInfractionsInControls: number) =>
+  RecentActivity.CONTROL_THRESHOLDS.find(({ limit }) => ratioInfractionsInControls < limit)?.color
 
 export const recentControlActivityStyle = feature => {
   if (feature.getId() === `${Layers.RECENT_CONTROLS_ACTIVITY.code}:DRAWED_GEOMETRY`) {
