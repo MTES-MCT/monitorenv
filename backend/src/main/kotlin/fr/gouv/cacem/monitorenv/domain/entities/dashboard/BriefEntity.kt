e6bcac7d6a33e2e166a8b970c3c20845ec553e0c
Claire Dagan <clairedagan@gmail.com>
2025-03-25 17:50:35 +0100

    [Brief] create first page

--- backend/src/main/kotlin/fr/gouv/cacem/monitorenv/domain/entities/dashboard/BriefEntity.kt
+++ backend/src/main/kotlin/fr/gouv/cacem/monitorenv/domain/entities/dashboard/BriefEntity.kt
@@ -2,5 +2,5 @@ package fr.gouv.cacem.monitorenv.domain.entities.dashboard
 
 data class BriefEntity(
     val dashboard: DashboardEntity,
-    val images: List<BriefImageEntity>,
+    val images: List<BriefImageEntity>? = listOf(),
 )
