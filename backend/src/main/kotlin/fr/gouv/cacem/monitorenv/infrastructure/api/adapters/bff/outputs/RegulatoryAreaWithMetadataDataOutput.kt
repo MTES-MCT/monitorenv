56babee70839c4df3a443803f62b16eacd139d14
Maxime Perrault <maxperrault94@gmail.com>
2025-04-02 15:44:15 +0200

    feat: refacto to use one table with self referencing instead of multiples tables

--- backend/src/main/kotlin/fr/gouv/cacem/monitorenv/infrastructure/api/adapters/bff/outputs/RegulatoryAreaWithMetadataDataOutput.kt
+++ backend/src/main/kotlin/fr/gouv/cacem/monitorenv/infrastructure/api/adapters/bff/outputs/RegulatoryAreaWithMetadataDataOutput.kt
@@ -1,7 +1,6 @@
 package fr.gouv.cacem.monitorenv.infrastructure.api.adapters.bff.outputs
 
 import fr.gouv.cacem.monitorenv.domain.entities.regulatoryArea.RegulatoryAreaEntity
-import fr.gouv.cacem.monitorenv.infrastructure.api.adapters.bff.outputs.tags.TagOutput
 import org.locationtech.jts.geom.MultiPolygon
 
 data class RegulatoryAreaWithMetadataDataOutput(
--- backend/src/main/kotlin/fr/gouv/cacem/monitorenv/infrastructure/database/repositories/interfaces/IDBSubTagVigilanceAreaRepository.kt
+++
@@ -1,9 +0,0 @@
-package fr.gouv.cacem.monitorenv.infrastructure.database.repositories.interfaces
-
-import fr.gouv.cacem.monitorenv.infrastructure.database.model.SubTagVigilanceAreaModel
-import fr.gouv.cacem.monitorenv.infrastructure.database.model.SubTagVigilanceAreaPk
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface IDBSubTagVigilanceAreaRepository : JpaRepository<SubTagVigilanceAreaModel, SubTagVigilanceAreaPk> {
-    fun deleteAllByVigilanceAreaId(vigilanceAreaId: Int)
-}
