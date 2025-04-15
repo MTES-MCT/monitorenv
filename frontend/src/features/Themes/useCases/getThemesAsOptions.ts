442e76d665728b20a0a24c11abad255f4283d074
Maxime Perrault <maxperrault94@gmail.com>
2025-04-15 14:54:22 +0200

    feat: refacto reporting control plan to theme

--- frontend/src/features/Themes/useCases/getThemesAsOptions.ts
+++ frontend/src/features/Themes/useCases/getThemesAsOptions.ts
@@ -5,9 +5,12 @@ import type { ThemeAPI } from 'domain/entities/themes'
 export const getThemesAsOptions = (themes: ThemeAPI[], childrenKey: string = 'subThemes'): CheckTreePickerOption[] =>
   themes
     .map(theme => ({
-      [childrenKey]: theme.subThemes
-        .map(subTheme => ({ label: subTheme.name, value: subTheme.id }))
-        .sort((a, b) => a.label.localeCompare(b.label)),
+      [childrenKey]:
+        theme.subThemes.length === 0
+          ? undefined
+          : theme.subThemes
+              .map(subTheme => ({ label: subTheme.name, value: subTheme.id }))
+              .sort((a, b) => a.label.localeCompare(b.label)),
       label: theme.name,
       value: theme.id
     }))
@@ -16,7 +19,7 @@ export const getThemesAsOptions = (themes: ThemeAPI[], childrenKey: string = 'su
 export const getThemesAsOptionsCheckPicker = (themes: ThemeAPI[]): Option<number>[] =>
   themes.map(theme => ({ label: theme.name, value: theme.id })).sort((a, b) => a.label.localeCompare(b.label))
 
-export const parseOptionToTheme = (options: CheckTreePickerOption[], childrenKey: string = 'subThemes'): ThemeAPI[] =>
+export const parseOptionsToThemes = (options: CheckTreePickerOption[], childrenKey: string = 'subThemes'): ThemeAPI[] =>
   options.map(option => ({
     id: +option.value,
     name: option.label,
