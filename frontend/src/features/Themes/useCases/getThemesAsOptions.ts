1537d33daf97e8882d0308e2cc3f1a24d95f6caf
Maxime Perrault <maxperrault94@gmail.com>
2025-04-16 09:18:41 +0200

    feat: add checktreepicker to regulatory area filter

--- frontend/src/features/Themes/useCases/getThemesAsOptions.ts
+++ frontend/src/features/Themes/useCases/getThemesAsOptions.ts
@@ -1,22 +1,30 @@
-import type { Option } from '@mtes-mct/monitor-ui'
-import type { CheckTreePickerOption } from '@mtes-mct/monitor-ui__root'
+import type { CheckTreePickerOption, Option } from '@mtes-mct/monitor-ui'
 import type { ThemeAPI } from 'domain/entities/themes'
 
 export const getThemesAsOptions = (themes: ThemeAPI[], childrenKey: string = 'subThemes'): CheckTreePickerOption[] =>
   themes
-    .map(theme => ({
-      [childrenKey]: theme.subThemes
-        .map(subTheme => ({ label: subTheme.name, value: subTheme.id }))
-        .sort((a, b) => a.label.localeCompare(b.label)),
-      label: theme.name,
-      value: theme.id
-    }))
+    .map(theme => {
+      const subThemes =
+        theme.subThemes.length === 0
+          ? undefined
+          : {
+              [childrenKey]: theme.subThemes
+                .map(({ id, name }) => ({ label: name, value: id }))
+                .sort((a, b) => a.label.localeCompare(b.label))
+            }
+
+      return {
+        ...subThemes,
+        label: theme.name,
+        value: theme.id
+      }
+    })
     .sort((a, b) => a.label.localeCompare(b.label))
 
 export const getThemesAsOptionsCheckPicker = (themes: ThemeAPI[]): Option<number>[] =>
   themes.map(theme => ({ label: theme.name, value: theme.id })).sort((a, b) => a.label.localeCompare(b.label))
 
-export const parseOptionToTheme = (options: CheckTreePickerOption[], childrenKey: string = 'subThemes'): ThemeAPI[] =>
+export const parseOptionsToThemes = (options: CheckTreePickerOption[], childrenKey: string = 'subThemes'): ThemeAPI[] =>
   options.map(option => ({
     id: +option.value,
     name: option.label,
@@ -25,3 +33,14 @@ export const parseOptionToTheme = (options: CheckTreePickerOption[], childrenKey
       name: child.label
     }))
   }))
+
+export const filterSubThemes = (theme: ThemeAPI, toDeleteId: number): ThemeAPI | undefined => {
+  if (theme.subThemes.length === 1) {
+    return undefined
+  }
+
+  return {
+    ...theme,
+    subThemes: theme.subThemes.filter(subTheme => subTheme.id !== toDeleteId)
+  }
+}
