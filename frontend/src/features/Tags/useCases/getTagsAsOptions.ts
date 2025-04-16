34f5fc60aa78dcbf6c9c3e8d1491f1c130429107
Maxime Perrault <maxperrault94@gmail.com>
2025-04-16 09:18:41 +0200

    feat: add checktreepicker to regulatory area filter

--- frontend/src/features/Tags/useCases/getTagsAsOptions.ts
+++ frontend/src/features/Tags/useCases/getTagsAsOptions.ts
@@ -1,13 +1,57 @@
-import type { CheckTreePickerOption } from '@mtes-mct/monitor-ui__root'
+import type { CheckTreePickerOption } from '@mtes-mct/monitor-ui'
 import type { TagAPI } from 'domain/entities/tags'
 
 export const getTagsAsOptions = (tags: TagAPI[], childrenKey: string = 'subTags'): CheckTreePickerOption[] =>
   tags
+    .map(tag => {
+      const subTags =
+        tag.subTags.length === 0
+          ? undefined
+          : {
+              [childrenKey]: tag.subTags
+                .map(({ id, name }) => ({ label: name, value: id }))
+                .sort((a, b) => a.label.localeCompare(b.label))
+            }
+
+      return {
+        ...subTags,
+        label: tag.name,
+        value: tag.id
+      }
+    })
+    .sort((a, b) => a.label.localeCompare(b.label))
+
+export const getTagsAsOptionsLegacy = (tags: TagAPI[], childrenKey: string = 'subTags'): CheckTreePickerOption[] =>
+  tags
     .map(tag => ({
-      [childrenKey]: tag.subTags
-        .map(({ name }) => ({ label: name, value: name }))
-        .sort((a, b) => a.label.localeCompare(b.label)),
+      [childrenKey]:
+        tag.subTags.length === 0
+          ? undefined
+          : tag.subTags
+              .map(({ name }) => ({ label: name, value: name }))
+              .sort((a, b) => a.label.localeCompare(b.label)),
       label: tag.name,
       value: tag.name
     }))
     .sort((a, b) => a.label.localeCompare(b.label))
+
+export const parseOptionsToTags = (options: CheckTreePickerOption[], childrenKey: string = 'subTags'): TagAPI[] =>
+  options.map(option => ({
+    id: +option.value,
+    name: option.label,
+    subTags: (option[childrenKey] ?? []).map((child: CheckTreePickerOption) => ({
+      id: child.value,
+      name: child.label
+    }))
+  }))
+
+export const filterSubTags = (tag: TagAPI, toDeleteId: number): TagAPI | undefined => {
+  if (tag.subTags.length === 1) {
+    return undefined
+  }
+
+  return {
+    ...tag,
+    subTags: tag.subTags.filter(subTag => subTag.id !== toDeleteId)
+  }
+}
