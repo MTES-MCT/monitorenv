c62337f82b462db557c5c41c483b229f1491373e
Claire Dagan <clairedagan@gmail.com>
2025-04-17 17:44:03 +0200

    [Flow] update regulations flow after updating column names

--- datascience/src/pipeline/queries/cross/cacem/regulations.sql
+++ datascience/src/pipeline/queries/cross/cacem/regulations.sql
@@ -1,43 +1,43 @@
 SELECT 
   id,
   st_multi(ST_SimplifyPreserveTopology(ST_CurveToLine(geom), 0.00001)) geom,
-  entity_name,
+  ent_name,
   url,
   layer_name,
-  "façade" as facade,
+  facade,
   ref_reg,
-  "Editeur" as editeur,
+  editeur,
   source,
-  "Observation" as observation,
-  "Thematique" as thematique,
-  duree_validite,
-  temporalite,
+  obs,
+  thematique,
+  validite,
+  tempo,
   type,
   date,
   date_fin,
-  "Edition" as edition,
+  edition,
   md5(
         COALESCE(geom::text, '') ||
-        COALESCE(entity_name::text, '') ||
+        COALESCE(ent_name::text, '') ||
         COALESCE(url::text, '') ||
         COALESCE(layer_name::text, '') ||
-        COALESCE("façade"::text, '') ||
+        COALESCE(facade::text, '') ||
         COALESCE(ref_reg::text, '') ||
-        COALESCE("Editeur"::text, '') ||
+        COALESCE(editeur::text, '') ||
         COALESCE(source::text, '') ||
-        COALESCE("Observation"::text, '') ||
-        COALESCE("Thematique"::text, '') ||
-        COALESCE(duree_validite::text, '') ||
-        COALESCE(temporalite::text, '') ||
+        COALESCE(obs::text, '') ||
+        COALESCE(thematique::text, '') ||
+        COALESCE(validite::text, '') ||
+        COALESCE(tempo::text, '') ||
         COALESCE(type::text, '') ||
         COALESCE(date::text, '') ||
         COALESCE(date_fin::text, '') ||
-        COALESCE("Edition"::text, '') 
+        COALESCE(edition::text, '') 
   ) as row_hash
 FROM prod."REG_ENV_V3"
 WHERE 
   geom IS NOT NULL
-  AND entity_name IS NOT NULL
+  AND ent_name IS NOT NULL
   AND layer_name IS NOT NULL
-  AND "Thematique" IS NOT NULL
+  AND thematique IS NOT NULL
   AND id IN :ids
