ad1c5b1ce530c1effddfccba34cb2681b4a75c08
Claire Dagan <clairedagan@gmail.com>
2025-04-18 09:48:37 +0200

    [RegulatoryAreas] update only sql request with alias

--- datascience/src/pipeline/flows/regulations.py
+++ datascience/src/pipeline/flows/regulations.py
@@ -76,7 +76,7 @@ def update_regulations(new_regulations: pd.DataFrame):
                 """CREATE TEMP TABLE tmp_regulations_cacem(
                 id serial,
                 geom public.geometry(MultiPolygon,4326),
-                ent_name character varying,
+                entity_name character varying,
                 url character varying,
                 layer_name character varying,
                 facade character varying,
@@ -84,12 +84,12 @@ def update_regulations(new_regulations: pd.DataFrame):
                 edition character varying,
                 editeur character varying,
                 source character varying,
-                obs character varying,
+                observation character varying,
                 thematique character varying,
                 date character varying,
-                validite character varying,
+                duree_validite character varying,
                 date_fin character varying,
-                tempo character varying,
+                temporalite character varying,
                 type character varying,
                 row_hash text)
                 ON COMMIT DROP;"""
@@ -99,7 +99,7 @@ def update_regulations(new_regulations: pd.DataFrame):
         columns_to_load = [
             "id",
             "geom",
-            "ent_name",
+            "entity_name",
             "url",
             "layer_name",
             "facade",
@@ -107,11 +107,11 @@ def update_regulations(new_regulations: pd.DataFrame):
             "edition",
             "editeur",
             "source",
-            "obs",
+            "observation",
             "thematique",
             "date",
-            "validite",
-            "tempo",
+            "duree_validite",
+            "temporalite",
             "type",
             "row_hash"
         ]
@@ -131,7 +131,7 @@ def update_regulations(new_regulations: pd.DataFrame):
             text(
                 """UPDATE regulations_cacem reg
                 SET geom = tmp.geom,
-                entity_name = tmp.ent_name,
+                entity_name = tmp.entity_name,
                 url = tmp.url,
                 layer_name = tmp.layer_name,
                 facade = tmp.facade,
@@ -139,11 +139,11 @@ def update_regulations(new_regulations: pd.DataFrame):
                 edition = tmp.edition,
                 editeur = tmp.editeur,
                 source = tmp.source,
-                observation = tmp.obs,
+                observation = tmp.observation,
                 thematique = tmp.thematique,
                 date = tmp.date,
-                duree_validite = tmp.validite,
-                temporalite = tmp.tempo,
+                duree_validite = tmp.duree_validite,
+                temporalite = tmp.temporalite,
                 type = tmp.type,
                 row_hash = tmp.row_hash
                 FROM tmp_regulations_cacem tmp
@@ -162,32 +162,8 @@ def load_new_regulations(new_regulations: pd.DataFrame):
         new_amp (pd.DataFrame): output of ``extract_new_regulations`` task.
     """
 
-    # Source -> destination column mapping
-    column_mapping = {
-        "geom" : "geom",
-        "ent_name": "entity_name",
-        "url": "url",
-        "layer_name": "layer_name",
-        "facade": "facade",
-        "ref_reg": "ref_reg",
-        "edition": "edition",
-        "editeur": "editeur",
-        "source": "source",
-        "thematique": "thematique",
-        "date": "date",
-        "date_fin": "date_fin",
-        "type": "type",
-        "id": "id",
-        "obs": "observation",
-        "validite": "duree_validite",
-        "tempo": "temporalite",
-    }
-
-     # renamed les colonnes
-    new_regulations_renamed = new_regulations.rename(columns=column_mapping)
-
     load(
-        new_regulations_renamed,
+        new_regulations,
         table_name="regulations_cacem",
         schema="public",
         db_name="monitorenv_remote",
