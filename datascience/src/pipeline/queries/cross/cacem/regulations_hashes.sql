ca05eb4b31c973248391d53d4fa60e3da0cd9b26
Claire Dagan <clairedagan@gmail.com>
2025-04-21 14:29:13 +0200

    [RegulatoryAreas] update regulations_hashes.sql with new column names

--- datascience/src/pipeline/queries/cross/cacem/regulations_hashes.sql
+++ datascience/src/pipeline/queries/cross/cacem/regulations_hashes.sql
@@ -2,25 +2,25 @@ SELECT
     id,
     md5(
         COALESCE(geom::text, '') ||
-        COALESCE(entity_name::text, '') ||
+        COALESCE(ent_name::text, '') ||
         COALESCE(url::text, '') ||
         COALESCE(layer_name::text, '') ||
-        COALESCE("façade"::text, '') ||
+        COALESCE(facade::text, '') ||
         COALESCE(ref_reg::text, '') ||
-        COALESCE("Editeur"::text, '') ||
+        COALESCE(editeur::text, '') ||
         COALESCE(source::text, '') ||
-        COALESCE("Observation"::text, '') ||
-        COALESCE("Thematique"::text, '') ||
-        COALESCE(duree_validite::text, '') ||
-        COALESCE(temporalite::text, '') ||
+        COALESCE(obs::text, '') ||
+        COALESCE(thematique::text, '') ||
+        COALESCE(validite::text, '')||
+        COALESCE(tempo::text, '')||
         COALESCE(type::text, '') ||
         COALESCE(date::text, '') ||
         COALESCE(date_fin::text, '') ||
-        COALESCE("Edition"::text, '') 
+        COALESCE(edition::text, '') 
   ) AS cacem_row_hash
 FROM prod."REG_ENV_V3"
 WHERE 
   geom IS NOT NULL
-  AND entity_name IS NOT NULL
+  AND ent_name IS NOT NULL
   AND layer_name IS NOT NULL
-  AND "Thematique" IS NOT NULL
+  AND thematique IS NOT NULL
