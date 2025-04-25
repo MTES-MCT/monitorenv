580c0c088516cbb0b7c0d13d73f1205e153bbe99
Claire Dagan <clairedagan@gmail.com>
2025-04-25 10:30:35 +0200

    [Mission] add props missionTypes and controlUnits in mission patchable public api

--- backend/src/main/kotlin/fr/gouv/cacem/monitorenv/domain/entities/mission/PatchableMissionEntity.kt
+++ backend/src/main/kotlin/fr/gouv/cacem/monitorenv/domain/entities/mission/PatchableMissionEntity.kt
@@ -1,11 +1,14 @@
 package fr.gouv.cacem.monitorenv.domain.entities.mission
 
+import fr.gouv.cacem.monitorenv.domain.entities.controlUnit.LegacyControlUnitEntity
 import java.time.ZonedDateTime
-import java.util.Optional
+import java.util.*
 
 data class PatchableMissionEntity(
+    val controlUnits: List<LegacyControlUnitEntity>?,
+    val endDateTimeUtc: Optional<ZonedDateTime>?,
+    val missionTypes: List<MissionTypeEnum>?,
     val observationsByUnit: Optional<String>?,
     val startDateTimeUtc: ZonedDateTime?,
-    val endDateTimeUtc: Optional<ZonedDateTime>?,
     val isUnderJdp: Boolean?,
 )
--- backend/src/test/kotlin/fr/gouv/cacem/monitorenv/domain/use_cases/missions/PatchMissionUTest.kt
+++ backend/src/test/kotlin/fr/gouv/cacem/monitorenv/domain/use_cases/missions/PatchMissionUTest.kt
@@ -2,7 +2,10 @@ package fr.gouv.cacem.monitorenv.domain.use_cases.missions
 
 import com.nhaarman.mockitokotlin2.given
 import com.nhaarman.mockitokotlin2.verify
+import fr.gouv.cacem.monitorenv.domain.entities.controlUnit.LegacyControlUnitEntity
+import fr.gouv.cacem.monitorenv.domain.entities.controlUnit.LegacyControlUnitResourceEntity
 import fr.gouv.cacem.monitorenv.domain.entities.mission.MissionEntity
+import fr.gouv.cacem.monitorenv.domain.entities.mission.MissionTypeEnum
 import fr.gouv.cacem.monitorenv.domain.entities.mission.PatchableMissionEntity
 import fr.gouv.cacem.monitorenv.domain.exceptions.BackendUsageException
 import fr.gouv.cacem.monitorenv.domain.mappers.PatchEntity
@@ -34,8 +37,28 @@ class PatchMissionUTest {
         val tomorrow = ZonedDateTime.now().plusDays(1)
         val patchedObservationsByUnit = "patched observations"
         val patchedIsUnderJdp = false
+        val controlUnit =
+            listOf(
+                LegacyControlUnitEntity(
+                    id = 2,
+                    administration = "Gendarmerie Nationale",
+                    isArchived = false,
+                    name = "BN Toulon",
+                    resources =
+                        listOf(
+                            LegacyControlUnitResourceEntity(
+                                id = 1,
+                                controlUnitId = 2,
+                                name = "Vedette",
+                            ),
+                        ),
+                    contact = null,
+                ),
+            )
         val patchableMission =
             PatchableMissionEntity(
+                controlUnits = controlUnit,
+                missionTypes = listOf(MissionTypeEnum.SEA),
                 observationsByUnit = Optional.of(patchedObservationsByUnit),
                 startDateTimeUtc = today,
                 endDateTimeUtc = Optional.of(tomorrow),
@@ -45,10 +68,12 @@ class PatchMissionUTest {
         val missionPatched =
             aMissionEntity(
                 id = id,
+                controlUnits = controlUnit,
                 observationsByUnit = patchedObservationsByUnit,
                 startDateTimeUtc = today,
                 endDateTimeUtc = tomorrow,
                 isUnderJdp = patchedIsUnderJdp,
+                missionTypes = listOf(MissionTypeEnum.SEA),
             )
 
         given(missionRepository.findById(id)).willReturn(missionFromDatabase)
@@ -62,6 +87,8 @@ class PatchMissionUTest {
         assertThat(savedMission.mission.startDateTimeUtc).isEqualTo(missionPatched.startDateTimeUtc)
         assertThat(savedMission.mission.endDateTimeUtc).isEqualTo(missionPatched.endDateTimeUtc)
         assertThat(savedMission.mission.isUnderJdp).isEqualTo(missionPatched.isUnderJdp)
+        assertThat(savedMission.mission.controlUnits).isEqualTo(missionPatched.controlUnits)
+        assertThat(savedMission.mission.missionTypes).isEqualTo(missionPatched.missionTypes)
         verify(missionRepository).save(missionPatched)
         assertThat(log.out).contains("Attempt to PATCH mission $id")
         assertThat(log.out).contains("Mission $id patched")
@@ -73,6 +100,8 @@ class PatchMissionUTest {
         val id = Random.nextInt()
         val patchableMission =
             PatchableMissionEntity(
+                controlUnits = null,
+                missionTypes = null,
                 observationsByUnit = null,
                 startDateTimeUtc = null,
                 endDateTimeUtc = null,
