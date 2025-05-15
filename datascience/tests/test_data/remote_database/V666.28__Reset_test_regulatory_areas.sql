759d428b539cf91d2f621ba5f7b96235a8981aae
Maxime Perrault <maxperrault94@gmail.com>
2025-05-15 12:15:19 +0200

    add flow for tags and themes, add themes regulatorys and tags on regulatory areas flow

--- datascience/tests/test_data/remote_database/V666.28__Reset_test_regulatory_areas.sql
+++ datascience/tests/test_data/remote_database/V666.28__Reset_test_regulatory_areas.sql
@@ -1,1 +1,6 @@
+-- Supprimer les données des tables dépendantes
+DELETE FROM themes_regulatory_areas;
+DELETE FROM tags_regulatory_areas;
+DELETE FROM dashboard_datas;
+
 DELETE FROM regulations_cacem;
