e8e98909150baf254a4690e25f73b2eafa0f35c2
Maxime Perrault <maxperrault94@gmail.com>
2025-05-21 11:40:18 +0200

    add row_hash to compare themes and tags on select query + add where condition on select themes and tags

--- datascience/src/pipeline/queries/cross/cacem/tags.sql
+++ datascience/src/pipeline/queries/cross/cacem/tags.sql
@@ -3,12 +3,6 @@ SELECT
     "name",
     parent_id,
     started_at,
-    ended_at,
-    md5(
-        COALESCE("name"::VARCHAR, '') ||
-        COALESCE(parent_id::INT, 0) ||
-        COALESCE(started_at::TIMESTAMP, NOW()) ||
-        COALESCE(ended_at::TIMESTAMP, NOW())
-  ) AS cacem_row_hash
+    ended_at
 FROM prod.tags
 WHERE id IN :ids
