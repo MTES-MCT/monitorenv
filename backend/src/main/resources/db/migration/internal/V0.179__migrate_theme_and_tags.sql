a18d305c0bb3fcfcd32a189b91a1bb6b71becd00
Maxime Perrault <maxperrault94@gmail.com>
2025-05-22 16:33:46 +0200

    tech: move themes and tags migration from test migration file to prod migration file

---
+++ backend/src/main/resources/db/migration/internal/V0.179__migrate_theme_and_tags.sql
@@ -0,0 +1,34 @@
+-- INSERTING TAGS <-> VIGILANCES AREAS FROM CURRENT VIGILANCES AREAS
+INSERT INTO tags_vigilance_areas (tags_id, vigilance_areas_id)
+SELECT t.id, va.id
+FROM vigilance_areas va
+         INNER JOIN tags t ON t.name = ANY (va.themes);
+
+-- INSERTING THEMES <-> ENV ACTION FROM control_plans
+INSERT INTO themes_env_actions (env_actions_id, themes_id)
+SELECT DISTINCT eacpt.env_action_id, t.id
+FROM env_actions_control_plan_themes eacpt
+         INNER JOIN control_plan_themes cpt ON eacpt.theme_id = cpt.id
+         INNER JOIN themes t ON t.control_plan_themes_id = cpt.id;
+
+INSERT INTO themes_env_actions (env_actions_id, themes_id)
+SELECT DISTINCT eacpst.env_action_id, t.id
+FROM public.env_actions_control_plan_sub_themes eacpst
+         INNER JOIN control_plan_sub_themes cpst ON eacpst.subtheme_id = cpst.id
+         INNER JOIN themes t ON t.control_plan_sub_themes_id = cpst.id;
+
+INSERT INTO themes_env_actions (env_actions_id, themes_id)
+SELECT DISTINCT eacpt.env_action_id, t.id
+FROM public.env_actions_control_plan_tags eacpt
+         INNER JOIN control_plan_tags cpt ON eacpt.tag_id = cpt.id
+         INNER JOIN themes t ON t.control_plan_tags_id = cpt.id;
+
+INSERT INTO themes_reportings (reportings_id, themes_id)
+SELECT DISTINCT reporting.id, reporting.control_plan_theme_id
+FROM public.reportings reporting;
+
+INSERT INTO themes_reportings (reportings_id, themes_id)
+SELECT DISTINCT rcpst.reporting_id, t.id
+FROM public.reportings_control_plan_sub_themes rcpst
+         INNER JOIN control_plan_sub_themes cpst ON rcpst.subtheme_id = cpst.id
+         INNER JOIN themes t ON t.control_plan_sub_themes_id = cpst.id;
--- backend/src/main/resources/db/testdata/V666.21__insert_dummy_themes.sql
+++ backend/src/main/resources/db/testdata/V666.21__insert_dummy_themes.sql
@@ -4,12 +4,6 @@ FROM control_plan_themes;
 
 select setval('themes_id_seq', (SELECT MAX(id) FROM themes));
 
-INSERT INTO themes_env_actions (env_actions_id, themes_id)
-SELECT DISTINCT eacpt.env_action_id, t.id
-FROM env_actions_control_plan_themes eacpt
-         INNER JOIN control_plan_themes cpt ON eacpt.theme_id = cpt.id
-         INNER JOIN themes t ON t.control_plan_themes_id = cpt.id;
-
 INSERT INTO themes (name, parent_id, started_at, ended_at, control_plan_sub_themes_id)
 SELECT DISTINCT subtheme, theme_id, '2023-01-01 00:00:00'::timestamp, '2023-12-31 23:59:59'::timestamp, id
 FROM control_plan_sub_themes
@@ -25,31 +19,9 @@ SELECT DISTINCT subtheme, theme_id, '2025-01-01 00:00:00'::timestamp, '2025-12-3
 FROM control_plan_sub_themes
 where year = 2025;
 
-INSERT INTO themes_env_actions (env_actions_id, themes_id)
-SELECT DISTINCT eacpst.env_action_id, t.id
-FROM public.env_actions_control_plan_sub_themes eacpst
-         INNER JOIN control_plan_sub_themes cpst ON eacpst.subtheme_id = cpst.id
-         INNER JOIN themes t ON t.control_plan_sub_themes_id = cpst.id;
-
 INSERT INTO themes (name, parent_id, started_at, ended_at, control_plan_tags_id)
 SELECT DISTINCT tag, theme_id, '2023-01-01 00:00:00'::timestamp, '2099-12-31 23:59:59'::timestamp, id
 FROM control_plan_tags;
-
-INSERT INTO themes_env_actions (env_actions_id, themes_id)
-SELECT DISTINCT eacpt.env_action_id, t.id
-FROM public.env_actions_control_plan_tags eacpt
-         INNER JOIN control_plan_tags cpt ON eacpt.tag_id = cpt.id
-         INNER JOIN themes t ON t.control_plan_tags_id = cpt.id;
-
-INSERT INTO themes_reportings (reportings_id, themes_id)
-SELECT DISTINCT reporting.id, reporting.control_plan_theme_id
-FROM public.reportings reporting;
-
-INSERT INTO themes_reportings (reportings_id, themes_id)
-SELECT DISTINCT rcpst.reporting_id, t.id
-FROM public.reportings_control_plan_sub_themes rcpst
-         INNER JOIN control_plan_sub_themes cpst ON rcpst.subtheme_id = cpst.id
-         INNER JOIN themes t ON t.control_plan_sub_themes_id = cpst.id;
 
 INSERT INTO themes_vigilance_areas (vigilance_areas_id, themes_id)
 VALUES (2, 108),
