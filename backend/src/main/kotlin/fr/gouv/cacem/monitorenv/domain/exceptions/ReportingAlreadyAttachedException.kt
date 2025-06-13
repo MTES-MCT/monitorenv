7a72869b9a615264951581a8f7f732cfe3adcac1
Claire Dagan <clairedagan@gmail.com>
2025-06-13 14:19:27 +0200

    [Tech] replace ReportingAlreadyAttachedException by BackendUsageException

--- backend/src/main/kotlin/fr/gouv/cacem/monitorenv/domain/exceptions/ReportingAlreadyAttachedException.kt
+++
@@ -1,7 +0,0 @@
-package fr.gouv.cacem.monitorenv.domain.exceptions
-
-/** Thrown when attempting to attach a reporting already attached to a mission. */
-@Deprecated("Use `BackendUsageException` instead.")
-class ReportingAlreadyAttachedException(
-    message: String,
-) : RuntimeException(message)
