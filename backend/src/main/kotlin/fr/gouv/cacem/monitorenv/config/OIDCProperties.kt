5bc8137634d417e55aac17122b7815deea6d170c
Claire Dagan <clairedagan@gmail.com>
2025-07-08 13:49:17 +0200

    [Tech] replace application.properties by application.yml and add environment variable

--- backend/src/main/kotlin/fr/gouv/cacem/monitorenv/config/OIDCProperties.kt
+++ backend/src/main/kotlin/fr/gouv/cacem/monitorenv/config/OIDCProperties.kt
@@ -1,13 +1,15 @@
 package fr.gouv.cacem.monitorenv.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.stereotype.Component
+import org.springframework.context.annotation.Configuration
 
-@Component
+@Configuration
 @ConfigurationProperties(prefix = "monitorenv.oidc")
 class OIDCProperties {
     var enabled: Boolean? = false
-    var userinfoEndpoint: String? = null
-    var issuerUri: String? = null
     var cacheInMinutes: Int = 120
+    var loginUrl: String = ""
+    var successUrl: String = ""
+    var errorUrl: String = ""
+    var authorizedSirets: List<String> = listOf()
 }
