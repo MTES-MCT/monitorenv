b63e0c3993fd24de09e2fc2e2cac648764afc8f2
Claire Dagan <clairedagan@gmail.com>
2025-07-10 17:18:34 +0200

    [Mission] update dynamically mission form depending on event type

--- frontend/src/features/Mission/components/MissionForm/hooks/useListenMissionEventUpdates.ts
+++ frontend/src/features/Mission/components/MissionForm/hooks/useListenMissionEventUpdates.ts
@@ -5,14 +5,19 @@ import { useAppSelector } from '../../../../../hooks/useAppSelector'
 import { missionEventListener } from '../sse'
 
 const MISSION_UPDATES_URL = `/api/v1/missions/sse`
-const MISSION_UPDATE_EVENT = `MISSION_UPDATE`
-const FULL_MISSION_UPDATE_EVENT = `FULL_MISSION_UPDATE`
+export const MISSION_UPDATE_EVENT = `MISSION_UPDATE`
+export const FULL_MISSION_UPDATE_EVENT = `FULL_MISSION_UPDATE`
 
 export function useListenMissionEventUpdates() {
   const isListeningToEvents = useAppSelector(state => state.missionForms.isListeningToEvents)
   const eventSourceRef = useRef<EventSource>()
-  const { contextMissionEvent, setMissionEventInContext } = useMissionEventContext()
-  const listener = useRef(missionEventListener(mission => setMissionEventInContext(mission)))
+  const { contextMissionEvent, setEventType, setMissionEventInContext } = useMissionEventContext()
+  const listener = useRef(
+    missionEventListener((mission, event) => {
+      setMissionEventInContext(mission)
+      setEventType(event)
+    })
+  )
 
   useEffect(() => {
     eventSourceRef.current = new EventSource(MISSION_UPDATES_URL)
@@ -32,13 +37,18 @@ export function useListenMissionEventUpdates() {
       eventSourceRef.current?.removeEventListener(MISSION_UPDATE_EVENT, listener.current)
       eventSourceRef.current?.removeEventListener(FULL_MISSION_UPDATE_EVENT, listener.current)
       setMissionEventInContext(undefined)
+      setEventType(undefined)
 
       return
     }
-    listener.current = missionEventListener(mission => setMissionEventInContext(mission))
+    listener.current = missionEventListener((mission, event) => {
+      setMissionEventInContext(mission)
+      setEventType(event)
+    })
+
     eventSourceRef.current?.addEventListener(MISSION_UPDATE_EVENT, listener.current)
     eventSourceRef.current?.addEventListener(FULL_MISSION_UPDATE_EVENT, listener.current)
-  }, [isListeningToEvents, setMissionEventInContext])
+  }, [isListeningToEvents, setMissionEventInContext, setEventType])
 
   return contextMissionEvent
 }
