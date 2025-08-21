250381e96c3b3a3cc0a199a84ad95893fa641cf1
Maxime Perrault <maxperrault94@gmail.com>
2025-08-21 14:35:54 +0200

    feat: create beach flow

---
+++ datascience/tests/test_pipeline/test_flows/test_beaches.py
@@ -0,0 +1,19 @@
+from geopandas.testing import assert_geodataframe_equal
+
+from src.pipeline.flows.beaches import extract_beaches, load_beaches
+from src.read_query import read_query
+
+def test_load_localized_areas(create_cacem_tables):
+  beaches = extract_beaches.run()
+  assert beaches.shape[0] == 1
+
+  load_beaches.run(beaches)
+  imported_beaches = read_query(
+      db="monitorenv_remote",
+      query="SELECT id, nom, insee, nom_offici, geom, code_posta FROM beaches",
+      backend="geopandas",
+      geom_col="geom",
+      crs=4326,
+  )
+
+  assert_geodataframe_equal(beaches, imported_beaches) 
--- infra/init/geoserver_init_layers.sh
+++ infra/init/geoserver_init_layers.sh
@@ -344,3 +344,16 @@ curl -v -u ${GEOSERVER_ADMIN_USER}:${GEOSERVER_ADMIN_PASSWORD} -X POST http://${
 }
 EOF
 
+curl -v -u ${GEOSERVER_ADMIN_USER}:${GEOSERVER_ADMIN_PASSWORD} -X POST http://${GEOSERVER_HOST}:${GEOSERVER_PORT}/geoserver/rest/workspaces/monitorenv/datastores/monitorenv_postgis/featuretypes -H  "accept: text/html" -H  "content-type: application/json" -d @- << EOF
+{
+  "featureType": {
+    "name": "beaches",
+    "nativeName": "beaches",
+    "title": "beaches",
+    "nativeCRS": "EPSG:4326",
+    "srs": "EPSG:4326",
+    "enabled": true,
+  }
+}
+EOF
+
