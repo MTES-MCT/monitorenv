ead027641e0605e52475185c1347fbcc50ca43e9
Maxime Perrault <maximeperrault.dev@gmail.com>
2025-10-16 16:52:25 +0200

    feat: setup vessel search on map

---
+++ frontend/src/features/LocateOnMap/VesselSearchItem.tsx
@@ -0,0 +1,99 @@
+import countries from 'i18n-iso-countries'
+import Highlighter from 'react-highlight-words'
+import styled from 'styled-components'
+
+type VesselSearchItemProps = {
+  flag?: string
+  immatriculation?: string
+  imo?: string
+  mmsi?: string
+  searchQuery?: string
+  vesselName?: string
+}
+
+export function VesselSearchItem({ flag, immatriculation, imo, mmsi, searchQuery, vesselName }: VesselSearchItemProps) {
+  return (
+    <Wrapper>
+      <Name $isUnknown={!vesselName}>
+        {!!flag && (
+          <Flag
+            rel="preload"
+            src={`/flags/${flag.substring(0, 2).toLowerCase()}.svg`}
+            title={countries.getName(flag, 'fr')}
+          />
+        )}
+        <Highlighter
+          autoEscape
+          highlightClassName="highlight"
+          searchWords={searchQuery ? [searchQuery] : []}
+          textToHighlight={vesselName ?? 'SANS NOM'}
+        />
+      </Name>
+      <Identities>
+        <span>
+          <Identity
+            $isUnknown={!mmsi}
+            autoEscape
+            highlightClassName="highlight"
+            searchWords={searchQuery ? [searchQuery] : []}
+            textToHighlight={mmsi || 'Inconnu'}
+          />{' '}
+          (MMSI)
+        </span>
+        <span>
+          <Identity
+            $isUnknown={!immatriculation}
+            autoEscape
+            highlightClassName="highlight"
+            searchWords={searchQuery ? [searchQuery] : []}
+            textToHighlight={immatriculation || 'Inconnu'}
+          />{' '}
+          (Immat.)
+        </span>
+        <span>
+          <Identity
+            $isUnknown={!imo}
+            autoEscape
+            highlightClassName="highlight"
+            searchWords={searchQuery ? [searchQuery] : []}
+            textToHighlight={imo || 'Inconnu'}
+          />{' '}
+          (IMO)
+        </span>
+      </Identities>
+    </Wrapper>
+  )
+}
+
+const Flag = styled.img<{
+  rel?: 'preload'
+}>`
+  font-size: 25px;
+  width: 26px;
+  border: 1px solid ${p => p.theme.color.lightGray};
+`
+
+const Name = styled.span<{ $isUnknown?: boolean }>`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  font-weight: 500;
+  ${p => p.$isUnknown && `font-style: italic;`}
+`
+
+const Identities = styled.span`
+  display: flex;
+  color: ${p => p.theme.color.slateGray};
+  justify-content: space-between;
+`
+
+const Identity = styled(Highlighter)<{ $isUnknown?: boolean }>`
+  ${p => p.$isUnknown && `font-style: italic;`}
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  row-gap: 5px;
+`
--- frontend/src/features/LocateOnMap/index.tsx
+++ frontend/src/features/LocateOnMap/index.tsx
@@ -1,94 +1,28 @@
-import { useBeaches } from '@features/LocateOnMap/hook/useBeaches'
-import {
-  Accent,
-  CustomSearch,
-  Icon,
-  IconButton,
-  OPENLAYERS_PROJECTION,
-  Search,
-  Size,
-  WSG84_PROJECTION
-} from '@mtes-mct/monitor-ui'
-import { getColorWithAlpha } from '@utils/utils'
-import { transformExtent } from 'ol/proj'
+import { SearchLocation } from '@features/LocateOnMap/SearchLocation'
+import { SearchSwitcher, SearchType } from '@features/LocateOnMap/SearchSwitcher'
+import { SearchVessel } from '@features/LocateOnMap/SearchVessels'
+import { Accent, Icon, IconButton, Size } from '@mtes-mct/monitor-ui'
 import { useState } from 'react'
 import styled from 'styled-components'
 
-import { getPlaceCoordinates, useGooglePlacesAPI } from '../../api/googlePlacesAPI/googlePlacesAPI'
-import { setFitToExtent, setLocateOnMap } from '../../domain/shared_slices/Map'
-import { useAppDispatch } from '../../hooks/useAppDispatch'
 import { useAppSelector } from '../../hooks/useAppSelector'
 
-import type { Extent } from 'ol/extent'
+export const isVesselsEnabled = () => import.meta.env.FRONTEND_VESSELS_ENABLED === 'true'
 
 export function LocateOnMap() {
-  const dispatch = useAppDispatch()
+  const [searchType, setSearchType] = useState<SearchType>(SearchType.PLACES)
   const hasFullHeightRightDialogOpen = useAppSelector(state => state.mainWindow.hasFullHeightRightDialogOpen)
   const isRightMenuOpened = useAppSelector(state => state.mainWindow.isRightMenuOpened)
-  const locateOnMap = useAppSelector(state => state.map.locateOnMap)
-  const [searchedLocation, setSearchedLocation] = useState<string | undefined>(undefined)
-  const results = useGooglePlacesAPI(searchedLocation)
-  const { beaches, error, options: beachesResults } = useBeaches()
-
-  const handleSelectLocation = async (location: { id: string; name: string } | undefined) => {
-    if (!location || !location?.id) {
-      dispatch(setLocateOnMap(undefined))
-
-      return
-    }
-    let extent: Extent | undefined
-
-    if (location.id.startsWith('beaches')) {
-      const selectedBeach = beaches.find(beach => beach.id === location.id)
-      if (selectedBeach) {
-        extent = transformExtent(selectedBeach.bbox, WSG84_PROJECTION, OPENLAYERS_PROJECTION)
-      }
-    } else {
-      const place = await getPlaceCoordinates(location.id)
-      if (place?.bbox) {
-        extent = transformExtent(place.bbox, WSG84_PROJECTION, OPENLAYERS_PROJECTION)
-      }
-    }
-    if (extent) {
-      dispatch(setLocateOnMap({ extent, location }))
-      dispatch(setFitToExtent(extent))
-    }
-  }
-
-  const onQuery = (searchQuery: string | undefined) => {
-    setSearchedLocation(searchQuery)
-  }
-
-  const options = [...results, ...beachesResults]
-
-  const locateOnMapCustomSearch = new CustomSearch(options ?? [], ['label'])
 
   return (
     <Wrapper $hasFullHeightRightDialogOpen={hasFullHeightRightDialogOpen} $isRightMenuOpened={isRightMenuOpened}>
-      <StyledSearch
-        key="location-search"
-        customSearch={locateOnMapCustomSearch}
-        data-cy="location-search-input"
-        error={error}
-        isLabelHidden
-        isLight
-        isSearchIconHidden
-        label="Rechercher un lieu"
-        name="search-place"
-        onChange={handleSelectLocation}
-        onQuery={onQuery}
-        options={options}
-        optionValueKey="name"
-        placeholder="rechercher un lieu (port, lieu-dit, baie...)"
-        size={Size.LARGE}
-        value={locateOnMap?.location}
-      />
-      <StyledIconButton
-        accent={Accent.PRIMARY}
-        Icon={Icon.Search}
-        size={Size.LARGE}
-        title="rechercher un lieu (port, lieu-dit, baie...)"
-      />
+      {searchType === SearchType.PLACES && <SearchLocation />}
+      {searchType === SearchType.VESSELS && <SearchVessel />}
+
+      {isVesselsEnabled() && (
+        <SearchSwitcher onChange={nextSearchType => setSearchType(nextSearchType)} searchType={searchType} />
+      )}
+      <StyledIconButton accent={Accent.PRIMARY} Icon={Icon.Search} size={Size.LARGE} tabIndex={-1} title="Rechercher" />
     </Wrapper>
   )
 }
@@ -104,11 +38,6 @@ const Wrapper = styled.div<{
     p.$hasFullHeightRightDialogOpen ? (p.$isRightMenuOpened ? 560 : 512) : 10}px;
   top: 10px;
   transition: right 0.5s ease-out;
-  width: 365px;
-`
-const StyledSearch = styled(Search)`
-  box-shadow: 0px 3px 6px ${p => getColorWithAlpha(p.theme.color.slateGray, 0.25)};
-  flex-grow: 1;
 `
 
 // TODO delete padding when Monitor-ui component have good padding
