c6257733711a496b164e665229451ce9b9340e4f
Vincent <vincent.chery@m4x.org>
2025-11-25 17:35:07 +0100

    Add main and deployments

---
+++ pipeline/main.py
@@ -0,0 +1,12 @@
+from prefect import deploy
+
+from config import DOCKER_IMAGE, MONITORENV_VERSION
+from src.deployments import deployments
+
+if __name__ == "__main__":
+    deploy(
+        *deployments,
+        image=f"{DOCKER_IMAGE}:{MONITORENV_VERSION}",
+        build=False,
+        work_pool_name="monitorenv",
+    )
---
+++ pipeline/src/deployments.py
@@ -0,0 +1,71 @@
+from dataclasses import dataclass, field
+from typing import List
+
+from prefect import Flow
+from prefect.client.schemas.objects import (
+    ConcurrencyLimitConfig,
+    ConcurrencyLimitStrategy,
+)
+from prefect.runner.storage import LocalStorage
+from prefect.schedules import Schedule
+
+from config import (
+    HOST_ENV_FILE_LOCATION,
+    PREFECT_API_URL,
+    VESSEL_FILES_DIRECTORY,
+    VESSEL_FILES_GID,
+)
+from src.flows.control_objectives import control_objectives_flow
+
+################################# List flows to deploy ################################
+
+
+def default_concurrency_limit() -> ConcurrencyLimitConfig:
+    return ConcurrencyLimitConfig(
+        limit=1, collision_strategy=ConcurrencyLimitStrategy.CANCEL_NEW
+    )
+
+
+@dataclass
+class FlowAndSchedules:
+    flow: Flow
+    schedules: List[Schedule] = None
+    concurrency_limit: ConcurrencyLimitConfig = field(
+        default_factory=default_concurrency_limit
+    )
+
+
+flows_to_deploy = [
+    FlowAndSchedules(flow=control_objectives_flow),
+]
+
+
+deployments = [
+    flow_to_deploy.flow.to_deployment(
+        name=flow_to_deploy.flow.name,
+        schedules=flow_to_deploy.schedules,
+        concurrency_limit=flow_to_deploy.concurrency_limit,
+    )
+    for flow_to_deploy in flows_to_deploy
+]
+
+################### Define flows' run config ####################
+for deployment in deployments:
+    deployment.job_variables = {
+        "env": {"PREFECT_API_URL": PREFECT_API_URL},
+        "volumes": [
+            f"{HOST_ENV_FILE_LOCATION}:/home/monitorenv-pipeline/pipeline/.env"
+        ],
+        "auto_remove": True,
+        "image_pull_policy": "IfNotPresent",
+    }
+    deployment.work_pool_name = "monitorenv"
+    deployment.storage = LocalStorage("/home/monitorenv-pipeline/pipeline")
+
+    if deployment.name in ("Control objectives",):
+        deployment.job_variables["container_create_kwargs"] = {
+            "group_add": [VESSEL_FILES_GID]
+        }
+        deployment.job_variables["volumes"].append(
+            f"{VESSEL_FILES_DIRECTORY}:/home/monitorenv-pipeline/pipeline/src/data"
+        )
