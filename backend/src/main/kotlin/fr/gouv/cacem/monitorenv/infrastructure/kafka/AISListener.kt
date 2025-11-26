f94c8536b834c89c0cd1f53dfd73353ed2c941fa
Maxime Perrault <maximeperrault.dev@gmail.com>
2025-11-26 16:33:07 +0100

    tech: add service docker to run e2e test and local dev mode

--- backend/src/main/kotlin/fr/gouv/cacem/monitorenv/infrastructure/kafka/AISListener.kt
+++ backend/src/main/kotlin/fr/gouv/cacem/monitorenv/infrastructure/kafka/AISListener.kt
@@ -1,14 +1,20 @@
 package fr.gouv.cacem.monitorenv.infrastructure.kafka
 
+import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
 
 @Component
+@ConditionalOnProperty(value = ["monitorenv.kafka.ais.enabled"], havingValue = "true")
 class AISListener {
+    private val logger = LoggerFactory.getLogger(AISListener::class.java)
+
     val messages = mutableListOf<String>()
 
     @KafkaListener(topics = ["ais"])
     fun listenAIS(message: String) {
         messages.add(message)
+        logger.info("AIS received message: $message")
     }
 }
