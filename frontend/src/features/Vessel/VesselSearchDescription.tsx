fb450c35eba41ce4419328be070cd8ca0d51701e
Maxime Perrault <maximeperrault.dev@gmail.com>
2025-11-27 11:08:28 +0100

    feat: add category to vessel identity, remove country trigram, hide commercialname if not exist

--- frontend/src/features/Vessel/VesselSearchDescription.tsx
+++ frontend/src/features/Vessel/VesselSearchDescription.tsx
@@ -1,15 +1,18 @@
 import { useField } from 'formik'
 import styled from 'styled-components'
 
+import { Vessel } from './types'
+
 import type { Infraction } from '../../domain/entities/missions'
 
 type VesselSearchDescriptionProps = {
+  category?: string
   path: string
 }
 
 const UNKNOWN = 'Inconnu'
 
-export function VesselSearchDescription({ path }: VesselSearchDescriptionProps) {
+export function VesselSearchDescription({ category, path }: VesselSearchDescriptionProps) {
   const [{ value: infraction }] = useField<Infraction>(path)
 
   return (
@@ -26,6 +29,9 @@ export function VesselSearchDescription({ path }: VesselSearchDescriptionProps) 
       <Value>
         {infraction.vesselSize ? `${infraction.vesselSize}m` : UNKNOWN}
         <Description>(Taille)</Description>
+      </Value>
+      <Value>
+        {category ? Vessel.CategoryLabel[category] : UNKNOWN} <Description>(Cat.)</Description>
       </Value>
       <Value>
         {infraction.vesselType ?? UNKNOWN} <Description>(Type)</Description>
