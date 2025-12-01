4fbd9897a06cb54a96b60f0d0512dcd2a76500ff
Vincent <vincent.chery@m4x.org>
2025-12-01 17:37:15 +0100

    Convert beaches and competence_cross_areas flows

---
+++ pipeline/src/flows/competence_cross_areas.py
@@ -0,0 +1,33 @@
+import geopandas as gpd
+from prefect import flow, get_run_logger, task
+
+from src.generic_tasks import extract, load
+
+
+@task
+def extract_competence_cross_areas() -> gpd.GeoDataFrame:
+    return extract(
+        db_name="cacem_local",
+        query_filepath="cross/cacem/competence_cross_areas.sql",
+        backend="geopandas",
+        geom_col="geom",
+        crs=4326,
+    )
+
+
+@task
+def load_competence_cross_areas(competence_cross_areas: gpd.GeoDataFrame):
+    load(
+        competence_cross_areas,
+        table_name="competence_cross_areas",
+        schema="public",
+        db_name="monitorenv_remote",
+        logger=get_run_logger(),
+        how="replace",
+    )
+
+
+@flow(name="Competence cross areas")
+def competence_cross_areas_flow():
+    competence_cross_areas = extract_competence_cross_areas()
+    load_competence_cross_areas(competence_cross_areas)
