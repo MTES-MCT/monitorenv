abe1fef7c24e7da52477bdf2e153835be4357268
Vincent <vincent.chery@m4x.org>
2025-12-02 09:02:37 +0100

    Convert facade areas flow

---
+++ pipeline/src/flows/facade_areas.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from prefect import flow, get_run_logger, task
+
+from src.generic_tasks import extract, load
+
+
+@task
+def extract_facade_areas() -> pd.DataFrame:
+    """
+    Extract facade areas from the monitorfish_local (cross) database as a DataFrame.
+
+    Returns:
+        pd.DataFrame: GeoDataFrame of facade areas
+    """
+
+    return extract(
+        db_name="monitorfish_local",
+        query_filepath="cross/cnsp/facade_areas.sql",
+    )
+
+
+@task
+def load_facade_areas(facade_areas: pd.DataFrame):
+
+    logger = get_run_logger()
+
+    load(
+        facade_areas,
+        table_name="facade_areas_subdivided",
+        schema="public",
+        db_name="monitorenv_remote",
+        logger=logger,
+        how="replace",
+    )
+
+
+@flow(name="Facade areas")
+def facade_areas_flow():
+    facade_areas = extract_facade_areas()
+    load_facade_areas(facade_areas)
