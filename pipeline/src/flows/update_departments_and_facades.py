a63d056bbf1f7d28e95b002f07750b5775743025
Vincent <vincent.chery@m4x.org>
2025-12-02 11:40:39 +0100

    Convert update_departments_and_facades flow

---
+++ pipeline/src/flows/update_departments_and_facades.py
@@ -0,0 +1,13 @@
+from prefect import flow
+
+from config import QUERIES_LOCATION
+from src.shared_tasks.etl import run_sql_script
+
+
+@flow(name="Update departments and façades for missions and envActions")
+def update_departments_and_facades_flow():
+    run_sql_script(QUERIES_LOCATION / "monitorenv/update_missions_facades.sql")
+    run_sql_script(
+        QUERIES_LOCATION / "monitorenv/update_actions_departments.sql"
+    )
+    run_sql_script(QUERIES_LOCATION / "monitorenv/update_actions_facades.sql")
