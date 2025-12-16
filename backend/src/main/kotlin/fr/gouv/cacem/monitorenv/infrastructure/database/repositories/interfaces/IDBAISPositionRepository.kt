ee8f5093b680e698095fd9cf641cb6d75e318c31
Maxime Perrault <maximeperrault.dev@gmail.com>
2025-12-16 16:47:41 +0100

    feat: create hypertable and change primary key

--- backend/src/main/kotlin/fr/gouv/cacem/monitorenv/infrastructure/database/repositories/interfaces/IDBAISPositionRepository.kt
+++ backend/src/main/kotlin/fr/gouv/cacem/monitorenv/infrastructure/database/repositories/interfaces/IDBAISPositionRepository.kt
@@ -1,6 +1,7 @@
 package fr.gouv.cacem.monitorenv.infrastructure.database.repositories.interfaces
 
 import fr.gouv.cacem.monitorenv.infrastructure.database.model.AISPositionModel
+import fr.gouv.cacem.monitorenv.infrastructure.database.model.AISPositionPK
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface IDBAISPositionRepository : JpaRepository<AISPositionModel, Int>
+interface IDBAISPositionRepository : JpaRepository<AISPositionModel, AISPositionPK>
--- backend/src/main/resources/db/migration/internal/V0.199__create_table_ais_positions.sql
+++ backend/src/main/resources/db/migration/internal/V0.199__create_table_ais_positions.sql
@@ -2,24 +2,14 @@
 
 CREATE TABLE public.ais_positions
 (
-    id      SERIAL PRIMARY KEY,
     mmsi    INT,
     coord   GEOMETRY,
     status  TEXT,
     course  DOUBLE PRECISION,
     heading DOUBLE PRECISION,
     speed   DOUBLE PRECISION,
-    ts      TIMESTAMPTZ
+    ts      TIMESTAMPTZ,
+    PRIMARY KEY (mmsi, ts)
 );
 
--- SELECT create_hypertable('ais_positions', by_range('ts', INTERVAL '1 day'), if_not_exists => TRUE);
--- CREATE INDEX ON ais_positions (mmsi, ts DESC);
---
--- ALTER TABLE ais_positions SET (
---     timescaledb.compress,
---     timescaledb.compress_segmentby = 'mmsi',
---     timescaledb.compress_orderby = 'ts DESC'
---     );
---
--- -- Compression automatique après 7 jours
--- SELECT add_compression_policy('ais_positions', INTERVAL '1 month');
+SELECT create_hypertable('ais_positions', by_range('ts', INTERVAL '1 day'), if_not_exists => TRUE);
