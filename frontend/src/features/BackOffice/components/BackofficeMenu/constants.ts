07764f08bb3aaade558fa1d073817f3323866f65
Claire Dagan <claire.dagan.pro@gmail.com>
2026-01-15 12:00:59 +0100

    [Backoffice] update menu

--- frontend/src/features/BackOffice/components/BackofficeMenu/constants.ts
+++ frontend/src/features/BackOffice/components/BackofficeMenu/constants.ts
@@ -3,10 +3,12 @@ export enum BackOfficeMenuKey {
   CONTROL_UNIT_CONTACT_LIST = 'CONTROL_UNIT_CONTACT_LIST',
   CONTROL_UNIT_LIST = 'CONTROL_UNIT_LIST',
   CONTROL_UNIT_RESOURCE_LIST = 'CONTROL_UNIT_RESOURCE_LIST',
+  REGULATORY_AREA_LIST = 'REGULATORY_AREA_LIST',
   STATION_LIST = 'STATION_LIST'
 }
 
 export const BACK_OFFICE_MENU_LABEL: Record<BackOfficeMenuKey, string> = {
+  [BackOfficeMenuKey.REGULATORY_AREA_LIST]: 'Zones réglementaires',
   [BackOfficeMenuKey.ADMINISTRATION_LIST]: 'Administrations',
   [BackOfficeMenuKey.CONTROL_UNIT_CONTACT_LIST]: 'Contacts',
   [BackOfficeMenuKey.CONTROL_UNIT_RESOURCE_LIST]: 'Moyens',
@@ -15,6 +17,7 @@ export const BACK_OFFICE_MENU_LABEL: Record<BackOfficeMenuKey, string> = {
 }
 
 export const BACK_OFFICE_MENU_PATH: Record<BackOfficeMenuKey, string> = {
+  [BackOfficeMenuKey.REGULATORY_AREA_LIST]: '/regulatory_areas',
   [BackOfficeMenuKey.ADMINISTRATION_LIST]: '/administrations',
   [BackOfficeMenuKey.CONTROL_UNIT_CONTACT_LIST]: '/control_unit_contacts',
   [BackOfficeMenuKey.CONTROL_UNIT_RESOURCE_LIST]: '/control_unit_resources',
