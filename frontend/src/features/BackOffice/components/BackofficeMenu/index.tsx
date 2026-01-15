58034029697898ea776783b8ae909084e30be884
Claire Dagan <claire.dagan.pro@gmail.com>
2026-01-15 12:00:59 +0100

    [Backoffice] update menu

--- frontend/src/features/BackOffice/components/BackofficeMenu/index.tsx
+++ frontend/src/features/BackOffice/components/BackofficeMenu/index.tsx
@@ -1,18 +1,43 @@
-import { NavLink } from 'react-router'
+import { Icon } from '@mtes-mct/monitor-ui'
+import { NavLink, useLocation } from 'react-router'
 import styled from 'styled-components'
 
 import { BACK_OFFICE_MENU_LABEL, BACK_OFFICE_MENU_PATH, BackOfficeMenuKey } from './constants'
 
 export function BackOfficeMenu() {
+  const locationPath = useLocation().pathname
+
   return (
     <Wrapper>
-      <StyledNavLink to={`/backoffice${BACK_OFFICE_MENU_PATH[BackOfficeMenuKey.ADMINISTRATION_LIST]}`}>
+      <StyledNavLink
+        $isActive={
+          locationPath === `/backoffice${BACK_OFFICE_MENU_PATH[BackOfficeMenuKey.REGULATORY_AREA_LIST]}` ||
+          locationPath === '/backoffice'
+        }
+        to={`/backoffice${BACK_OFFICE_MENU_PATH[BackOfficeMenuKey.REGULATORY_AREA_LIST]}`}
+      >
+        <Icon.MapLayers />
+        {BACK_OFFICE_MENU_LABEL[BackOfficeMenuKey.REGULATORY_AREA_LIST]}
+      </StyledNavLink>
+      <StyledNavLink
+        $isActive={locationPath === `/backoffice${BACK_OFFICE_MENU_PATH[BackOfficeMenuKey.ADMINISTRATION_LIST]}`}
+        to={`/backoffice${BACK_OFFICE_MENU_PATH[BackOfficeMenuKey.ADMINISTRATION_LIST]}`}
+      >
+        <Icon.GroupPerson />
         {BACK_OFFICE_MENU_LABEL[BackOfficeMenuKey.ADMINISTRATION_LIST]}
       </StyledNavLink>
-      <StyledNavLink to={`/backoffice${BACK_OFFICE_MENU_PATH[BackOfficeMenuKey.CONTROL_UNIT_LIST]}`}>
+      <StyledNavLink
+        $isActive={locationPath === `/backoffice${BACK_OFFICE_MENU_PATH[BackOfficeMenuKey.CONTROL_UNIT_LIST]}`}
+        to={`/backoffice${BACK_OFFICE_MENU_PATH[BackOfficeMenuKey.CONTROL_UNIT_LIST]}`}
+      >
+        <Icon.ControlUnit />
         {BACK_OFFICE_MENU_LABEL[BackOfficeMenuKey.CONTROL_UNIT_LIST]}
       </StyledNavLink>
-      <StyledNavLink to={`/backoffice${BACK_OFFICE_MENU_PATH[BackOfficeMenuKey.STATION_LIST]}`}>
+      <StyledNavLink
+        $isActive={locationPath === `/backoffice${BACK_OFFICE_MENU_PATH[BackOfficeMenuKey.STATION_LIST]}`}
+        to={`/backoffice${BACK_OFFICE_MENU_PATH[BackOfficeMenuKey.STATION_LIST]}`}
+      >
+        <Icon.Pinpoint />
         {BACK_OFFICE_MENU_LABEL[BackOfficeMenuKey.STATION_LIST]}
       </StyledNavLink>
     </Wrapper>
@@ -24,27 +49,34 @@ const Wrapper = styled.div`
   border-right: solid 1px black;
   color: ${p => p.theme.color.gainsboro};
   display: flex;
+  font-size: 13px;
   flex-direction: column;
-  letter-spacing: 0.5px;
   line-height: 1;
-  padding: 16px 24px;
-  width: 200px;
+  width: 228px;
 `
 
-const StyledNavLink = styled(NavLink)`
+const StyledNavLink = styled(NavLink)<{ $isActive?: boolean }>`
   align-items: center;
-  color: ${p => p.theme.color.gainsboro};
+  background-color: ${p => (p.$isActive ? p.theme.color.blueGray : 'none')};
+  border-bottom: solid 0.5px ${p => p.theme.color.slateGray};
+  color: ${p => (p.$isActive ? p.theme.color.white : p.theme.color.gainsboro)};
   display: flex;
-  height: 45px;
-  text-align: left;
+  gap: 8px;
+  height: 52px;
+  padding: 16px;
 
-  && {
-    color: ${p => p.theme.color.gainsboro};
+  &:hover,
+  &:focus {
+    background-color: ${p => (p.$isActive ? p.theme.color.blueGray : 'rgba(255, 255, 255, 0.125)')};
+    color: ${p => p.theme.color.white};
+    text-decoration: none;
   }
-  &:after {
-    color: ${p => p.theme.color.gainsboro};
-  }
-  &:before {
-    color: ${p => p.theme.color.gainsboro};
+
+  &:first-child {
+    border-top: solid 0.5px ${p => p.theme.color.slateGray};
+    margin-top: 64px;
+    &:hover {
+      border: none;
+    }
   }
 `
