d9655db23ae3de7fb5077145c68e097a7ee2547a
Maxime Perrault <maximeperrault.dev@gmail.com>
2026-01-22 09:14:03 +0100

    fix: make test more robust to date changing

--- frontend/cypress/e2e/side_window/vigilance_areas_list/filters.spec.ts
+++ frontend/cypress/e2e/side_window/vigilance_areas_list/filters.spec.ts
@@ -87,13 +87,14 @@ context('Side Window > Vigilance Areas List > Filter Bar', () => {
   })
 
   it('Should filter vigilance areas by type', () => {
-    cy.fill('Type de zone de vigilance', ['Aucune période de vigilance en cours'])
-    cy.getDataCy('vigilance-area-row').should('have.length', 1)
-
     cy.fill('Type de zone de vigilance', ['Période de vigilance critique en cours'])
     cy.getDataCy('vigilance-area-row').should('have.length', 1)
 
     cy.fill('Type de zone de vigilance', ['Période de vigilance simple en cours'])
-    cy.getDataCy('vigilance-area-row').should('have.length', 2)
+    cy.getDataCy('vigilance-area-row').should('have.length', 3)
+
+    cy.fill('Période de vigilance', 'Cette année')
+    cy.fill('Type de zone de vigilance', ['Aucune période de vigilance en cours'])
+    cy.getDataCy('vigilance-area-row').should('have.length.to.be.greaterThan', 0)
   })
 })
