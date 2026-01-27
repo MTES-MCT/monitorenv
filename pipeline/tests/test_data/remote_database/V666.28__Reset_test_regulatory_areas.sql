30aa5e0e41d6357f015af7a72c008344f7e424ec
Claire Dagan <claire.dagan.pro@gmail.com>
2026-01-27 11:51:39 +0100

    [RegulatoryAreas] add test to flow

--- pipeline/tests/test_data/remote_database/V666.28__Reset_test_regulatory_areas.sql
+++ pipeline/tests/test_data/remote_database/V666.28__Reset_test_regulatory_areas.sql
@@ -7,3 +7,8 @@ DELETE FROM regulations_cacem;
 
 insert into public.regulations_cacem (id, geom, url, layer_name, facade, ref_reg, edition, editeur, source, observation, thematique, date, duree_validite, date_fin, temporalite, type, plan, poly_name, resume, row_hash) values (1, 'MULTIPOLYGON(((0 0,10 0,10 10,0 10,0 0)))', 'url1', 'layer_name1', 'MED', 'ref_reg1', '2025-01-01', 'editrice1', 'source1', 'observation1', 'thematique1', '2010-06-01', '10 ans', '2024-01-01', 'temporaire', 'Décret', 'PIRC', 'poly_name1', 'resume1', '4ccc708e6a0c4f311dd7e537e282f7f6');
 insert into public.regulations_cacem (id, geom, url, layer_name, facade, ref_reg, edition, editeur, source, observation, thematique, date, duree_validite, date_fin, temporalite, type, plan, poly_name, resume, row_hash) values (2, 'MULTIPOLYGON(((120 -20,135 -20,135 -10,120 -10,120 -20)))', 'url2', 'layer_name2', 'NAMO', 'ref_reg2', '2025-01-01', 'editeur2', 'source2', 'observation2', 'thematique2', '2005-07-01', '20 ans', '2025-01-01', 'permanent', 'Arrêté préfectoral', 'PSCEM', 'poly_name2', 'resume2', '8c3842144dfaf46ead39bfa628dd9513');
+
+DELETE FROM regulatory_areas;
+
+insert into public.regulatory_areas (id, geom,ref_reg, edition_cacem, row_hash) values (1, 'MULTIPOLYGON(((0 0,10 0,10 10,0 10,0 0)))', 'ref_reg1', '2025-01-01', '4ccc708e6a0c4f311dd7e537e282f7f6');
+insert into public.regulatory_areas (id, geom,ref_reg, edition_cacem, row_hash) values (2, 'MULTIPOLYGON(((120 -20,135 -20,135 -10,120 -10,120 -20)))', 'ref_reg2', '2025-01-01', '8c3842144dfaf46ead39bfa628dd9513');
