bfdc9dbca7c7aad1cb01d8a4542f35e706aa5f66
Loup Theron <loup-rodney@hotmail.fr>
2026-02-25 17:05:06 +0100

    Add RAPPORT_NAV to default mission sources

--- backend/src/test/kotlin/fr/gouv/cacem/monitorenv/domain/use_cases/missions/GetMissionsUTest.kt
+++ backend/src/test/kotlin/fr/gouv/cacem/monitorenv/domain/use_cases/missions/GetMissionsUTest.kt
@@ -32,6 +32,7 @@ class GetMissionsUTest {
                 listOf(
                     MissionSourceEnum.MONITORENV,
                     MissionSourceEnum.MONITORFISH,
+                    MissionSourceEnum.RAPPORT_NAV,
                 ),
                 null,
                 null,
@@ -67,6 +68,7 @@ class GetMissionsUTest {
                 listOf(
                     MissionSourceEnum.MONITORENV,
                     MissionSourceEnum.MONITORFISH,
+                    MissionSourceEnum.RAPPORT_NAV,
                 ),
                 null,
                 null,
