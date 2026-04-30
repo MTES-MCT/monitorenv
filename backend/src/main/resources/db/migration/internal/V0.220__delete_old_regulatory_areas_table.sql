589664baec06c4fde8b9a89aee125b0c6ae42b9c
Claire Dagan <claire.dagan.pro@gmail.com>
2026-04-30 11:42:49 +0200

    [RegulatoryAreas] delete old tables and fix test data

--- backend/src/main/resources/db/migration/internal/V0.220__delete_old_regulatory_areas_table.sql
+++ backend/src/main/resources/db/migration/internal/V0.220__delete_old_regulatory_areas_table.sql
@@ -4,3 +4,5 @@ ALTER TABLE dashboard_datas
 ADD CONSTRAINT fk_regulatory_cacem FOREIGN KEY (regulations_cacem_id) REFERENCES public.regulatory_areas (id);
 
 DROP TABLE public.regulations_cacem;
+DROP TABLE public.themes_regulatory_areas;
+DROP TABLE public.tags_regulatory_areas;
