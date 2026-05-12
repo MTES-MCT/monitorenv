6324675ba1a3c7937cf8c008002d6f9d38ff1141
Claire Dagan <claire.dagan.pro@gmail.com>
2026-05-12 09:40:34 +0200

    [Tags] add new tags

---
+++ backend/src/main/resources/db/migration/internal/V0.221__add_tags.sql
@@ -0,0 +1,22 @@
+SELECT
+  setval(
+    'tags_id_seq',
+    (
+      SELECT
+        MAX(id)
+      FROM
+        tags
+    )
+  );
+
+INSERT INTO
+  public.tags (name, parent_id, started_at, ended_at)
+VALUES
+  ('Dauphin', null, '2026-05-12 00:00:00.000000', null),
+  ('Algue', null, '2026-05-12 00:00:00.000000', null),
+  ('Coque', null, '2026-05-12 00:00:00.000000', null),
+  ('Telline', null, '2026-05-12 00:00:00.000000', null),
+  ('Chlordécone', null, '2026-05-12 00:00:00.000000', null),
+  ('Canne à pêche', null, '2026-05-12 00:00:00.000000', null),
+  ('Casier', null, '2026-05-12 00:00:00.000000', null),
+  ('Tortue marine', null, '2026-05-12 00:00:00.000000', null);
