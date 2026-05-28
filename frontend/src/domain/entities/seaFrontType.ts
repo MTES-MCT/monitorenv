0270330e91fdc711377d7ca2dcacbef8600473d0
Maxime Perrault <maximeperrault.dev@gmail.com>
2026-05-28 10:57:52 +0200

    feat: add facade and resolve label instead of value in panel

--- frontend/src/domain/entities/seaFrontType.ts
+++ frontend/src/domain/entities/seaFrontType.ts
@@ -1,8 +1,10 @@
 // TODO change seaFront with Quater
 // TODO Rename that to `SeaFront` and export it from to a `Global` namespace (if we keep domain/ on Env).
 // Don't forget to mirror any update here in the backend enum.
+
 export enum SeaFrontEnum {
   // TODO Why is it not in ALL MAJ? Does the DB has a mix of both cases (`Guadeloupe`, `SOUTH_INDIAN_OCEAN`)?
+  CLIPPERTON = 'Clipperton',
   GUADELOUPE = 'Guadeloupe',
   GUYANE = 'Guyane',
   MARTINIQUE = 'Martinique',
@@ -10,11 +12,17 @@ export enum SeaFrontEnum {
   MED = 'MED',
   MEMN = 'MEMN',
   NAMO = 'NAMO',
+  NEW_CALEDONIA = 'Nouvelle-Calédonie',
+  // eslint-disable-next-line typescript-sort-keys/string-enum
+  FRENCH_POLYNESIA = 'Polynésie Française',
   SA = 'SA',
-  SOUTH_INDIAN_OCEAN = 'SOUTH_INDIAN_OCEAN',
-  UNKNOWN = 'UNKNOWN'
+  SAINT_PIERRE_ET_MIQUELON = 'Saint-Pierre-et-Miquelon',
+  SOUTH_INDIAN_OCEAN = 'Sud Océan Indien',
+  UNKNOWN = 'UNKNOWN',
+  WALLIS_ET_FUTUNA = 'Wallis-et-Futuna'
 }
 export enum SeaFrontLabel {
+  CLIPPERTON = 'Clipperton',
   GUADELOUPE = 'Guadeloupe',
   GUYANE = 'Guyane',
   MARTINIQUE = 'Martinique',
@@ -22,12 +30,21 @@ export enum SeaFrontLabel {
   MED = 'MED',
   MEMN = 'MEMN',
   NAMO = 'NAMO',
+  NEW_CALEDONIA = 'Nouvelle-Calédonie',
+  // eslint-disable-next-line typescript-sort-keys/string-enum
+  FRENCH_POLYNESIA = 'Polynésie Française',
   SA = 'SA',
+  SAINT_PIERRE_ET_MIQUELON = 'Saint-Pierre-et-Miquelon',
   SOUTH_INDIAN_OCEAN = 'Sud Océan Indien',
-  UNKNOWN = 'Façade non renseignée'
+  UNKNOWN = 'Façade non renseignée',
+  WALLIS_ET_FUTUNA = 'Wallis-et-Futuna'
 }
 
 export const SeaFrontLabels = {
+  CLIPPERTON: {
+    label: 'Clipperton',
+    value: 'CLIPPERTON'
+  },
   GUADELOUPE: {
     label: 'Guadeloupe',
     value: 'Guadeloupe'
@@ -56,12 +73,29 @@ export const SeaFrontLabels = {
     label: 'NAMO',
     value: 'NAMO'
   },
+  NEW_CALEDONIA: {
+    label: 'Nouvelle-Calédonie',
+    value: 'NOUVELLE_CALEDONIE'
+  },
+  // eslint-disable-next-line sort-keys-fix/sort-keys-fix
+  FRENCH_POLYNESIA: {
+    label: 'Polynésie Française',
+    value: 'POLYNESIE_FRANCAISE'
+  },
   SA: {
     label: 'SA',
     value: 'SA'
   },
+  SAINT_PIERRE_ET_MIQUELON: {
+    label: 'Saint-Pierre-et-Miquelon',
+    value: 'SAINT_PIERRE_ET_MIQUELON'
+  },
   SOUTH_INDIAN_OCEAN: {
     label: 'Sud Océan Indien',
     value: 'Sud Océan Indien'
+  },
+  WALLIS_ET_FUTUNA: {
+    label: 'Wallis-et-Futuna',
+    value: 'WALLIS_ET_FUTUNA'
   }
 }
