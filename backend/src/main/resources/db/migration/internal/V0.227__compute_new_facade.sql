d1332a858d6cde2ad1d5fb6b83fdfe8030b9b14b
Maxime Perrault <maximeperrault.dev@gmail.com>
2026-07-21 09:35:40 +0200

    fix: add ST_MakeValid to computed seafront

--- backend/src/main/resources/db/migration/internal/V0.227__compute_new_facade.sql
+++ backend/src/main/resources/db/migration/internal/V0.227__compute_new_facade.sql
@@ -1,11 +1,11 @@
 UPDATE regulatory_areas ra
 SET facade = (SELECT fas.facade
               FROM facade_areas_subdivided fas
-              WHERE ST_Intersects(ra.geom, fas.geometry)
+              WHERE ST_Intersects(ST_MakeValid(ra.geom), fas.geometry)
               GROUP BY fas.facade
               ORDER BY SUM(
                                ST_Area(
-                                       ST_Intersection(ra.geom, fas.geometry)::geography
+                                       ST_Intersection(ST_MakeValid(ra.geom), fas.geometry)::geography
                                )
                        ) DESC
               LIMIT 1)
@@ -14,11 +14,11 @@ WHERE ra.geom IS NOT NULL;
 UPDATE vigilance_areas va
 SET sea_front = (SELECT fas.facade
                  FROM facade_areas_subdivided fas
-                 WHERE ST_Intersects(va.geom, fas.geometry)
+                 WHERE ST_Intersects(ST_MakeValid(va.geom), fas.geometry)
                  GROUP BY fas.facade
                  ORDER BY SUM(
                                   ST_Area(
-                                          ST_Intersection(va.geom, fas.geometry)::geography
+                                          ST_Intersection(ST_MakeValid(va.geom), fas.geometry)::geography
                                   )
                           ) DESC
                  LIMIT 1)
@@ -27,11 +27,11 @@ WHERE va.geom IS NOT NULL;
 UPDATE missions m
 SET facade = (SELECT fas.facade
               FROM facade_areas_subdivided fas
-              WHERE ST_Intersects(m.geom, fas.geometry)
+              WHERE ST_Intersects(ST_MakeValid(m.geom), fas.geometry)
               GROUP BY fas.facade
               ORDER BY SUM(
                                ST_Area(
-                                       ST_Intersection(m.geom, fas.geometry)::geography
+                                       ST_Intersection(ST_MakeValid(m.geom), fas.geometry)::geography
                                )
                        ) DESC
               LIMIT 1)
@@ -41,11 +41,11 @@ where m.start_datetime_utc::TIMESTAMPTZ >= '2026-01-01T00:00:00'
 UPDATE env_actions e
 SET facade = (SELECT fas.facade
               FROM facade_areas_subdivided fas
-              WHERE ST_Intersects(e.geom, fas.geometry)
+              WHERE ST_Intersects(ST_MakeValid(e.geom), fas.geometry)
               GROUP BY fas.facade
               ORDER BY SUM(
                                ST_Area(
-                                       ST_Intersection(e.geom, fas.geometry)::geography
+                                       ST_Intersection(ST_MakeValid(e.geom), fas.geometry)::geography
                                )
                        ) DESC
               LIMIT 1)
@@ -56,11 +56,11 @@ where e.action_start_datetime_utc::TIMESTAMPTZ >= '2026-01-01T00:00:00'
 UPDATE reportings r
 SET sea_front = (SELECT fas.facade
                  FROM facade_areas_subdivided fas
-                 WHERE ST_Intersects(r.geom, fas.geometry)
+                 WHERE ST_Intersects(ST_MakeValid(r.geom), fas.geometry)
                  GROUP BY fas.facade
                  ORDER BY SUM(
                                   ST_Area(
-                                          ST_Intersection(r.geom, fas.geometry)::geography
+                                          ST_Intersection(ST_MakeValid(r.geom), fas.geometry)::geography
                                   )
                           ) DESC
                  LIMIT 1)
