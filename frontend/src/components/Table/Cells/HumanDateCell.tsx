535ef995d289b448bfe59893b08f5336faa35454
Claire Dagan <claire.dagan.pro@gmail.com>
2026-07-21 08:55:16 +0200

    [Mission] fix mission dates in mission list

--- frontend/src/components/Table/Cells/HumanDateCell.tsx
+++ frontend/src/components/Table/Cells/HumanDateCell.tsx
@@ -1,10 +1,10 @@
-import { customDayjs } from '@mtes-mct/monitor-ui'
+import { getLocalizedDayjs } from '@mtes-mct/monitor-ui'
 
 export function HumanDateCell({ date }: { date: string | undefined }) {
   if (!date) {
     return <span>-</span>
   }
-  const formattedDate = customDayjs(date).format('DD MMM YY, HH[h]mm')
+  const formattedDate = getLocalizedDayjs(date).format('DD MMM YY, HH[h]mm')
 
   return <span title={formattedDate}>{formattedDate}</span>
 }
