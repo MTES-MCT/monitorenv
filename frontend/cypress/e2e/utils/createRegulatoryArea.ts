e5d1ba110ccdf9b7d03a4c7b374a501bac7abd33
Maxime Perrault <maximeperrault.dev@gmail.com>
2026-07-22 18:44:36 +0200

    fix: test e2e

--- frontend/cypress/e2e/utils/createRegulatoryArea.ts
+++ frontend/cypress/e2e/utils/createRegulatoryArea.ts
@@ -1,16 +1,11 @@
-export function createRegulatoryArea(
-  id: string,
-  groupName: string = 'RNN Iroise',
-  title: string = 'Nouvelle zone réglementaire'
-) {
+export function createRegulatoryArea(id: string, title: string = 'Nouvelle zone réglementaire') {
   cy.fill('Titre de la zone réglementaire', title)
-  cy.fill('Groupe de réglementation', groupName)
   cy.fill('Géométrie', id)
   cy.fill('Façade', 'NAMO')
   cy.fill('Type d’acte administratif', 'Arrêté inter-préfectoral')
   cy.fill('Tags et sous-tags', ['AMP'])
+  cy.fill('Thématiques et sous-thématiques', ['Réglementation de la réserve naturelle'])
   cy.fill('Résumé', 'Résumé de la nouvelle zone réglementaire')
   cy.get('#PIRCType').click()
-
   cy.fill('URL du lien', 'https://www.google.com')
 }
