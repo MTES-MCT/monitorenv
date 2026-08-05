ffe5f455738fadf738fe82525f2fc8bc04f3e06e
Claire Dagan <claire.dagan.pro@gmail.com>
2026-08-05 13:26:12 +0200

    [Data] fix migration

--- backend/src/main/resources/db/migration/internal/V0.229__re_add_regulatory_areas_group.sql
+++ backend/src/main/resources/db/migration/internal/V0.229__re_add_regulatory_areas_group.sql
@@ -1,9 +1,10 @@
 DELETE FROM regulatory_areas_group;
 DELETE FROM regulatory_areas WHERE area_type = 'GROUP';
 
-WITH original_areas AS MATERIALIZED (SELECT id, layer_name, geom
-                                     FROM regulatory_areas),
+WITH original_areas AS MATERIALIZED (SELECT id, layer_name, location, geom
+                                          FROM regulatory_areas),
      grouped_reg AS (SELECT layer_name,
+                                location,
                             ST_Multi(
                                     ST_CollectionExtract(
                                             ST_Collect(ST_CollectionExtract(geom, 3)),
@@ -11,26 +12,29 @@ WITH original_areas AS MATERIALIZED (SELECT id, layer_name, geom
                                     )
                             ) AS geom
                      FROM regulatory_areas
-                     GROUP BY layer_name),
+                        GROUP BY layer_name, location),
      base_id AS (SELECT GREATEST(COALESCE(MAX(id), 0), 999999) AS base_id
                  FROM regulatory_areas),
      new_groups AS (
-         INSERT INTO regulatory_areas (id, area_type, layer_name, geom, creation)
+          INSERT INTO regulatory_areas (id, area_type, layer_name, location, geom, creation)
              SELECT ids.id,
                     'GROUP',
                     ids.layer_name,
+                      ids.location,
                     ids.geom,
                     now()
              FROM (SELECT base_id + ROW_NUMBER() OVER (ORDER BY gr.layer_name) AS id,
                           gr.layer_name,
+                             gr.location,
                           gr.geom
                    FROM grouped_reg gr
                             CROSS JOIN base_id) ids
-             RETURNING id, layer_name)
+              RETURNING id, layer_name, location)
 INSERT
 INTO regulatory_areas_group (regulatory_area_id, group_id)
 SELECT o.id,
        g.id
 FROM original_areas o
          JOIN new_groups g
-              ON g.layer_name = o.layer_name;
+                ON g.layer_name = o.layer_name
+                    AND g.location IS NOT DISTINCT FROM o.location;
